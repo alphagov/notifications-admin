feb3b14a0a6c36f02151c00c3168625f513445b1
Rebecca Law <rebecca.law@digital.cabinet-office.gov.uk>
2015-11-20 09:51:42 +0000

    Add trigger dependent build and slack notification

---
+++ scripts/trigger-dependent-build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -x
+
+# This script lives in each of the upstream repos. Add this to .travis.yml to
+# run after each successful build (assuming that the script is in the root of
+# the repo):
+#   after_success:
+#     - ./trigger-dependent-build
+#
+
+curl -vvv -s -X POST -H "Content-Type: application/json" -H "Accept: application/json" -H "Travis-API-Version: 3" -H "Authorization: token $auth_token" -d '{"request":{"branch":"master"}}' https://api.travis-ci.org/repo/alphagov%2Fnotify-functional-tests/requests
