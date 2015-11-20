96dd866e6fb2d948fc74c02bf7254450c982a629
Rebecca Law <rebecca.law@digital.cabinet-office.gov.uk>
2015-11-20 15:36:47 +0000

    Add Procfile for heroku

--- application.py
+++ application.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello from notify-admin-frontend'
+    return 'Hello from notifications-admin'
 
 if __name__ == '__main__':
     app.run(port=6012)
