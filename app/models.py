27572ddb08713761a4b63fe8afcd504872c6fbd7
Rebecca Law <rebecca.law@digital.cabinet-office.gov.uk>
2015-12-09 13:20:00 +0000

    Add indexes for the users table

--- app/models.py
+++ app/models.py
@@ -16,7 +16,7 @@ class User(db.Model):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, nullable=False, index=True, unique=True)
+    name = db.Column(db.String, nullable=False, index=True, unique=False)
     email_address = db.Column(db.String(255), nullable=False, index=True)
     password = db.Column(db.String, index=False, unique=False, nullable=False)
     mobile_number = db.Column(db.String, index=False, unique=False, nullable=False)
