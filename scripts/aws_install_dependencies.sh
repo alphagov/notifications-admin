5492116d191d84310e12dcdfb1559c5effab482d
Martyn Inglis <martyn.inglis@digital.cabinet-office.gov.uk>
2015-12-17 11:08:37 +0000

    Path wrong on start script

--- scripts/aws_install_dependencies.sh
+++ scripts/aws_install_dependencies.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Install dependencies"
-cd /home/ubuntu/notifications-api; gem install sass
-export FLASK_CONFIG=/home/ubuntu/config.cfg
+cd /home/ubuntu/notifications-admin;
+gem install sass;
 python app.py db upgrade
 pip install -r /home/ubuntu/notifications-admin/requirements.txt
