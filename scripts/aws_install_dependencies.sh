cbd47fe2ceadb0b8f081f9fafcb7e27b8477998e
Martyn Inglis <martyn.inglis@digital.cabinet-office.gov.uk>
2015-12-17 11:18:27 +0000

    Export flask config in set up script

--- scripts/aws_install_dependencies.sh
+++ scripts/aws_install_dependencies.sh
@@ -3,5 +3,6 @@
 echo "Install dependencies"
 cd /home/ubuntu/notifications-admin;
 gem install sass;
+export FLASK_CONFIG=/home/ubuntu/config.cfg
 python app.py db upgrade
 pip install -r /home/ubuntu/notifications-admin/requirements.txt
