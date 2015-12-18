cad56987543c17999a9209d2acc69b0d2aa49af2
Chris Hill-Scott <me@quis.cc>
2015-12-18 16:36:24 +0000

    Add big numbers to dashboard
    A suggestion of what high-level information users might need on the dashboard
    page.

--- app/main/views/dashboard.py
+++ app/main/views/dashboard.py
@@ -8,5 +8,8 @@ from ._jobs import jobs
 @main.route("/dashboard")
 def dashboard():
     return render_template(
-        'views/dashboard.html', jobs=jobs
+        'views/dashboard.html',
+        jobs=jobs,
+        free_text_messages_remaining=560,
+        spent_this_month='0.00'
     )
