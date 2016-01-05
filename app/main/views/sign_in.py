4fcc4efea2691f6c33b999b0c633691bdfcb3a9b
Nicholas Staples <nicholas.staples@solution-tribute.com>
2016-01-05 17:11:44 +0000

    Small comments.

--- app/main/views/sign_in.py
+++ app/main/views/sign_in.py
@@ -11,21 +11,17 @@ from app.main.views import send_sms_code
 
 @main.route('/sign-in', methods=(['GET', 'POST']))
 def sign_in():
-    try:
-        form = LoginForm()
-        if form.validate_on_submit():
-            user = users_dao.get_user_by_email(form.email_address.data)
-            if user:
-                if not user.is_locked() and user.is_active() and check_hash(form.password.data, user.password):
-                    send_sms_code(user.id, user.mobile_number)
-                    session['user_id'] = user.id
-                    return redirect(url_for('.two_factor'))
-                else:
-                    users_dao.increment_failed_login_count(user.id)
-            # Vague error message for login
-            form.password.errors.append('Username or password is incorrect')
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = users_dao.get_user_by_email(form.email_address.data)
+        if user:
+            if not user.is_locked() and user.is_active() and check_hash(form.password.data, user.password):
+                send_sms_code(user.id, user.mobile_number)
+                session['user_id'] = user.id
+                return redirect(url_for('.two_factor'))
+            else:
+                users_dao.increment_failed_login_count(user.id)
+        # Vague error message for login
+        form.password.errors.append('Username or password is incorrect')
 
-        return render_template('views/signin.html', form=form)
-    except:
-        import traceback
-        traceback.print_exc()
+    return render_template('views/signin.html', form=form)
