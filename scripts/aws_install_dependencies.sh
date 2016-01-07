cd1af565485d50130e8935b165d5db8272bb15fb
Martyn Inglis <martyn.inglis@digital.cabinet-office.gov.uk>
2016-01-07 16:13:25 +0000

    Fix python3/2 issues

--- scripts/aws_install_dependencies.sh
+++ scripts/aws_install_dependencies.sh
@@ -3,5 +3,5 @@
 echo "Install dependencies"
 cd /home/ubuntu/notifications-admin;
 export FLASK_CONFIG=/home/ubuntu/config.cfg
-python3 app.py db upgrade
 pip3 install -r /home/ubuntu/notifications-admin/requirements.txt
+python3 app.py db upgrade
