60f66018450b48880f1ca3ba9cdf7b8305092a87
Martyn Inglis <martyn.inglis@digital.cabinet-office.gov.uk>
2016-01-10 21:53:41 +0000

    Copy the db upgrade command into WSGI script.
    In there currently as wsgi has the credstash which we don't won't to apply in non-awe environments. This will need a refactor to better handle code duplication.

--- scripts/aws_install_dependencies.sh
+++ scripts/aws_install_dependencies.sh
@@ -4,4 +4,4 @@ echo "Install dependencies"
 cd /home/ubuntu/notifications-admin;
 export FLASK_CONFIG=/home/ubuntu/config.cfg
 pip3 install -r /home/ubuntu/notifications-admin/requirements.txt
-python3 app.py db upgrade
+python3 wsgi.py db upgrade
--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,9 @@
 from app import create_app
 import os
 from credstash import getAllSecrets
+from flask.ext.script import Manager, Server
+from flask_migrate import Migrate, MigrateCommand
+from app import create_app, db
 
 secrets = getAllSecrets(region="eu-west-1")
 
@@ -9,6 +12,11 @@ application = create_app(os.getenv('NOTIFICATIONS_ADMIN_ENVIRONMENT') or 'live')
 for key in application.config.keys():
         if key in secrets:
                 application.config[key] = secrets[key]
+
+
+manager = Manager(application)
+migrate = Migrate(application, db)
+manager.add_command('db', MigrateCommand)
 
 if __name__ == "__main__":
         application.run()
