42b62ce7cd25e8c4359191b8f350f0624cadde6e
Martyn Inglis <martyn.inglis@digital.cabinet-office.gov.uk>
2016-01-11 09:50:12 +0000

    Removed secrets setup from db script

--- db.py
+++ db.py
@@ -1,16 +1,8 @@
-from credstash import getAllSecrets
 from flask.ext.script import Manager, Server
 from flask_migrate import Migrate, MigrateCommand
 from app import create_app, db
 
-secrets = getAllSecrets(region="eu-west-1")
-
 application = create_app('live')
-
-for key in application.config.keys():
-        if key in secrets:
-                application.config[key] = secrets[key]
-
 
 manager = Manager(application)
 migrate = Migrate(application, db)
