ba50f132fcdf926dd8b88709a2ca6de606225169
Chris Hill-Scott <me@quis.cc>
2016-01-12 09:58:21 +0000

    Add table to user profile page

--- tests/app/main/views/test_user_profile.py
+++ tests/app/main/views/test_user_profile.py
@@ -1,4 +1,5 @@
 def test_should_show_overview_page(notifications_admin):
     response = notifications_admin.test_client().get('/user-profile')
 
+    assert 'Your profile' in response.get_data(as_text=True)
     assert response.status_code == 200
