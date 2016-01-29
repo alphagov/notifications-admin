bed111658741350f95a4ec96d4118575a854c815
Nicholas Staples <nicholas.staples@solution-tribute.com>
2016-01-29 14:41:07 +0000

    Status update added.

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 from flask.ext.script import Manager, Server
 from flask_migrate import Migrate, MigrateCommand
-from app import create_app
+from app import (create_app, get_app_version)
 
 
 application = create_app(os.getenv('NOTIFICATIONS_ADMIN_ENVIRONMENT') or 'development')
@@ -15,6 +15,14 @@ def list_routes():
     """List URLs of all application routes."""
     for rule in sorted(application.url_map.iter_rules(), key=lambda r: r.rule):
         print("{:10} {}".format(", ".join(rule.methods - set(['OPTIONS', 'HEAD'])), rule.rule))
+
+
+@manager.command
+def app_version():
+    """
+    Retrieve the version of the api.
+    """
+    return get_app_version()
 
 
 if __name__ == '__main__':
