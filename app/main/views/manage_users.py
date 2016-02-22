bf3f6d7d3eb657c04f5bac7e5b829485502bfa91
Chris Hill-Scott <me@quis.cc>
2016-02-22 12:07:09 +0000

    Fix users page for research
    For the lab sessions, users should just see themselves as the only user, not
    all the fake users (it might confuse them).

--- app/main/views/manage_users.py
+++ app/main/views/manage_users.py
@@ -15,55 +15,13 @@ from app.main.forms import InviteUserForm
 from app.main.dao.services_dao import get_service_by_id_or_404
 from app import user_api_client
 
-
 fake_users = [
     {
-        'name': 'Henry Hadlow',
-        'email_localpart': 'henry.hadlow',
-        'permission_send_messages': True,
-        'permission_manage_service': False,
-        'permission_manage_api_keys': False,
-        'active': True
-    },
-
-    {
-        'name': 'Pete Herlihy',
-        'email_localpart': 'pete.herlihy',
-        'permission_send_messages': False,
-        'permission_manage_service': False,
-        'permission_manage_api_keys': False,
-        'active': True
-    },
-    {
-        'name': 'Chris Hill-Scott',
-        'email_localpart': 'chris.hill-scott',
+        'name': '',
         'permission_send_messages': True,
         'permission_manage_service': True,
         'permission_manage_api_keys': True,
         'active': True
-    },
-    {
-        'name': 'Martyn Inglis',
-        'email_localpart': 'martyn.inglis',
-        'permission_send_messages': True,
-        'permission_manage_service': True,
-        'permission_manage_api_keys': True,
-        'active': True
-    },
-    {
-        'email_localpart': 'caley.smolska',
-        'permission_send_messages': True,
-        'permission_manage_service': False,
-        'permission_manage_api_keys': False,
-        'active': False
-    },
-
-    {
-        'email_localpart': 'ash.stephens',
-        'permission_send_messages': False,
-        'permission_manage_service': False,
-        'permission_manage_api_keys': False,
-        'active': False
     }
 ]
 
@@ -74,12 +32,9 @@ def manage_users(service_id):
     return render_template(
         'views/manage-users.html',
         service_id=service_id,
-        users=[
-            dict(id=user_id, **user) for (user_id, user) in enumerate(fake_users) if user['active']
-        ],
-        invited_users=[
-            dict(id=user_id, **user) for (user_id, user) in enumerate(fake_users) if not user['active']
-        ]
+        users=fake_users,
+        current_user=current_user,
+        invited_users=[]
     )
 
 
