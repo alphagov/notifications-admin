ce221fc40bcaab7b2d30d24f1e604093a9d661e1
Chris Hill-Scott <me@quis.cc>
2016-02-26 10:21:51 +0000

    Drop ‘preview service name’ page
    This page:
    - confused users in research
    - didn’t communicate what it was intended to (eg the generated email address,
      how your service name would appear in messages)
    
    This commit removes the page so that after typing in the service name the user
    is sent straight to the dashboard for their new service.

--- app/main/views/add_service.py
+++ app/main/views/add_service.py
@@ -11,39 +11,15 @@ from app.main.forms import AddServiceForm
 @login_required
 def add_service():
     form = AddServiceForm(services_dao.find_all_service_names)
-    services = services_dao.get_services(current_user.id)
-    if len(services['data']) == 0:
-        heading = 'Which service do you want to set up notifications for?'
-    else:
-        heading = 'Add a new service'
+    heading = 'Which service do you want to set up notifications for?'
     if form.validate_on_submit():
         session['service_name'] = form.name.data
-        return redirect(url_for('main.add_from_address'))
+        user = users_dao.get_user_by_id(session['user_id'])
+        service_id = services_dao.insert_new_service(session['service_name'], user.id)
+        return redirect(url_for('main.service_dashboard', service_id=service_id))
     else:
         return render_template(
             'views/add-service.html',
             form=form,
             heading=heading
         )
-
-
-@main.route("/confirm-add-service", methods=['GET', 'POST'])
-@login_required
-def add_from_address():
-    if request.method == 'POST':
-        user = users_dao.get_user_by_id(session['user_id'])
-        service_id = services_dao.insert_new_service(session['service_name'], user.id)
-        return redirect(url_for('main.service_dashboard', service_id=service_id))
-    else:
-        return render_template(
-            'views/add-from-address.html',
-            service_name=session['service_name'],
-            from_address="{}@notifications.service.gov.uk".format(_email_safe(session['service_name']))
-        )
-
-
-def _email_safe(string):
-    return "".join([
-        character.lower() if character.isalnum() or character == "." else ""
-        for character in re.sub("\s+", ".", string.strip())
-    ])
