76ccb99666092e866be37009dd18df49e83cd1fb
Martyn Inglis <martyn.inglis@digital.cabinet-office.gov.uk>
2016-03-03 16:41:21 +0000

    Fixed up the health check page

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import os
 from flask.ext.script import Manager, Server
-from flask_migrate import Migrate, MigrateCommand
-from app import (create_app, get_app_version)
+from app import create_app
 
 
 application = create_app(os.getenv('NOTIFICATIONS_ADMIN_ENVIRONMENT') or 'development')
@@ -15,14 +14,6 @@ def list_routes():
     """List URLs of all application routes."""
     for rule in sorted(application.url_map.iter_rules(), key=lambda r: r.rule):
         print("{:10} {}".format(", ".join(rule.methods - set(['OPTIONS', 'HEAD'])), rule.rule))
-
-
-@manager.command
-def app_version():
-    """
-    Retrieve the version of the api.
-    """
-    return get_app_version()
 
 
 if __name__ == '__main__':
