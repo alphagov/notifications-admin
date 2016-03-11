ccaa5410b355364ddad9873dc233488fd4d726d5
Rebecca Law <rebecca.law@digital.cabinet-office.gov.uk>
2016-03-11 10:47:16 +0000

    Removed test as it was not testing the right thing

--- tests/app/main/test_errorhandlers.py
+++ tests/app/main/test_errorhandlers.py
@@ -8,14 +8,3 @@ def test_bad_url_returns_page_not_found(app_):
         assert response.status_code == 404
         page = BeautifulSoup(response.data.decode('utf-8'), 'html.parser')
         assert page.h1.string.strip() == 'Page could not be found'
-
-
-def test_bad_input_returns_something(app_, api_user_active, mock_login):
-    with app_.test_request_context():
-        with app_.test_client() as client:
-            client.login(api_user_active)
-            response = client.get(url_for('main.service_dashboard', service_id=1))
-            response.status == 404
-            print(response.get_data(as_text=True))
-            page = BeautifulSoup(response.data.decode('utf-8'), 'html.parser')
-            assert page.h1.string.strip() == 'Page could not be found'
