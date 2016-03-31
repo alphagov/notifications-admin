70e140c90173f60e8c777a9b151ef1630af1498e
Martyn Inglis <martyn.inglis@digital.cabinet-office.gov.uk>
2016-03-31 13:58:43 +0100

    Got the path wrong. Fixed

--- scripts/aws_change_ownership.sh
+++ scripts/aws_change_ownership.sh
@@ -2,4 +2,4 @@
 
 echo "Chown application to be owned by notify"
 cd /home/notify-app/;
-chown -R notify-app:govuk-notify-applications notifications-api
+chown -R notify-app:govuk-notify-applications notifications-admin
