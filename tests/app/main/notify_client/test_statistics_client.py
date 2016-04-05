eade0ef971f16d03c55efbd1971463bb4235c5b6
Adam Shimali <adam.shimali@gmail.com>
2016-04-05 13:49:22 +0100

    Added test for template stats client.
    Removed date column from recently used templates table.

--- tests/app/main/notify_client/test_statistics_client.py
+++ tests/app/main/notify_client/test_statistics_client.py
@@ -1,13 +1,17 @@
+import uuid
+
 from app.notify_client.statistics_api_client import StatisticsApiClient
 
 
-def test_client_uses_correct_find_by_email(mocker, api_user_active):
+def test_notifications_statistics_client_calls_correct_api_endpoint(mocker, api_user_active):
 
-    expected_url = '/service/a1b2c3d4/notifications-statistics'
+    some_service_id = uuid.uuid4()
+    expected_url = '/service/{}/notifications-statistics'.format(some_service_id)
 
     client = StatisticsApiClient()
+
     mock_get = mocker.patch('app.notify_client.statistics_api_client.StatisticsApiClient.get')
 
-    client.get_statistics_for_service('a1b2c3d4')
+    client.get_statistics_for_service(some_service_id)
 
     mock_get.assert_called_once_with(url=expected_url)
