5c3683ae8630eed021ad5b4cf0f514e9c0517fa3
Rebecca Law <rebecca.law@digital.cabinet-office.gov.uk>
2016-04-21 11:19:22 +0100

    Fix order of imports on wsgi

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,5 @@
-import os
-
 from credstash import getAllSecrets
-
-from app import create_app
-from config import configs
+import os
 
 default_env_file = '/home/ubuntu/environment'
 environment = 'live'
@@ -12,10 +8,15 @@ if os.path.isfile(default_env_file):
     with open(default_env_file, 'r') as environment_file:
         environment = environment_file.readline().strip()
 
+
 # on aws get secrets and export to env
 os.environ.update(getAllSecrets(region="eu-west-1"))
 
+from config import configs  # noqa
+
 os.environ['NOTIFY_ADMIN_ENVIRONMENT'] = configs[environment]
+
+from app import create_app  # noqa
 
 application = create_app()
 
