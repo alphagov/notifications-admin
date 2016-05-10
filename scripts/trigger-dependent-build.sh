521fdc26abc408cf9f422e70ab95c218c1b020e1
Adam Shimali <adam.shimali@gmail.com>
2016-05-10 14:10:08 +0100

    Pass branch name to dependent build

--- scripts/trigger-dependent-build.sh
+++ scripts/trigger-dependent-build.sh
@@ -7,4 +7,6 @@
 #     - ./trigger-dependent-build
 #
 
-curl -vvv -s -X POST -H "Content-Type: application/json" -H "Accept: application/json" -H "Travis-API-Version: 3" -H "Authorization: token $auth_token" -d '{"request":{"branch":"master","config":{"env":{"global":["ENVIRONMENT=preview"]}}}}' https://api.travis-ci.org/repo/alphagov%2Fnotifications-functional-tests/requests
+echo "Triggering dependent build for $TRAVIS_BRANCH"
+
+curl -vvv -s -X POST -H "Content-Type: application/json" -H "Accept: application/json" -H "Travis-API-Version: 3" -H "Authorization: token $auth_token" -d '{"request":{"branch":"master","config":{"env":{"global":["ENVIRONMENT='$TRAVIS_BRANCH'"]}}}}' https://api.travis-ci.org/repo/alphagov%2Fnotifications-functional-tests/requests
