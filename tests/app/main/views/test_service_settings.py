4edd308a54736185688e367fd6bb3bd8f1e3fc83
Adam Shimali <adam.shimali@gmail.com>
2016-07-04 13:46:50 +0100

    Better flash messages for setting text message sender.

--- tests/app/main/views/test_service_settings.py
+++ tests/app/main/views/test_service_settings.py
@@ -735,3 +735,37 @@ def test_if_sms_sender_set_then_form_populated(app_,
         assert response.status_code == 200
         page = BeautifulSoup(response.data.decode('utf-8'), 'html.parser')
         assert page.find(id='sms_sender')['value'] == 'elevenchars'
+
+
+def test_set_text_message_sender_flash_messages(
+        app_,
+        active_user_with_permissions,
+        mocker,
+        mock_update_service,
+        service_one):
+    with app_.test_request_context():
+        with app_.test_client() as client:
+            client.login(active_user_with_permissions, mocker, service_one)
+            data = {"sms_sender": "elevenchars"}
+            response = client.post(url_for('main.service_set_sms_sender', service_id=service_one['id']),
+                                   data=data,
+                                   follow_redirects=True)
+
+            assert response.status_code == 200
+
+            page = BeautifulSoup(response.data.decode('utf-8'), 'html.parser')
+            element = page.find('div', {"class": "banner-default-with-tick"})
+
+            assert element.text.strip() == 'Text message sender set to elevenchars'
+
+            data = {"sms_sender": ""}
+            response = client.post(url_for('main.service_set_sms_sender', service_id=service_one['id']),
+                                   data=data,
+                                   follow_redirects=True)
+
+            assert response.status_code == 200
+
+            page = BeautifulSoup(response.data.decode('utf-8'), 'html.parser')
+            element = page.find('div', {"class": "banner-default-with-tick"})
+
+            assert element.text.strip() == 'Text message sender removed'
