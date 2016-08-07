a283a2f288d1482acdf6345dfba90c86c0b874f4
Chris Hill-Scott <me@quis.cc>
2016-08-07 10:21:06 +0100

    Option 2: faux dropdown

--- app/assets/javascripts/paginatedOptions.js
+++ app/assets/javascripts/paginatedOptions.js
@@ -2,27 +2,47 @@
 
   "use strict";
 
+  var filterOptionVisibility = $options => $options
+    .removeClass('js-visible')
+    .filter(
+      (index, element) => (index === 0 || $(element).has(':checked').length)
+    )
+    .addClass('js-visible');
+
   Modules.PaginatedOptions = function() {
 
     this.start = function(component) {
 
-      console.log('hi');
-
       let $component = $(component);
       let $options = $('label', $component);
-      let $button = $('.tertiary-button', $component);
-      let containerWidth = $component.width() - $button.outerWidth();
-      let runningWidth = 0, i = 0, newWidth;
+      let $button = $('<input type="button" value="Later" class="tertiary-button js-visible" />');
 
-      while (i <= $options.length) {
-        runningWidth = runningWidth + $options.eq(i++).outerWidth(true);
-        if (runningWidth > containerWidth) break;
-      }
+      $component.append($button);
 
-      $component.width(runningWidth);
+      filterOptionVisibility($options);
 
       $button.on('click', function() {
-        $options.eq(0).css('margin-left', $options.eq(0).outerWidth(true) * -1);
+        $options.addClass('js-visible').has(':checked').focus();
+        $button.removeClass('js-visible');
+      });
+
+      $component.on('click', 'label', function() {
+
+        // Workaround because GOV.UK SelectionButtons doesn’t deselect in this case
+        $options.filter((index, element) => $(element).not(':has(:checked)')).removeClass('selected');
+
+        filterOptionVisibility($options);
+
+        $button.addClass('js-visible');
+
+        if ($options.has(':checked').find('input').attr('id') === $options.eq(0).find('input').attr('id')) {
+          $button.prop('value', 'Later');
+        } else {
+          $button.prop('value', 'Choose a different time');
+        }
+
+        $options.has(':checked').focus();
+
       });
 
     };
