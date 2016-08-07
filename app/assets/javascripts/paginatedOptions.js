05a15c38e906982f598f5a5c7e37263aea32b28a
Chris Hill-Scott <me@quis.cc>
2016-08-07 19:41:10 +0100

    Better wording

--- app/assets/javascripts/paginatedOptions.js
+++ app/assets/javascripts/paginatedOptions.js
@@ -26,10 +26,21 @@
         $button.removeClass('js-visible');
       });
 
-      $component.on('click', 'label', function() {
+      $component.on('change', 'input[type=radio]', function() {
 
-        // Workaround because GOV.UK SelectionButtons doesn’t deselect in this case
-        $options.filter((index, element) => $(element).not(':has(:checked)')).removeClass('selected');
+        $(this).trigger('focus');
+
+      });
+
+      $(document).on('focus', '*', function(event) {
+
+        console.log('focus', $(this));
+
+        return;
+
+        console.log('blur', $component.find(':focus'));
+
+        if ($component.find(':focus').length > 0) return true;
 
         filterOptionVisibility($options);
 
@@ -41,7 +52,26 @@
           $button.prop('value', 'Choose a different time');
         }
 
-        $options.has(':checked').focus();
+        return true;
+
+      });
+
+      $component.on('click', 'input[type=radio]', function(event) {
+
+        // Workaround because GOV.UK SelectionButtons doesn’t deselect in this case
+        $options.filter((index, element) => $(element).not(':has(:checked)')).removeClass('selected');
+        console.log('click', event.pageX);
+        if (!event.pageX) return;
+
+        filterOptionVisibility($options);
+
+        $button.addClass('js-visible');
+
+        if ($options.has(':checked').find('input').attr('id') === $options.eq(0).find('input').attr('id')) {
+          $button.prop('value', 'Later');
+        } else {
+          $button.prop('value', 'Choose a different time');
+        }
 
       });
 
