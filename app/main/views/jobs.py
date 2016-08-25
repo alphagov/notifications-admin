c61199d17d7c87673d905e5efbaa3bfe16dd4d6b
Martyn Inglis <martyn.inglis@digital.cabinet-office.gov.uk>
2016-08-25 09:15:55 +0100

    Rewrote some slighlt over complicated logic

--- app/main/views/jobs.py
+++ app/main/views/jobs.py
@@ -78,14 +78,12 @@ def view_job(service_id, job_id):
     filter_args = _parse_filter_args(request.args)
     filter_args['status'] = _set_status_filters(filter_args)
 
-    finished = (
-        bool(job.get('notification_count', 0) and ((
-            job.get('notifications_delivered', 0) +
-            job.get('notifications_failed', 0)
-        ) == job.get('notification_count', 0))))
+    total_notifications = job.get('notification_count', 0)
+    processed_notifications = job.get('notifications_delivered', 0) + job.get('notifications_failed', 0)
+
     return render_template(
         'views/jobs/job.html',
-        finished=finished,
+        finished=(total_notifications == processed_notifications),
         uploaded_file_name=job['original_file_name'],
         template=Template(
             service_api_client.get_service_template(
