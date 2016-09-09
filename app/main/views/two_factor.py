defa7ac2c80b91fec447808fb0bd4abd01426640
Imdad Ahad <imdad.ahad@digital.cabinet-office.gov.uk>
2016-09-09 15:24:56 +0100

    Remove spacing

--- app/main/views/two_factor.py
+++ app/main/views/two_factor.py
@@ -26,14 +26,12 @@ def two_factor():
     if form.validate_on_submit():
         try:
             user = user_api_client.get_user(user_id)
-
             services = service_api_client.get_services({'user_id': str(user_id)}).get('data', [])
             # Check if coming from new password page
             if 'password' in session['user_details']:
                 user.set_password(session['user_details']['password'])
                 user.reset_failed_login_count()
                 user_api_client.update_user(user)
-
             activated_user = user_api_client.activate_user(user)
             login_user(activated_user, remember=True)
         finally:
