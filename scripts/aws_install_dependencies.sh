789fc7528c440aee6450aa01279c09269727913f
Imdad Ahad <imdad.ahad@digital.cabinet-office.gov.uk>
2016-10-21 16:38:17 +0100

    Use python wheels to speed up AS deployment: * Create wheels from python dependencies on building codedeploy artifact * Update script to install wheels and default to pypi if not found

--- scripts/aws_install_dependencies.sh
+++ scripts/aws_install_dependencies.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 echo "Install dependencies"
+
 cd /home/notify-app/notifications-admin;
-pip3 install -r /home/notify-app/notifications-admin/requirements.txt
+
+pip3 install --find-links=wheelhouse -r /home/notify-app/notifications-admin/requirements.txt
