93cdaf1811007366308d5eacbcfdaf9977b6ef67
Leo Hemsted <leo.hemsted@digital.cabinet-office.gov.uk>
2016-10-31 18:01:31 +0000

    allow users to see letters even if they dont have edit permissions
    same way as we handle seeing emails/sms

--- tests/app/main/views/test_letters.py
+++ tests/app/main/views/test_letters.py
@@ -17,30 +17,7 @@ def test_letters_access_restricted(logged_in_client, mocker, can_send_letters, r
     assert response.status_code == response_code
 
 
-@pytest.mark.parametrize('permission', [
-    'send_letters',
-    'manage_templates'
-])
-def test_letters_lets_in_with_permissions(
-    client,
-    mocker,
-    mock_login,
-    mock_has_permissions,
-    api_user_active,
-    permission,
-):
-    service = service_json(can_send_letters=True)
-    mocker.patch('app.service_api_client.get_service', return_value={"data": service})
-
-    api_user_active._permissions[str(service['id'])] = [permission]
-
-    client.login(api_user_active)
-    response = client.get(url_for('main.letters', service_id=service['id']))
-
-    assert response.status_code == 200
-
-
-def test_letters_rejects_without_permissions(
+def test_letters_lets_in_without_permission(
     client,
     mocker,
     mock_login,
@@ -53,4 +30,5 @@ def test_letters_rejects_without_permissions(
     client.login(api_user_active)
     response = client.get(url_for('main.letters', service_id=service['id']))
 
+    assert api_user_active.permissions == {}
     assert response.status_code == 200
