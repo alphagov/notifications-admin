5f47ebb8e00d8132ec1ba6370d7e1b80b3e8a103
Chris Hill-Scott <me@quis.cc>
2016-12-12 14:15:20 +0000

    Remove redundant test
    This looks like I accidentally copy pasted this (along with some stuff
    that was meant to come over) as part of 22fe164711c1518be909c6e624a5d24a7b337246

--- tests/app/main/views/test_feedback.py
+++ tests/app/main/views/test_feedback.py
@@ -17,21 +17,6 @@ def no_redirect():
     return lambda _external=True: None
 
 
-def test_logged_in_user_redirects_to_choose_service(app_,
-                                                    api_user_active,
-                                                    mock_get_user,
-                                                    mock_get_user_by_email,
-                                                    mock_login):
-    with app_.test_request_context():
-        with app_.test_client() as client:
-            client.login(api_user_active)
-            response = client.get(url_for('main.index'))
-            assert response.status_code == 302
-
-            response = client.get(url_for('main.sign_in', follow_redirects=True))
-            assert response.location == url_for('main.choose_service', _external=True)
-
-
 def test_get_support_index_page(client):
     resp = client.get(url_for('main.support'))
     assert resp.status_code == 200
