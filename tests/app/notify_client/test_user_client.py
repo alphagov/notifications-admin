7ad56df78b2130b98ce0a94da8c5e60fb6b411a4
Imdad Ahad <imdad.ahad@digital.cabinet-office.gov.uk>
2017-02-07 13:31:46 +0000

    Change user api client to update password with new endpoint

--- tests/app/notify_client/test_user_client.py
+++ tests/app/notify_client/test_user_client.py
@@ -22,3 +22,14 @@ def test_client_only_updates_allowed_attributes(mocker):
     with pytest.raises(TypeError) as error:
         UserApiClient().update_user_attribute('user_id', id='1')
     assert str(error.value) == 'Not allowed to update user attributes: id'
+
+
+def test_client_updates_password_separately(mocker, api_user_active):
+    expected_url = '/user/{}/update-password'.format(api_user_active.id)
+    expected_params = {'_password': 'newpassword'}
+    client = UserApiClient()
+    client.max_failed_login_count = 1  # doesn't matter for this test
+    mock_update_password = mocker.patch('app.notify_client.user_api_client.UserApiClient.post')
+
+    client.update_password(api_user_active.id, expected_params['_password'])
+    mock_update_password.assert_called_once_with(expected_url, data=expected_params)
