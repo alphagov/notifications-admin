4110d06e1fb474272d9c880bf72d94da257ee19b
Imdad Ahad <imdad.ahad@digital.cabinet-office.gov.uk>
2017-02-14 12:17:03 +0000

    Cancel uninstalling python packages

--- scripts/aws_clear_instance.sh
+++ scripts/aws_clear_instance.sh
@@ -5,10 +5,6 @@ echo "Removing application and dependencies"
 if [ -d "/home/notify-app/notifications-admin" ]; then
     # Remove and re-create the directory
     rm -rf /home/notify-app/notifications-admin
-    mkdir -vp /home/notify-app/notifications-admin
-    # Remove installed py3 packages
-    pip3 freeze | xargs pip3 uninstall -y
-else
-    echo "Directory does not exist, something went wrong!"
+    mkdir -p /home/notify-app/notifications-admin
 fi
 
