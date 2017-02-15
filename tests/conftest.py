6f8568b9041a6f6ab760cf9450ea72d753082795
Leo Hemsted <leo.hemsted@digital.cabinet-office.gov.uk>
2017-02-15 13:49:15 +0000

    add tests for gsm handling in save/edit template

--- tests/conftest.py
+++ tests/conftest.py
@@ -386,9 +386,8 @@ def mock_get_service_letter_template(mocker):
 
 @pytest.fixture(scope='function')
 def mock_create_service_template(mocker, fake_uuid):
-    def _create(name, type_, content, service, subject=None):
-        template = template_json(
-            fake_uuid, name, type_, content, service)
+    def _create(name, type_, content, service, subject=None, process_type=None):
+        template = template_json(fake_uuid, name, type_, content, service, process_type)
         return {'data': template}
 
     return mocker.patch(
@@ -399,8 +398,7 @@ def mock_create_service_template(mocker, fake_uuid):
 @pytest.fixture(scope='function')
 def mock_update_service_template(mocker):
     def _update(id_, name, type_, content, service, subject=None, process_type=None):
-        template = template_json(
-            service, id_, name, type_, content, subject)
+        template = template_json(service, id_, name, type_, content, subject, process_type)
         return {'data': template}
 
     return mocker.patch(
