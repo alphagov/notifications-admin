8d5af476203274d3de36c8d42738e149af967d26
Chris Hill-Scott <me@quis.cc>
2017-04-10 11:16:22 +0100

    Change class names for validation errors in html
    Implements the class name changes detailed in:
    https://github.com/alphagov/govuk_elements/pull/405

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -2,8 +2,6 @@ $(() => $("time.timeago").timeago());
 
 $(() => GOVUK.modules.start());
 
-$(() => new GOVUK.SelectionButtons('.block-label input'));
-
 $(() => $('.error-message').eq(0).parent('label').next('input').trigger('focus'));
 
 $(() => $('.banner-dangerous').eq(0).trigger('focus'));
