b02df9ba4b7d9ac51c1a57f78a32d1dfe6100c0b
Ken Tsang <ken.tsang@digital.cabinet-office.gov.uk>
2017-04-12 17:50:34 +0100

    Redirect after letter-jobs post

--- app/main/views/letter_jobs.py
+++ app/main/views/letter_jobs.py
@@ -1,6 +1,6 @@
 import datetime
 
-from flask import render_template, request
+from flask import redirect, render_template, request, session, url_for
 from flask_login import login_required
 
 from app import letter_jobs_client, format_datetime_24h
@@ -12,20 +12,27 @@ from app.utils import user_has_permissions
 @login_required
 @user_has_permissions(admin_override=True)
 def letter_jobs():
-    msg = ''
     letter_jobs_list = letter_jobs_client.get_letter_jobs()
 
     if request.method == 'POST':
         if len(request.form.getlist('job_id')) > 0:
             job_ids = request.form.getlist('job_id')
+            session['job_ids'] = job_ids
 
             response = letter_jobs_client.send_letter_jobs(job_ids)
             msg = response['response']
-
-            for job_id in job_ids:
-                job = [j for j in letter_jobs_list if job_id == j['id']][0]
-                job['sending'] = 'sending'
         else:
             msg = 'No jobs selected'
+
+        session['msg'] = msg
+
+        return redirect(url_for('main.letter_jobs'))
+
+    msg = session.pop('msg', None)
+    job_ids = session.pop('job_ids', None)
+    if job_ids:
+        for job_id in job_ids:
+            job = [j for j in letter_jobs_list if job_id == j['id']][0]
+            job['sending'] = 'sending'
 
     return render_template('views/letter-jobs.html', letter_jobs_list=letter_jobs_list, message=msg)
