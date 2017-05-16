3106ea0e15a954e0422e2474d42be9f5d490cbd5
Chris Hill-Scott <me@quis.cc>
2017-05-16 16:56:40 +0100

    Make send yourself a test form stay at top of page
    You might need to scroll this page quite a lot to see where a
    placeholder appears in your template – especially if you have a long
    email or letter.
    
    One of the things I’m trying to stop happening so much is a lot of
    scrolling back and forth. This would happen if you were scrolling down
    to see the placeholder, then back up to fill in its value.
    
    So this commit makes the textbox ‘sticky’, ie it always stays at the top
    of the viewport, even when you scroll down. This lets you see the
    placeholder and the textbox side by side, no matter how long the
    template is.
    
    The code to do this mostly comes from the GOV.UK Frontend Toolkit
    (documented here: https://github.com/alphagov/govuk_frontend_toolkit/blob/d9489a987086471fe30b4b925a81c12cd198c91d/docs/javascript.md#stick-at-top-when-scrolling).
    I had to add some extra CSS to make it look good when it overlaps the
    content of the page, which the GOV.UK Frontend Toolkit implementation
    doesn’t really anticipate.

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,5 +1,7 @@
 $(() => $("time.timeago").timeago());
 
+$(() => GOVUK.stickAtTopWhenScrolling.init());
+
 $(() => GOVUK.modules.start());
 
 $(() => $('.error-message').eq(0).parent('label').next('input').trigger('focus'));
