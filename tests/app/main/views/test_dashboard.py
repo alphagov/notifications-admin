74e79e175b70f6438c5a445f3fdd0abdace0e880
minglis <minglis@users.noreply.github.com>
2017-06-01 16:53:36 +0100

    Fix typo

--- tests/app/main/views/test_dashboard.py
+++ tests/app/main/views/test_dashboard.py
@@ -698,7 +698,7 @@ def test_should_show_all_jobs_with_valid_statuses(
     })
 
 
-def test_should_show_remaining_fee_tier_count(
+def test_should_show_remaining_free_tier_count(
     logged_in_client,
     mock_get_service_templates,
     mock_get_template_statistics,
