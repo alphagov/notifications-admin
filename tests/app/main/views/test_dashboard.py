834c255195865468bf7c916d18c0b38aa47564a7
minglis <minglis@users.noreply.github.com>
2017-06-08 13:55:13 +0100

    Fixed typos

--- tests/app/main/views/test_dashboard.py
+++ tests/app/main/views/test_dashboard.py
@@ -832,7 +832,7 @@ def test_should_show_all_jobs_with_valid_statuses(
     })
 
 
-def test_should_show_remaining_fee_tier_count(
+def test_should_show_remaining_free_tier_count(
     logged_in_client,
     mock_get_service_templates,
     mock_get_template_statistics,
@@ -853,7 +853,7 @@ def test_should_show_remaining_fee_tier_count(
     assert 'free text messages left' in response.get_data(as_text=True)
 
 
-def test_should_show_cost_if_exceeded_fee_tier_count(
+def test_should_show_cost_if_exceeded_free_tier_count(
     logged_in_client,
     mock_get_service_templates,
     mock_get_template_statistics,
