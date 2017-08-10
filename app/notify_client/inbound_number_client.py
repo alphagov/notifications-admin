30289f46b9a64319218654280f7e3bdc4fc9208e
venusbb <venus.bailey@digital.cabinet-office.gov.uk>
2017-08-10 11:59:46 +0100

    investigate test fail

--- app/notify_client/inbound_number_client.py
+++ app/notify_client/inbound_number_client.py
@@ -7,6 +7,8 @@ class InboundNumberClient(NotifyAdminAPIClient):
         super().__init__("a" * 73, "b")
 
     def init_app(self, app):
+        import pdb
+        pdb.set_trace()
         self.base_url = app.config['API_HOST_NAME']
         self.service_id = app.config['ADMIN_CLIENT_USER_NAME']
         self.api_key = app.config['ADMIN_CLIENT_SECRET']
