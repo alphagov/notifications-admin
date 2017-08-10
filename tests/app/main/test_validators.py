a10e76a9c2a7084358b1b8e2b6e4361d71f27e3f
Chris Hill-Scott <me@quis.cc>
2017-08-10 13:51:46 +0100

    Stop referring to central gov in error messages

--- tests/app/main/test_validators.py
+++ tests/app/main/test_validators.py
@@ -28,7 +28,7 @@ def test_valid_email_not_in_valid_domains(
 ):
     form = RegisterUserForm(email_address="test@test.com", mobile_number='441231231231')
     assert not form.validate()
-    assert "Enter a central government email address" in form.errors['email_address'][0]
+    assert "Enter a government email address" in form.errors['email_address'][0]
 
 
 def test_valid_email_in_valid_domains(
