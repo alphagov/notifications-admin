240f25eaf97bd65b9b9dab6f31bea614dd248528
Chris Hill-Scott <me@quis.cc>
2017-11-03 16:15:39 +0000

    Fix failing tests

--- tests/app/main/views/test_service_settings.py
+++ tests/app/main/views/test_service_settings.py
@@ -121,7 +121,7 @@ def test_should_show_overview(
         'Text message sender GOVUK Manage',
         'International text messages On Change',
         'Receive text messages On Change',
-        'API endpoint for received text messages Not set Change',
+        'Callback URL for received text messages Not set Change',
 
         'Label Value Action',
         'Send letters Off Change',
@@ -207,7 +207,7 @@ def test_service_settings_show_elided_api_url_if_needed(
 
     non_empty_trs = [tr.find_all('td') for tr in page.find_all('tr') if tr.find_all('td')]
     api_url = [api_setting[1].text.strip() for api_setting in non_empty_trs
-               if api_setting[0].text.strip() == 'API endpoint for received text messages'][0]
+               if api_setting[0].text.strip() == 'Callback URL for received text messages'][0]
     assert api_url == elided_url
     assert mocked_get_fn.called is True
 
