05357027072e809d891b228bb5704fb62791fd17
Leo Hemsted <leo.hemsted@digital.cabinet-office.gov.uk>
2017-11-07 11:23:15 +0000

    ensure flask app has right name

--- wsgi.py
+++ wsgi.py
@@ -9,9 +9,6 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'app', 'static')
 STATIC_URL = 'static/'
 
-app = Flask(__name__)
+app = Flask('app')
 create_app(app)
 application = WhiteNoise(app, STATIC_ROOT, STATIC_URL)
-
-if __name__ == "__main__":
-    application.run()
