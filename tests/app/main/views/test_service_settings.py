b9e5194fbd3600dafd40446ed69653e6ccfa808e
Chris Hill-Scott <me@quis.cc>
2018-01-31 10:37:44 +0000

    Add test case for spaces
    Turns out we accept spaces in text message senders. But there was no
    test case which covered this.

--- tests/app/main/views/test_service_settings.py
+++ tests/app/main/views/test_service_settings.py
@@ -810,6 +810,7 @@ def test_incorrect_letter_contact_block_input(
 
 @pytest.mark.parametrize('sms_sender_input, expected_error', [
     ('elevenchars', None),
+    ('11 chars', None),
     ('', 'Can’t be empty'),
     ('abcdefghijkhgkg', 'Enter 11 characters or fewer'),
     (' ¯\_(ツ)_/¯ ', 'Use letters and numbers only'),
