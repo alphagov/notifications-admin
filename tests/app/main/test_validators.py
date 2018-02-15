6bd99e0599d195130dcd699d5da7fb417c3f95e1
Rebecca Law <rebecca.law@digital.cabinet-office.gov.uk>
2018-02-15 16:06:44 +0000

    Added a validation for a minimum of 3 character. If the SMS sender is less than 3 characters one of our providers will return an error.

--- tests/app/main/test_validators.py
+++ tests/app/main/test_validators.py
@@ -206,3 +206,7 @@ def test_sms_sender_form_validation(
     form.sms_sender.data = '###########'
     form.validate()
     assert 'Use letters and numbers only' == form.errors['sms_sender'][0]
+
+    form.sms_sender.data = '0'
+    form.validate()
+    assert 'Enter 3 characters or more' == form.errors['sms_sender'][0]
