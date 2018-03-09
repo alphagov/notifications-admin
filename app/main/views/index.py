7828afe0d78aa13667710df7219a91d13eec898b
Athanasios Voutsadakis <athanasios.voutsadakis@digital.cabinet-office.gov.uk>
2018-03-09 11:56:34 +0000

    Sort requirements

--- app/main/views/index.py
+++ app/main/views/index.py
@@ -1,4 +1,4 @@
-from flask import redirect, render_template, request, url_for, abort
+from flask import abort, redirect, render_template, request, url_for
 from flask_login import current_user, login_required
 from notifications_utils.international_billing_rates import (
     INTERNATIONAL_BILLING_RATES,
