2fab3632535863f4fa1bb6b52a7c9aaef26f5697
Pea Tyczynska <pea.tyczynska@digital.cabinet-office.gov.uk>
2018-07-16 17:36:34 +0100

    Link to user services on User Information page

--- tests/app/main/views/test_find_users.py
+++ tests/app/main/views/test_find_users.py
@@ -115,8 +115,8 @@ def test_user_information_page_shows_information_about_user(
     assert document.xpath("//p/text()[normalize-space()='+447700900986']")
 
     assert document.xpath("//h2/text()[normalize-space()='Services']")
-    assert document.xpath("//p/text()[normalize-space()='Fresh Orchard Juice']")
-    assert document.xpath("//p/text()[normalize-space()='Nature Therapy']")
+    assert document.xpath("//a/text()[normalize-space()='Fresh Orchard Juice']")
+    assert document.xpath("//a/text()[normalize-space()='Nature Therapy']")
 
     assert document.xpath("//h2/text()[normalize-space()='Last login']")
     assert not document.xpath("//p/text()[normalize-space()='0 failed login attempts']")
