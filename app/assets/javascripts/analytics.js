8e6efadb235cae195be203574008a86899ea5131
Chris Hill-Scott <me@quis.cc>
2018-08-02 12:33:13 +0100

    Track virtual page view when enabling basic view
    So we can see what pages people go to after turning it on.

--- app/assets/javascripts/analytics.js
+++ app/assets/javascripts/analytics.js
@@ -1,11 +1,21 @@
+let stripUUIDs = string => string.replace(
+  /[a-f0-9]{8}-?[a-f0-9]{4}-?4[a-f0-9]{3}-?[89ab][a-f0-9]{3}-?[a-f0-9]{12}/g, '…'
+);
+
 (function(Modules) {
   "use strict";
 
-  if (!ga) return;
-
   function sendEvent(category, action, label) {
 
+    if (!ga) return;
     ga('send', 'event', category, action, label);
+
+  }
+
+  function sendVirtualPageView(path) {
+
+    if (!ga) return;
+    ga('send', 'pageview', stripUUIDs('/virtual' + path));
 
   }
 
@@ -26,6 +36,20 @@
       $(component).data('event-action'),
       $(component).data('event-label')
     );
+
+  };
+
+  Modules.TrackFormSubmission = function() {
+
+    this.start = component => {
+
+      $(component).on('submit', function() {
+
+        let formData = $('input[name!=csrf_token]', this).serialize();
+        sendVirtualPageView(window.location.pathname + '?' + formData);
+
+      });
+    };
 
   };
 
