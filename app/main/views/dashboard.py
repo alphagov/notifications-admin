4188c0297d9983980e49df2ba5eb78d462e80e56
Katie Smith <katie.smith@digital.cabinet-office.gov.uk>
2018-10-03 11:18:46 +0100

    Display letter postage on the usage page
    The monthly letter cost was already broken down by price on the usage
    page, but this change adds postage details to the usage page too.

--- app/main/views/dashboard.py
+++ app/main/views/dashboard.py
@@ -435,8 +435,12 @@ def get_free_paid_breakdown_for_billable_units(year, free_sms_fragment_limit, bi
             free_sms_fragment_limit, cumulative, previous_cumulative,
             [billing_month for billing_month in sms_units if billing_month['month'] == month]
         )
-        letter_billing = [(x['billing_units'], x['rate'], (x['billing_units'] * x['rate']))
+        letter_billing = [(x['billing_units'], x['rate'], (x['billing_units'] * x['rate']), x['postage'])
                           for x in letter_units if x['month'] == month]
+
+        if letter_billing:
+            letter_billing.sort(key=lambda x: (x[3], x[1]))
+
         letter_total = 0
         for x in letter_billing:
             letter_total += x[2]
