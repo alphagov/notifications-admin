23d00aacfdfe1fb51d3ed847fbf7815aa33d9ef1
Chris Hill-Scott <me@quis.cc>
2018-12-21 10:40:00 +0000

    Remove WhiteNoise
    We’re now serving assets from S3 when running on PaaS. So we’ll set
    the appropriate caching headers there or in Cloudfront. This means that
    the app no longer needs to serve cache headers, which is what we were
    using WhiteNoise for.
    
    This commits removes WhiteNoise in favour of letting Flask handle the
    serving of static assets.

--- application.py
+++ application.py
@@ -1,16 +1,6 @@
-import os
-
 from flask import Flask
-from whitenoise import WhiteNoise
-
 from app import create_app
 
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'app', 'static')
-STATIC_URL = 'static/'
+application = Flask('app')
 
-flask_app = Flask('app')
-
-create_app(flask_app)
-
-application = WhiteNoise(flask_app.wsgi_app, STATIC_ROOT, STATIC_URL, max_age=WhiteNoise.FOREVER)
+create_app(application)
