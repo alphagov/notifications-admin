bb1e9d2a0f183b8f987b5ef299ba189c0bfbc97e
Pea Tyczynska <pea.tyczynska@digital.cabinet-office.gov.uk>
2019-01-10 15:54:31 +0000

    Add validation-failed status to FAILURE_STATUSES in admin utils

--- app/utils.py
+++ app/utils.py
@@ -40,7 +40,7 @@ from werkzeug.datastructures import MultiDict
 SENDING_STATUSES = ['created', 'pending', 'sending', 'pending-virus-check']
 DELIVERED_STATUSES = ['delivered', 'sent', 'returned-letter']
 FAILURE_STATUSES = ['failed', 'temporary-failure', 'permanent-failure',
-                    'technical-failure', 'virus-scan-failed']
+                    'technical-failure', 'virus-scan-failed', 'validation-failed']
 REQUESTED_STATUSES = SENDING_STATUSES + DELIVERED_STATUSES + FAILURE_STATUSES
 
 
