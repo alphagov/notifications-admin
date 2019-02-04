ca6529dcdb523f0a5a73935dab53767cf4e49850
Chris Hill-Scott <me@quis.cc>
2019-02-04 15:17:27 +0000

    Update app/assets/javascripts/templateFolderForm.js
    Use native `Array.indexOf` instead.
    
    Co-Authored-By: tombye <tombaromba@gmail.com>

--- app/assets/javascripts/templateFolderForm.js
+++ app/assets/javascripts/templateFolderForm.js
@@ -147,7 +147,7 @@
       );
 
       // use dialog mode for states which contain more than one form control
-      if ($.inArray(this.currentState, ['move-to-existing-folder', 'add-new-template']) !== -1) {
+      if (['move-to-existing-folder', 'add-new-template'].indexOf(this.currentState) !== -1) {
         mode = 'dialog';
       }
       GOVUK.stickAtBottomWhenScrolling.setMode(mode);
