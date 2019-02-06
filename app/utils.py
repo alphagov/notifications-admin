f2c7be57a49f926fbaebddd534132decdd24d7de
Chris Hill-Scott <me@quis.cc>
2019-02-06 14:54:58 +0000

    Stop referring to service.postage
    All templates have postage now and we will be getting rid of
    `service.postage`.

--- app/utils.py
+++ app/utils.py
@@ -332,7 +332,7 @@ def get_template(
                 image_url=letter_preview_url,
                 page_count=int(page_count),
                 contact_block=template['reply_to_text'],
-                postage=template.get('postage') or service.postage,
+                postage=template['postage'],
             )
         else:
             return LetterPreviewTemplate(
