0e3d47bc08bd01c87bea016d0ffc19bead85b18d
Leo Hemsted <leo.hemsted@digital.cabinet-office.gov.uk>
2019-02-11 18:12:22 +0000

    use letter branding object instead of letter_logo_filename
    This is part of migrating away from dvla organisations. This only
    affects in-app previews and not actual letters printed by the api.

--- app/template_previews.py
+++ app/template_previews.py
@@ -11,7 +11,7 @@ class TemplatePreview:
             'letter_contact_block': template.get('reply_to_text', ''),
             'template': template,
             'values': values,
-            'filename': current_service.letter_logo_filename,
+            'filename': current_service.letter_branding and current_service.letter_branding['filename']
         }
         resp = requests.post(
             '{}/preview.{}{}'.format(
