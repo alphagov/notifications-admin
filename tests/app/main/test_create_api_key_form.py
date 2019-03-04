edc3aeded4c4fb130ef7cb708b8d924945582540
Chris Hill-Scott <me@quis.cc>
2019-03-04 19:50:00 +0000

    Allow replacing a revoked key
    We had someone wondering why they couldn’t do this. Seems legit to allow
    it.

--- tests/app/main/test_create_api_key_form.py
+++ tests/app/main/test_create_api_key_form.py
@@ -4,17 +4,34 @@ from werkzeug.datastructures import MultiDict
 from app.main.forms import CreateKeyForm
 
 
-def test_return_validation_error_when_key_name_exists(client):
+@pytest.mark.parametrize('expiry_date, expected_errors', (
+    (None, ['A key with this name already exists']),
+    ('2001-01-01 01:01:01', None),
+))
+def test_return_validation_error_when_key_name_exists(
+    client,
+    expiry_date,
+    expected_errors,
+):
     _existing_keys = [
-        {'name': 'some key'},
-        {'name': 'another key'},
+        {
+            'name': 'some key',
+            'expiry_date': expiry_date,
+        },
+        {
+            'name': 'another key',
+            'expiry_date': None,
+        },
     ]
 
-    form = CreateKeyForm(_existing_keys,
-                         formdata=MultiDict([('key_name', 'Some key')]))
+    form = CreateKeyForm(
+        _existing_keys,
+        formdata=MultiDict([('key_name', 'Some key')])
+    )
+
     form.key_type.choices = [('a', 'a'), ('b', 'b')]
     form.validate()
-    assert form.errors['key_name'] == ['A key with this name already exists']
+    assert form.errors.get('key_name') == expected_errors
 
 
 @pytest.mark.parametrize(
