c3a1e6ed59e011791439938cfea2858ec751b9ed
Tom Byers <tombaromba@gmail.com>
2019-04-01 09:58:13 +0100

    Bump Gulp to 4.0.0
    Brings in a new API which is a breaking change:
    
    https://github.com/gulpjs/gulp/blob/master/CHANGELOG.md#400
    
    Requires gulpfile.babel.js to be a CommonJS module
    and for all tasks to be exported.
    
    Replaces using ES6 modules for the CommonJS to
    include other packages to match the type of module
    the gulpfile now is.
    
    Updates to 4.0.0 include `series` and `parallel`
    methods which remove the need for the
    `run-sequence` package.
    
    Also bumps gulp-specific libraries.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -5,178 +5,181 @@
 
 // 1. LIBRARIES
 // - - - - - - - - - - - - - - -
-import gulp from 'gulp';
-import loadPlugins from 'gulp-load-plugins';
-import stylish from 'jshint-stylish';
-import runSequence from 'run-sequence';
+const { src, pipe, dest, series, parallel } = require('gulp');
+const loadPlugins = require('gulp-load-plugins');
+const stylish = require('jshint-stylish');
 
-const plugins = loadPlugins(),
+const plugins = loadPlugins();
 
 // 2. CONFIGURATION
 // - - - - - - - - - - - - - - -
-    paths = {
-        src: 'app/assets/',
-        dist: 'app/static/',
-        templates: 'app/templates/',
-        npm: 'node_modules/',
-        template: 'node_modules/govuk_template_jinja/',
-        toolkit: 'node_modules/govuk_frontend_toolkit/'
-    };
+const paths = {
+  src: 'app/assets/',
+  dist: 'app/static/',
+  templates: 'app/templates/',
+  npm: 'node_modules/',
+  template: 'node_modules/govuk_template_jinja/',
+  toolkit: 'node_modules/govuk_frontend_toolkit/'
+};
 
 // 3. TASKS
 // - - - - - - - - - - - - - - -
 
 // Move GOV.UK template resources
 
-gulp.task('copy:govuk_template:template', () => gulp.src(paths.template + 'views/layouts/govuk_template.html')
-  .pipe(gulp.dest(paths.templates))
-);
+const copy = {
+  govuk_template: {
+    template: () => {
+      return src(paths.template + 'views/layouts/govuk_template.html')
+       .pipe(dest(paths.templates));
+    },
+    css: () => {
+      return src(paths.template + 'assets/stylesheets/**/*.css')
+        .pipe(plugins.sass({
+          outputStyle: 'compressed'
+        }))
+        .on('error', plugins.sass.logError)
+        .pipe(plugins.cssUrlAdjuster({
+          prependRelative: process.env.NOTIFY_ENVIRONMENT == 'development' ? '/static/' : '/',
+        }))
+        .pipe(dest(paths.dist + 'stylesheets/'));
+    },
+    js: () => {
+      return src(paths.template + 'assets/javascripts/**/*.js')
+        .pipe(plugins.uglify())
+        .pipe(dest(paths.dist + 'javascripts/'));
+    },
+    images: () => {
+      return src(paths.template + 'assets/stylesheets/images/**/*')
+        .pipe(dest(paths.dist + 'images/'));
+    },
+    fonts: () => {
+      return src(paths.template + 'assets/stylesheets/fonts/**/*')
+        .pipe(dest(paths.dist + 'fonts/'));
+    },
+    error_page: () => {
+      return src(paths.src + 'error_pages/**/*')
+        .pipe(dest(paths.dist + 'error_pages/'))
+    }
+  }
+};
 
-gulp.task('copy:govuk_template:css', () => gulp.src(paths.template + 'assets/stylesheets/**/*.css')
-  .pipe(plugins.sass({
-    outputStyle: 'compressed'
-  }))
-  .on('error', plugins.sass.logError)
-  .pipe(plugins.cssUrlAdjuster({
-    prependRelative: process.env.NOTIFY_ENVIRONMENT == 'development' ? '/static/' : '/',
-  }))
-  .pipe(gulp.dest(paths.dist + 'stylesheets/'))
-);
 
-gulp.task('copy:govuk_template:js', () => gulp.src(paths.template + 'assets/javascripts/**/*.js')
-  .pipe(plugins.uglify())
-  .pipe(gulp.dest(paths.dist + 'javascripts/'))
-);
+const javascripts = () => {
+  return src([
+      paths.toolkit + 'javascripts/govuk/modules.js',
+      paths.toolkit + 'javascripts/govuk/show-hide-content.js',
+      paths.src + 'javascripts/stick-to-window-when-scrolling.js',
+      paths.src + 'javascripts/detailsPolyfill.js',
+      paths.src + 'javascripts/apiKey.js',
+      paths.src + 'javascripts/autofocus.js',
+      paths.src + 'javascripts/highlightTags.js',
+      paths.src + 'javascripts/fileUpload.js',
+      paths.src + 'javascripts/expandCollapse.js',
+      paths.src + 'javascripts/radioSelect.js',
+      paths.src + 'javascripts/updateContent.js',
+      paths.src + 'javascripts/listEntry.js',
+      paths.src + 'javascripts/liveSearch.js',
+      paths.src + 'javascripts/errorTracking.js',
+      paths.src + 'javascripts/preventDuplicateFormSubmissions.js',
+      paths.src + 'javascripts/fullscreenTable.js',
+      paths.src + 'javascripts/previewPane.js',
+      paths.src + 'javascripts/colourPreview.js',
+      paths.src + 'javascripts/templateFolderForm.js',
+      paths.src + 'javascripts/main.js'
+    ])
+    .pipe(plugins.prettyerror())
+    .pipe(plugins.babel({
+      presets: ['es2015']
+    }))
+    .pipe(plugins.addSrc.prepend([
+      paths.npm + 'hogan.js/dist/hogan-3.0.2.js',
+      paths.npm + 'jquery/dist/jquery.min.js',
+      paths.npm + 'query-command-supported/dist/queryCommandSupported.min.js',
+      paths.npm + 'diff-dom/diffDOM.js',
+      paths.npm + 'timeago/jquery.timeago.js',
+      paths.npm + 'textarea-caret/index.js'
+    ]))
+    .pipe(plugins.uglify())
+    .pipe(plugins.concat('all.js'))
+    .pipe(dest(paths.dist + 'javascripts/'))
+};
 
-gulp.task('copy:govuk_template:images', () => gulp.src(paths.template + 'assets/stylesheets/images/**/*')
-  .pipe(gulp.dest(paths.dist + 'images/'))
-);
 
-gulp.task('copy:govuk_template:fonts', () => gulp.src(paths.template + 'assets/stylesheets/fonts/**/*')
-  .pipe(gulp.dest(paths.dist + 'fonts/'))
-);
-
-gulp.task('javascripts', () => gulp
-  .src([
-    paths.toolkit + 'javascripts/govuk/modules.js',
-    paths.toolkit + 'javascripts/govuk/show-hide-content.js',
-    paths.src + 'javascripts/stick-to-window-when-scrolling.js',
-    paths.src + 'javascripts/detailsPolyfill.js',
-    paths.src + 'javascripts/apiKey.js',
-    paths.src + 'javascripts/autofocus.js',
-    paths.src + 'javascripts/highlightTags.js',
-    paths.src + 'javascripts/fileUpload.js',
-    paths.src + 'javascripts/expandCollapse.js',
-    paths.src + 'javascripts/radioSelect.js',
-    paths.src + 'javascripts/updateContent.js',
-    paths.src + 'javascripts/listEntry.js',
-    paths.src + 'javascripts/liveSearch.js',
-    paths.src + 'javascripts/errorTracking.js',
-    paths.src + 'javascripts/preventDuplicateFormSubmissions.js',
-    paths.src + 'javascripts/fullscreenTable.js',
-    paths.src + 'javascripts/previewPane.js',
-    paths.src + 'javascripts/colourPreview.js',
-    paths.src + 'javascripts/templateFolderForm.js',
-    paths.src + 'javascripts/main.js'
-  ])
-  .pipe(plugins.prettyerror())
-  .pipe(plugins.babel({
-    presets: ['es2015']
-  }))
-  .pipe(plugins.addSrc.prepend([
-    paths.npm + 'hogan.js/dist/hogan-3.0.2.js',
-    paths.npm + 'jquery/dist/jquery.min.js',
-    paths.npm + 'query-command-supported/dist/queryCommandSupported.min.js',
-    paths.npm + 'diff-dom/diffDOM.js',
-    paths.npm + 'timeago/jquery.timeago.js',
-    paths.npm + 'textarea-caret/index.js'
-  ]))
-  .pipe(plugins.uglify())
-  .pipe(plugins.concat('all.js'))
-  .pipe(gulp.dest(paths.dist + 'javascripts/'))
-);
-
-gulp.task('sass', () => gulp
-  .src(paths.src + '/stylesheets/main*.scss')
-  .pipe(plugins.prettyerror())
-  .pipe(plugins.sass({
-    outputStyle: 'compressed',
-    includePaths: [
-      paths.npm + 'govuk-elements-sass/public/sass/',
-      paths.toolkit + 'stylesheets/'
-    ]
-  }))
-  .pipe(plugins.base64({baseDir: 'app'}))
-  .pipe(gulp.dest(paths.dist + 'stylesheets/'))
-);
+const sass = () => {
+  return src(paths.src + '/stylesheets/main*.scss')
+    .pipe(plugins.prettyerror())
+    .pipe(plugins.sass({
+      outputStyle: 'compressed',
+      includePaths: [
+        paths.npm + 'govuk-elements-sass/public/sass/',
+        paths.toolkit + 'stylesheets/'
+      ]
+    }))
+    .pipe(plugins.base64({baseDir: 'app'}))
+    .pipe(dest(paths.dist + 'stylesheets/'))
+};
 
 
 // Copy images
 
-gulp.task('images', () => gulp
-  .src([
-    paths.src + 'images/**/*',
-    paths.toolkit + 'images/**/*',
-    paths.template + 'assets/images/**/*'
-  ])
-  .pipe(gulp.dest(paths.dist + 'images/'))
-);
-
-gulp.task('copy:govuk_template:error_page', () => gulp.src(paths.src + 'error_pages/**/*')
-  .pipe(gulp.dest(paths.dist + 'error_pages/'))
-);
+const images = () => {
+  return src([
+      paths.src + 'images/**/*',
+      paths.toolkit + 'images/**/*',
+      paths.template + 'assets/images/**/*'
+    ])
+    .pipe(dest(paths.dist + 'images/'))
+};
 
 
 // Watch for changes and re-run tasks
-gulp.task('watchForChanges', function() {
-  gulp.watch(paths.src + 'javascripts/**/*', ['javascripts']);
-  gulp.watch(paths.src + 'stylesheets/**/*', ['sass']);
-  gulp.watch(paths.src + 'images/**/*', ['images']);
-  gulp.watch('gulpfile.babel.js', ['default']);
-});
+const watchForChanges = () => {
+  return watch(paths.src + 'javascripts/**/*', ['javascripts'])
+    .watch(paths.src + 'stylesheets/**/*', ['sass'])
+    .watch(paths.src + 'images/**/*', ['images'])
+    .watch('gulpfile.babel.js', ['default']);
+};
 
-gulp.task('lint:sass', () => gulp
-  .src([
-    paths.src + 'stylesheets/*.scss',
-    paths.src + 'stylesheets/components/*.scss',
-    paths.src + 'stylesheets/views/*.scss',
-  ])
-    .pipe(plugins.sassLint())
-    .pipe(plugins.sassLint.format(stylish))
-    .pipe(plugins.sassLint.failOnError())
-);
-
-gulp.task('lint:js', () => gulp
-  .src(paths.src + 'javascripts/**/*.js')
-    .pipe(plugins.jshint())
-    .pipe(plugins.jshint.reporter(stylish))
-    .pipe(plugins.jshint.reporter('fail'))
-);
-
-gulp.task('lint',
-  ['lint:sass', 'lint:js']
-);
+const lint = {
+  'sass': () => {
+    return src([
+        paths.src + 'stylesheets/*.scss',
+        paths.src + 'stylesheets/components/*.scss',
+        paths.src + 'stylesheets/views/*.scss',
+      ])
+      .pipe(plugins.sassLint())
+      .pipe(plugins.sassLint.format(stylish))
+      .pipe(plugins.sassLint.failOnError());
+  },
+  'js': (cb) => {
+    return src(paths.src + 'javascripts/**/*.js')
+      .pipe(plugins.jshint())
+      .pipe(plugins.jshint.reporter(stylish))
+      .pipe(plugins.jshint.reporter('fail'))
+  }
+};
 
 // Default: compile everything
-gulp.task('default', function() {
-  runSequence(
-    [
-      'copy:govuk_template:template',
-      'copy:govuk_template:images',
-      'copy:govuk_template:fonts',
-      'copy:govuk_template:css',
-      'copy:govuk_template:js',
-      'images',
-    ],
-    [
-      'copy:govuk_template:error_page',
-      'javascripts',
-      'sass'
-    ]
-  );
-});
+const defaultTask = parallel(
+  series(
+    copy.govuk_template.template,
+    copy.govuk_template.images,
+    copy.govuk_template.fonts,
+    copy.govuk_template.css,
+    copy.govuk_template.js,
+    images
+  ),
+  series(
+    copy.govuk_template.error_page,
+    javascripts,
+    sass
+  )
+);
+
+exports.default = defaultTask;
+
+exports.lint = series(lint.sass, lint.js);
 
 // Optional: recompile on changes
-gulp.task('watch',
-    ['default', 'watchForChanges']
-);
+exports.watch = series(defaultTask, watchForChanges);
