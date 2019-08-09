877c8be22dcb7c1d750e01ef127404ffe5bbeefe
Tom Byers <tombaromba@gmail.com>
2019-08-09 10:14:55 +0100

    Add helper for mocking all attribute gets/sets
    Attributes should be accessed with
    `getAttribute`/`setAttribute` but code can also
    use the legacy method of using the property
    directly on the node, ie
    
    `element.checked = true`
    
    This provides a helper to allow tracking of calls
    to both in one place.

--- tests/javascripts/support/helpers.js
+++ tests/javascripts/support/helpers.js
@@ -99,6 +99,36 @@ function activateRadioWithSpace (el) {
 
 };
 
+class MockAttribute {
+
+  constructor (jest, el, attr) {
+    this._jest = jest;
+    this._el = el;
+    this._attr = attr;
+    this.spies = {};
+    this._mockAPI();
+  }
+
+  _mockAPI () {
+
+    // track calls to get/setAttribute
+    this.spies.getAttribute = this._jest.spyOn(this._el, 'getAttribute');
+    this.spies.setAttribute = this._jest.spyOn(this._el, 'setAttribute');
+
+    // proxy calls to legacy getters/setters
+    this.spies.get = this._jest.spyOn(this._el, this._attr, 'get').mockImplementation(() => this._el.getAttribute(this._attr));
+    this.spies.set = this._jest.spyOn(this._el, this._attr, 'set').mockImplementation(value => this._el.setAttribute(this._attr, value));
+
+  }
+
+  reset () {
+
+    Object.keys(this.spies).forEach(key => this.spies[key].mockClear());
+
+  }
+
+}
+
 class ElementQuery {
   constructor (el) {
     this.el = el;
@@ -237,4 +267,5 @@ exports.clickElementWithMouse = clickElementWithMouse;
 exports.moveSelectionToRadio = moveSelectionToRadio;
 exports.activateRadioWithSpace = activateRadioWithSpace;
 exports.element = element;
+exports.MockAttribute = MockAttribute;
 exports.WindowMock = WindowMock;
