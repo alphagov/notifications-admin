c57741686653866930b209bf7a5696dcc5d870a1
Katie Smith <katie.smith@digital.cabinet-office.gov.uk>
2019-09-06 10:39:23 +0100

    Add letter upload form which redirects to blank preview page
    Added a form to upload a single letter. Currently this only uses the
    form to validate that a file is submitted and that the file is a PDF. If
    either of these validations fail, the form will display an error.
    Otherwise, we redirect to a new preview page which just has the filename
    as the heading for now.

--- app/main/views/uploads.py
+++ app/main/views/uploads.py
@@ -1,6 +1,10 @@
-from flask import render_template
+import uuid
 
+from flask import redirect, render_template, request, url_for
+
+from app import current_service
 from app.main import main
+from app.main.forms import PDFUploadForm
 from app.utils import user_has_permissions
 
 
@@ -8,3 +12,31 @@ from app.utils import user_has_permissions
 @user_has_permissions('send_messages')
 def uploads(service_id):
     return render_template('views/uploads/index.html')
+
+
+@main.route("/services/<service_id>/upload-letter", methods=['GET', 'POST'])
+@user_has_permissions('send_messages')
+def upload_letter(service_id):
+    form = PDFUploadForm()
+
+    if form.validate_on_submit():
+        upload_id = uuid.uuid4()
+
+        return redirect(
+            url_for(
+                'main.uploaded_letter_preview',
+                service_id=current_service.id,
+                file_id=upload_id,
+                original_filename=form.file.data.filename,
+            )
+        )
+
+    return render_template('views/uploads/choose-file.html', form=form)
+
+
+@main.route("/services/<service_id>/preview-letter/<file_id>")
+@user_has_permissions('send_messages')
+def uploaded_letter_preview(service_id, file_id):
+    original_filename = request.args.get('original_filename')
+
+    return render_template('views/uploads/preview.html', original_filename=original_filename)
--- tests/app/main/views/test_uploads.py
+++ tests/app/main/views/test_uploads.py
@@ -1,5 +1,70 @@
+from flask import url_for
+
 from tests.conftest import SERVICE_ONE_ID
 
 
 def test_get_upload_hub_page(client_request):
-    client_request.get('main.uploads', service_id=SERVICE_ONE_ID)
+    page = client_request.get('main.uploads', service_id=SERVICE_ONE_ID)
+
+    assert page.find('h1').text == 'Uploads'
+    assert page.find('a', text='Upload a letter').attrs['href'] == url_for(
+        'main.upload_letter', service_id=SERVICE_ONE_ID
+    )
+
+
+def test_get_upload_letter(client_request):
+    page = client_request.get('main.upload_letter', service_id=SERVICE_ONE_ID)
+
+    assert page.find('h1').text == 'Upload a letter'
+    assert page.find('input', class_='file-upload-field')
+    assert page.select('button[type=submit]')
+
+
+def test_post_upload_letter_redirects_for_valid_file(mocker, client_request):
+    mocker.patch('uuid.uuid4', return_value='fake-uuid')
+
+    with open('tests/test_pdf_files/one_page_pdf.pdf', 'rb') as file:
+        client_request.post(
+            'main.upload_letter',
+            service_id=SERVICE_ONE_ID,
+            _data={'file': file},
+            _expected_redirect=url_for(
+                'main.uploaded_letter_preview',
+                service_id=SERVICE_ONE_ID,
+                file_id='fake-uuid',
+                original_filename='tests/test_pdf_files/one_page_pdf.pdf',
+                _external=True
+            )
+        )
+
+
+def test_post_upload_letter_shows_error_when_file_is_not_a_pdf(client_request):
+    with open('tests/non_spreadsheet_files/actually_a_png.csv', 'rb') as file:
+        page = client_request.post(
+            'main.upload_letter',
+            service_id=SERVICE_ONE_ID,
+            _data={'file': file},
+            _expected_status=200
+        )
+    assert page.find('span', class_='error-message').text.strip() == "Letters must be saved as a PDF"
+
+
+def test_post_upload_letter_shows_error_when_no_file_uploaded(client_request):
+    page = client_request.post(
+        'main.upload_letter',
+        service_id=SERVICE_ONE_ID,
+        _data={'file': ''},
+        _expected_status=200
+    )
+    assert page.find('span', class_='error-message').text.strip() == "You need to upload a file to submit"
+
+
+def test_uploaded_letter_preview(client_request):
+    page = client_request.get(
+        'main.uploaded_letter_preview',
+        service_id=SERVICE_ONE_ID,
+        file_id='fake-uuid',
+        original_filename='my_letter.pdf',
+    )
+
+    assert page.find('h1').text == 'my_letter.pdf'
