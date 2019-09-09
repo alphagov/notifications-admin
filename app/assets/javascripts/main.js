04afb322a3c376dafda67174b0b0467423b459fe
Chris Hill-Scott <me@quis.cc>
2019-09-09 17:00:22 +0100

    Hacky defocus banner to prevent flash

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -10,4 +10,4 @@ $(() => GOVUK.modules.start());
 
 $(() => $('.error-message').eq(0).parent('label').next('input').trigger('focus'));
 
-$(() => $('.banner-dangerous').eq(0).trigger('focus'));
+// $(() => $('.banner-dangerous').eq(0).trigger('focus'));
