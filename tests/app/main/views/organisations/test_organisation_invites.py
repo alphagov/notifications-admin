1e03a2668b318f6b250869a22ccb6defe309d158
karlchillmaid <karl.chillmaid@digital.cabinet-office.gov.uk>
2019-09-13 10:16:16 +0100

    Replace can't with cannot

--- tests/app/main/views/organisations/test_organisation_invites.py
+++ tests/app/main/views/organisations/test_organisation_invites.py
@@ -80,7 +80,7 @@ def test_invite_org_user_errors_when_same_email_as_inviter(
     )
 
     assert mock_invite_org_user.called is False
-    assert normalize_spaces(page.select_one('.error-message').text) == 'You can’t send an invitation to yourself'
+    assert normalize_spaces(page.select_one('.error-message').text) == 'You cannot send an invitation to yourself'
 
 
 def test_accepted_invite_when_other_user_already_logged_in(
