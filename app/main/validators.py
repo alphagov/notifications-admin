4b2990e7359a4a0763da9b94ea8a626f1b37f5b7
karlchillmaid <karl.chillmaid@digital.cabinet-office.gov.uk>
2019-09-13 11:55:21 +0100

    Replace isn't with is not

--- app/main/validators.py
+++ app/main/validators.py
@@ -32,7 +32,7 @@ class CsvFileValidator:
 
     def __call__(self, form, field):
         if not Spreadsheet.can_handle(field.data.filename):
-            raise ValidationError("{} isn’t a spreadsheet that Notify can read".format(field.data.filename))
+            raise ValidationError("{} is not a spreadsheet that Notify can read".format(field.data.filename))
 
 
 class ValidGovEmail:
