e02f94f2385a489a86f43b30b93a721d8d4c318d
Tom Byers <tombaromba@gmail.com>
2019-11-25 10:37:35 +0000

    Revert "Replace govuk template with govuk frontend components"

--- app/navigation.py
+++ app/navigation.py
@@ -328,10 +328,6 @@ class HeaderNavigation(Navigation):
         'whitelist',
     }
 
-    # header HTML now comes from GOVUK Frontend so requires a boolean, not an attribute
-    def is_selected(self, navigation_item):
-        return request.endpoint in self.mapping[navigation_item]
-
 
 class MainNavigation(Navigation):
 
