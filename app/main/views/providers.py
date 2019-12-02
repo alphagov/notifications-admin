16ebdfeb8b36fa0966bb1b418c6143fc127c07c0
Chris Hill-Scott <me@quis.cc>
2019-12-02 14:29:34 +0000

    Add page showing priority of two SMS providers
    It’s not very useful to know the priority of one provider without
    knowing the other. And these pages were never really designed, so they
    weren’t super easy to understand anyway.
    
    This commit adds a page that takes the first two text message providers
    and shows their relative priority against each other.
    
    It follows the design of the events page, as a pattern for showing a
    log of historical events.

--- app/main/views/providers.py
+++ app/main/views/providers.py
@@ -1,10 +1,16 @@
-from flask import render_template, url_for
+from collections import defaultdict
+from datetime import datetime
+from operator import itemgetter
+
+from flask import abort, render_template, url_for
 from werkzeug.utils import redirect
 
-from app import provider_client
+from app import format_date_numeric, provider_client
 from app.main import main
 from app.main.forms import ProviderForm
 from app.utils import user_is_platform_admin
+
+PROVIDER_PRIORITY_MEANING_SWITCHOVER = datetime(2019, 11, 29, 11, 0)
 
 
 @main.route("/providers")
@@ -49,6 +55,84 @@ def edit_provider(provider_id):
         return redirect(url_for('.view_providers'))
 
     return render_template('views/providers/edit-provider.html', form=form, provider=provider)
+
+
+@main.route("/provider/edit-sms-provider-ratio", methods=['GET', 'POST'])
+@user_is_platform_admin
+def edit_sms_provider_ratio():
+
+    providers = sorted([
+        provider
+        for provider in provider_client.get_all_providers()['provider_details']
+        if provider['notification_type'] == 'sms'
+    ], key=itemgetter('identifier'), reverse=True)
+
+    if len(providers) < 2:
+        abort(400)
+
+    return render_template(
+        'views/providers/edit-sms-provider-ratio.html',
+        versions=_chunk_versions_by_day(_get_versions(providers[0], providers[1])),
+    )
+
+
+def _get_versions(provider0, provider1):
+
+    versions = sorted((
+        provider_client.get_provider_versions(provider0['id'])['data'] +
+        provider_client.get_provider_versions(provider1['id'])['data']
+    ), key=lambda version: version['updated_at'] or '', reverse=True)
+
+    for index, version in enumerate(versions):
+
+        previous_version = get_previous_version(
+            versions,
+            index,
+            version['identifier'],
+        )
+
+        if (
+            (version['updated_at'] or '0') < str(PROVIDER_PRIORITY_MEANING_SWITCHOVER)
+        ):
+            version['priority'], previous_version['priority'] = (
+                int(version['priority'] > previous_version['priority']),
+                int(version['priority'] <= previous_version['priority'])
+            )
+
+        if version['identifier'] == provider0['identifier']:
+            fresh_version = version.copy()
+            fresh_version['other_provider'] = previous_version.copy()
+            yield fresh_version
+        elif previous_version['identifier'] in {provider0['identifier'], None}:
+            fresh_version = previous_version.copy()
+            fresh_version['other_provider'] = version.copy()
+            yield fresh_version
+
+
+def get_previous_version(versions, start_index, current_provider_identifier):
+    for index, version in enumerate(versions):
+        if index < start_index:
+            continue
+        if current_provider_identifier == version['identifier']:
+            continue
+        return version
+    return {
+        'identifier': None,
+        'priority': 999,
+        'updated_at': None,
+    }
+
+
+def _chunk_versions_by_day(versions):
+
+    days = defaultdict(list)
+
+    for version in sorted(versions, key=lambda version: version['updated_at'] or '', reverse=True):
+        days[
+            format_date_numeric(version['updated_at']) if version['updated_at'] else ''
+        ].append(version)
+
+    return sorted(days.items(), reverse=True)
 
 
 @main.route("/provider/<uuid:provider_id>")
