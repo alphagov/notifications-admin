0885dde2c18dc6a29e3190f2f82fa52312218e94
Tom Byers <tombaromba@gmail.com>
2020-01-22 11:45:10 +0000

    Update Google Analytics tracker config
    Removes the following fields from the tracker
    config:
    - `name`, which was erroring due to it
      including a `.`
    - `displayFeaturesTask` which seems to be
      deprecated
    
    Also refactors the `create` command to put all
    fields into the options parameter, as shown in the
    developer docs:
    
    https://developers.google.com/analytics/devguides/collection/analyticsjs/creating-trackers

--- app/assets/javascripts/analytics/analytics.js
+++ app/assets/javascripts/analytics/analytics.js
@@ -6,10 +6,14 @@
   // Stripped-down wrapper for Google Analytics, based on:
   // https://github.com/alphagov/static/blob/master/doc/analytics.md
   const Analytics = function (config) {
-    window.ga('create', config.trackingId, config.cookieDomain, config.name, { 'cookieExpires': config.expires * 24 * 60 * 60 });
+    window.ga('create', {
+      'trackingId': config.trackingId,
+      'cookieDomain': config.cookieDomain,
+      'cookieExpires': config.expires * 24 * 60 * 60
+    });
 
     window.ga('set', 'anonymizeIp', config.anonymizeIp);
-    window.ga('set', 'displayFeaturesTask', config.displayFeaturesTask);
+    window.ga('set', 'allowAdFeatures', config.allowAdFeatures);
     window.ga('set', 'transport', config.transport);
 
   };
