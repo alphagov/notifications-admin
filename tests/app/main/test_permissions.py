a35931a33a47de385294042c75bd8cbe89d1671c
Chris Hill-Scott <me@quis.cc>
2020-02-03 11:35:29 +0000

    Swap order of usage and team members for org users
    Org users, when looking at the page for their org, see:
    > Usage
    > Team members
    
    When they click into a service it switches to:
    > Team members
    > Usage
    
    This is jarring. It should stay consistent. I think it that _Usage_ then
    _Team members_ is the natural way of ordering the navigation at the
    organisation level, so let’s follow that through to the service level.
    
    This does mean that if someone is a member of both an organisation and a
    service that the nav will jump (because it’ll switch to the existing,
    service-level order of _Team members_ then _Usage_) but it’s going to
    jump anyway because you get all the extra navigation items when you’re a
    member of a service.

--- tests/app/main/test_permissions.py
+++ tests/app/main/test_permissions.py
@@ -373,8 +373,8 @@ def test_service_navigation_for_org_user(
     assert [
         item.text.strip() for item in page.select('nav.navigation a')
     ] == [
-        'Team members',
         'Usage',
+        'Team members',
     ]
 
 
