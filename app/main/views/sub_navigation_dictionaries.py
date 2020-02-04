5314ba72cc1cc76239a22d97f82e9b09dfc0e555
karlchillmaid <karl.chillmaid@digital.cabinet-office.gov.uk>
2020-02-04 13:48:45 +0000

    Remove 'Upload a letter'
    Remove 'Upload a letter' from navigation because it's not available to all users yet.

--- app/main/views/sub_navigation_dictionaries.py
+++ app/main/views/sub_navigation_dictionaries.py
@@ -76,10 +76,6 @@ def using_notify_nav():
                     "name": "Send files by email",
                     "link": "main.send_files_by_email",
                 },
-                {
-                    "name": "Upload a letter",
-                    "link": "main.upload_a_letter",
-                },
             ]
         },
         {
