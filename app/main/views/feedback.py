34a5ddaf2cae5db0a38a5920751eefbb73f49175
Chris Hill-Scott <me@quis.cc>
2020-03-24 15:54:44 +0000

    Assume email always provided
    There’s no way for a user to submit the form without it now.

--- app/main/views/feedback.py
+++ app/main/views/feedback.py
@@ -111,10 +111,9 @@ def feedback(ticket_type):
         else:
             service_string = ''
 
-        feedback_msg = '{}\n{}{}'.format(
+        feedback_msg = '{}\n{}'.format(
             form.feedback.data,
             service_string,
-            '' if user_email else '{} (no email address supplied)'.format(form.name.data)
         )
 
         zendesk_client.create_ticket(
