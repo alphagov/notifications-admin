2092a045479c5a368285eb857ca7b45260fd13f1
Tom Byers <tombaromba@gmail.com>
2020-05-07 15:44:13 +0100

    Split common checkbox methods off into mixin

--- app/main/forms.py
+++ app/main/forms.py
@@ -542,11 +542,7 @@ BroadcastPermissionsAbstract = type("BroadcastPermissionsAbstract", (StripWhites
 })
 
 
-class govukCheckboxField(BooleanField):
-
-    def __init__(self, label='', validators=None, param_extensions=None, **kwargs):
-        super(govukCheckboxField, self).__init__(label, validators, false_values=None, **kwargs)
-        self.param_extensions = param_extensions
+class govukCheckboxesMixin:
 
     def extend_params(self, params, extensions):
         items = None
@@ -570,6 +566,13 @@ class govukCheckboxField(BooleanField):
                         params['items'].append(items[idx])
                     else:
                         params['items'][idx].update(items[idx])
+
+
+class govukCheckboxField(govukCheckboxesMixin, BooleanField):
+
+    def __init__(self, label='', validators=None, param_extensions=None, **kwargs):
+        super(govukCheckboxField, self).__init__(label, validators, false_values=None, **kwargs)
+        self.param_extensions = param_extensions
 
     # self.__call__ renders the HTML for the field by:
     # 1. delegating to self.meta.render_field which
@@ -610,7 +613,7 @@ class govukCheckboxField(BooleanField):
 
 
 # based on work done by @richardjpope: https://github.com/richardjpope/recourse/blob/master/recourse/forms.py#L6
-class govukCheckboxesField(SelectMultipleField):
+class govukCheckboxesField(govukCheckboxesMixin, SelectMultipleField):
 
     render_as_list = False
 
