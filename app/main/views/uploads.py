1c5e6612640edc33ccd6fc17b4bee6b6f6b1cb11
Chris Hill-Scott <me@quis.cc>
2020-05-19 17:26:55 +0100

    Accept file id in URL, not as form parameter
    In the future we need to get the metadata from the file in order to work
    out what form validation rules should apply (postage is only required
    for UK letters).
    
    To start doing this we need all instances of the app accepting `post`
    requests with the `file_id` in the URL, as well as in the form data (for
    backwards compatibility).

--- app/main/views/uploads.py
+++ app/main/views/uploads.py
@@ -339,13 +339,14 @@ def view_letter_upload_as_preview(service_id, file_id):
 
 
 @main.route("/services/<uuid:service_id>/upload-letter/send", methods=['POST'])
+@main.route("/services/<uuid:service_id>/upload-letter/send/<uuid:file_id>", methods=['POST'])
 @user_has_permissions('send_messages', restrict_admin_usage=True)
-def send_uploaded_letter(service_id):
+def send_uploaded_letter(service_id, file_id=None):
     if not (current_service.has_permission('letter') and current_service.has_permission('upload_letters')):
         abort(403)
 
-    form = LetterUploadPostageForm()
-    file_id = form.file_id.data
+    form = LetterUploadPostageForm(file_id=file_id)
+    file_id = file_id or form.file_id.data
 
     if not form.validate_on_submit():
         return uploaded_letter_preview(service_id, file_id)
