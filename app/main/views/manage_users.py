6edc356c2277f4481d4592b9480f5bbf5d7ac5da
Chris Hill-Scott <me@quis.cc>
2020-06-08 10:58:15 +0100

    Allow invite form to be pre-populated
    At the moment users must be invited to join a service. But this means:
    - users must know that a service already exists
    - they need to know who to ask for an invite
    
    If the user doesn’t know these thing then sometimes they just go ahead
    and set up a new service. Which means they have to get all the way to
    the point of requesting to go live before we tell them that there’s
    already a service with a similar name or purpose.
    
    So we should let users:
    1. discover what other services exist in their organisation
    2. apply to join a service
    3. automatically notify the service managers of their interest
    4. be invited by a service manager
    5. accept the invite
    
    This commit implements step 4. We can just link them to the invite form
    in step 3., but we should make it easy for them to send the invite,
    without having to copy and paste email addresses.
    
    So this commit let the invite form be pre-populated with an existing
    user’s email address.

--- app/main/views/manage_users.py
+++ app/main/views/manage_users.py
@@ -48,8 +48,9 @@ def manage_users(service_id):
 
 
 @main.route("/services/<uuid:service_id>/users/invite", methods=['GET', 'POST'])
+@main.route("/services/<uuid:service_id>/users/invite/<uuid:user_id>", methods=['GET', 'POST'])
 @user_has_permissions('manage_service')
-def invite_user(service_id):
+def invite_user(service_id, user_id=None):
 
     if current_service.has_permission('broadcast'):
         form_class = BroadcastInviteUserForm
@@ -61,6 +62,12 @@ def invite_user(service_id):
         all_template_folders=current_service.all_template_folders,
         folder_permissions=[f['id'] for f in current_service.all_template_folders]
     )
+
+    if user_id:
+        user_to_invite = User.from_id(user_id)
+        form.email_address.data = user_to_invite.email_address
+    else:
+        user_to_invite = None
 
     service_has_email_auth = current_service.has_permission('email_auth')
     if not service_has_email_auth:
@@ -85,6 +92,7 @@ def invite_user(service_id):
         form=form,
         service_has_email_auth=service_has_email_auth,
         mobile_number=True,
+        user_to_invite=user_to_invite,
     )
 
 
