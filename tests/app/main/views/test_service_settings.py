6a49e5bcbdd7e70c60c670c0845a0fc8737d9390
Katie Smith <katie.smith@digital.cabinet-office.gov.uk>
2020-06-12 14:28:18 +0100

    Add international letter rates to pricing page
    This also updates a few pages where we listed the minimum and maximum
    letter price.

--- tests/app/main/views/test_service_settings.py
+++ tests/app/main/views/test_service_settings.py
@@ -3674,7 +3674,7 @@ def test_unknown_channel_404s(
 ), [
     (
         'letter',
-        'It costs between 35p and 81p to send a letter using Notify.',
+        'It costs between 35p and £1.16 to send a letter using Notify.',
         'Send letters',
         ['email', 'sms'],
         'False',
@@ -3683,7 +3683,7 @@ def test_unknown_channel_404s(
     ),
     (
         'letter',
-        'It costs between 35p and 81p to send a letter using Notify.',
+        'It costs between 35p and £1.16 to send a letter using Notify.',
         'Send letters',
         ['email', 'sms', 'letter'],
         'True',
