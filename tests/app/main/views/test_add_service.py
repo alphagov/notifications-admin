d0dd6218f734a68c1accf99e12d99628e3bfa8c3
Pea Tyczynska <pea.tyczynska@digital.cabinet-office.gov.uk>
2020-07-01 11:49:11 +0100

    Show different page for local org users  when adding new service

--- tests/app/main/views/test_add_service.py
+++ tests/app/main/views/test_add_service.py
@@ -88,7 +88,7 @@ def test_show_different_page_if_user_org_type_is_local(
     assert page.select_one('h1').text.strip() == 'About your service'
     assert page.select_one('input[name=name]')['value'] == ''
     assert page.select_one('main .govuk-body').text.strip() == (
-        'Give your service a name that tells users what your'
+        'Give your service a name that tells users what your '
         'messages are about, as well as who they’re from. For example:')
 
 
