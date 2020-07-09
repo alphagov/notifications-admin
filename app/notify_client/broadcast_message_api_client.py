effe24893e0e0a4a33bd2f225104c5ea6484f958
Chris Hill-Scott <me@quis.cc>
2020-07-09 10:33:50 +0100

    Make the broadcast flow talk to the API
    This commit removes the code the puts areas into the session and instead
    creates and then updates a draft broadcast in the database.
    
    This is so we can avoid session-related bugs, and potentially having a
    large session when we start adding personalisation etc.
    
    Once a broadcast is ready to go it is set to `broadcasting` straight
    away with no approval. We’ll revisit this as we learn more about how
    users might want to manage who can create and approve broadcasts.
    
    The tests are a bit light in terms of checking what’s on the page, but
    clicking through the pages is probably good enough for now.

---
+++ app/notify_client/broadcast_message_api_client.py
@@ -0,0 +1,46 @@
+from app.notify_client import NotifyAdminAPIClient, _attach_current_user
+
+
+class BroadcastMessageAPIClient(NotifyAdminAPIClient):
+
+    def create_broadcast_message(
+        self,
+        *,
+        service_id,
+        template_id,
+    ):
+        data = {
+            "service_id": service_id,
+            "template_id": template_id,
+            "personalisation": {},
+        }
+
+        data = _attach_current_user(data)
+
+        broadcast_message = self.post(
+            f'/service/{service_id}/broadcast-message',
+            data=data,
+        )
+
+        return broadcast_message
+
+    def get_broadcast_message(self, *, service_id, broadcast_message_id):
+        return self.get(f'/service/{service_id}/broadcast-message/{broadcast_message_id}')
+
+    def update_broadcast_message(self, *, service_id, broadcast_message_id, data):
+        self.post(
+            f'/service/{service_id}/broadcast-message/{broadcast_message_id}',
+            data=data,
+        )
+
+    def update_broadcast_message_status(self, status, *, service_id, broadcast_message_id):
+        data = _attach_current_user({
+            'status': status,
+        })
+        self.post(
+            f'/service/{service_id}/broadcast-message/{broadcast_message_id}/status',
+            data=data,
+        )
+
+
+broadcast_message_api_client = BroadcastMessageAPIClient()
