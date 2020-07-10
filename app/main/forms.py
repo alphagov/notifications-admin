8fa385220b457d869149e814a870cbc8951d3fd2
Tom Byers <tombaromba@gmail.com>
2020-07-10 10:47:32 +0100

    Convert radios on uploads preview page (basic)

--- app/main/forms.py
+++ app/main/forms.py
@@ -1414,7 +1414,7 @@ class LetterUploadPostageForm(StripWhitespaceForm):
     def show_postage(self):
         return len(self.postage.choices) > 1
 
-    postage = RadioField(
+    postage = GovukRadiosField(
         'Choose the postage for this letter',
         choices=[
             ('first', 'First class post'),
