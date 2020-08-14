08fa1415c44257f680462c3bbdedaa6d05333069
David McDonald <david.mcdonald@digital.cabinet-office.gov.uk>
2020-08-14 14:04:05 +0100

    wip

--- app/cloudfoundry_config.py
+++ app/cloudfoundry_config.py
@@ -3,11 +3,17 @@ Extracts cloudfoundry config from its json and populates the environment variabl
 on local/aws boxes
 """
 
-import json
 import os
+import json
 
 
 def extract_cloudfoundry_config():
-    vcap_application = json.loads(os.environ.get('VCAP_APPLICATION'))
+    vcap_services = json.loads(os.environ['VCAP_SERVICES'])
+    vcap_application = json.loads(os.environ['VCAP_APPLICATION'])
+    set_config_env_vars(vcap_services, vcap_application)
+
+
+def set_config_env_vars(vcap_services, vcap_application):
+    os.environ['REDIS_URL'] = vcap_services['redis'][0]['credentials']['uri']
     os.environ['NOTIFY_ENVIRONMENT'] = vcap_application['space_name']
     os.environ['NOTIFY_LOG_PATH'] = '/home/vcap/logs/app.log'
--- application.py
+++ application.py
@@ -1,3 +1,6 @@
+import eventlet
+eventlet.monkey_patch()
+
 from flask import Flask
 from app import create_app
 
