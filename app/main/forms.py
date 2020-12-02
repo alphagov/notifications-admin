uk-gov-mirror/alphagov.notifications-admin

Convert radios on set inbound page (basic)

app/main/forms.py
@@ -1682,7 +1682,7 @@ class ServiceInboundNumberForm(StripWhitespaceForm):
         super().__init__(*args, **kwargs)
         self.inbound_number.choices = kwargs['inbound_number_choices']
 
-    inbound_number = RadioField(
+    inbound_number = govukRadioField(
         "Select your inbound number",
         thing='an inbound number',
     )

alphagov/notifications-admin

app/main/forms.py
@@ -1916,7 +1916,7 @@ class ServiceInboundNumberForm(StripWhitespaceForm):
         super().__init__(*args, **kwargs)
         self.inbound_number.choices = kwargs['inbound_number_choices']
 
-    inbound_number = RadioField(
+    inbound_number = GovukRadiosField(
         "Select your inbound number",
         thing='an inbound number',
     )