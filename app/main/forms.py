uk-gov-mirror/alphagov.notifications-admin

Convert radios on user page (basic)
Covers the page for both new and existing users.

app/main/forms.py
@@ -814,7 +814,7 @@ class PermissionsForm(StripWhitespaceForm):
         'Folders this team member can see',
         field_label='folder')
 
-    login_authentication = RadioField(
+    login_authentication = govukRadioField(
         'Sign in using',
         choices=[
             ('sms_auth', 'Text message code'),

alphagov/notifications-admin

app/main/forms.py
@@ -981,7 +981,7 @@ class BasePermissionsForm(StripWhitespaceForm):
         'Folders this team member can see',
         field_label='folder')
 
-    login_authentication = RadioField(
+    login_authentication = GovukRadiosField(
         'Sign in using',
         choices=[
             ('sms_auth', 'Text message code'),