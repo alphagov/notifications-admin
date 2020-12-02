uk-gov-mirror/alphagov.notifications-admin

Convert radios on edit postage page (basic)

app/main/forms.py
@@ -1158,7 +1158,7 @@ class LetterTemplateForm(EmailTemplateForm):
 
 
 class LetterTemplatePostageForm(StripWhitespaceForm):
-    postage = RadioField(
+    postage = govukRadioField(
         'Choose the postage for this letter template',
         choices=[
             ('first', 'First class'),

alphagov/notifications-admin

app/main/forms.py
@@ -1389,7 +1389,7 @@ class LetterTemplateForm(EmailTemplateForm):
 
 
 class LetterTemplatePostageForm(StripWhitespaceForm):
-    postage = RadioField(
+    postage = GovukRadiosField(
         'Choose the postage for this letter template',
         choices=[
             ('first', 'First class'),