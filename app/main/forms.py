uk-gov-mirror/alphagov.notifications-admin

Convert radios on support triage page (basic)

app/main/forms.py
@@ -1314,7 +1314,7 @@ class FeedbackOrProblem(StripWhitespaceForm):
 
 
 class Triage(StripWhitespaceForm):
-    severe = RadioField(
+    severe = govukRadioField(
         'Is it an emergency?',
         choices=[
             ('yes', 'Yes'),

alphagov/notifications-admin

app/main/forms.py
@@ -1545,7 +1545,7 @@ class FeedbackOrProblem(StripWhitespaceForm):
 
 
 class Triage(StripWhitespaceForm):
-    severe = RadioField(
+    severe = GovukRadiosField(
         'Is it an emergency?',
         choices=[
             ('yes', 'Yes'),