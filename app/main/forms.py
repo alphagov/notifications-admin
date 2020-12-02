uk-gov-mirror/alphagov.notifications-admin

Convert radios on add nhs local org page (basic)

app/main/forms.py
@@ -955,7 +955,7 @@ class AddNHSLocalOrganisationForm(StripWhitespaceForm):
         super().__init__(*args, **kwargs)
         self.organisations.choices = organisation_choices
 
-    organisations = RadioField(
+    organisations = govukRadioField(
         'Which NHS Trust or Clinical Commissioning Group do you work for?',
         thing='an NHS Trust or Clinical Commissioning Group'
     )

alphagov/notifications-admin

app/main/forms.py
@@ -1157,7 +1157,7 @@ class AddNHSLocalOrganisationForm(StripWhitespaceForm):
         super().__init__(*args, **kwargs)
         self.organisations.choices = organisation_choices
 
-    organisations = RadioField(
+    organisations = GovukRadiosField(
         'Which NHS Trust or Clinical Commissioning Group do you work for?',
         thing='an NHS Trust or Clinical Commissioning Group'
     )