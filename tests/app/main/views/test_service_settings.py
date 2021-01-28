ed45cd18b0cbd7fb82ebd6bbcf0136c86a92ba30
karlchillmaid <karl.chillmaid@digital.cabinet-office.gov.uk>
2021-01-28 13:47:45 +0000

    Change p to pence

--- tests/app/main/views/test_service_settings.py
+++ tests/app/main/views/test_service_settings.py
@@ -3790,7 +3790,7 @@ def test_unknown_channel_404s(
 ), [
     (
         'letter',
-        'It costs between 39p and £1.21 to send a letter using Notify.',
+        'It costs between 39 pence and £1.21 to send a letter using Notify.',
         'Send letters',
         ['email', 'sms'],
         'False',
@@ -3799,7 +3799,7 @@ def test_unknown_channel_404s(
     ),
     (
         'letter',
-        'It costs between 39p and £1.21 to send a letter using Notify.',
+        'It costs between 39 pence and £1.21 to send a letter using Notify.',
         'Send letters',
         ['email', 'sms', 'letter'],
         'True',
