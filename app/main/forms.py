f0ce2c6f5b8740ece42dc8ebc62a8aecef2efa18
Rebecca Law <rebecca.law@digital.cabinet-office.gov.uk>
2021-02-09 14:07:01 +0000

    Update utils to version 43.8.
    Invalid characters for the first line of a postal address now include < >

--- app/main/forms.py
+++ app/main/forms.py
@@ -1362,7 +1362,7 @@ class LetterAddressForm(StripWhitespaceForm):
 
         if address.has_invalid_characters:
             raise ValidationError(
-                'Address lines must not start with any of the following characters: @ ( ) = [ ] ” \\ / ,'
+                'Address lines must not start with any of the following characters: @ ( ) = [ ] ” \\ / , < >'
             )
 
 
