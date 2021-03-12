7583c8d9faacf3cfbea3b1a525a4ec1806c93b58
Chris Hill-Scott <me@quis.cc>
2021-03-12 17:12:47 +0000

    Remove redundant hidden H3s
    This commit replaces the H3s which only repeated information with some
    hidden text that will make it read nicer for screenreaders.

--- tests/app/main/views/test_index.py
+++ tests/app/main/views/test_index.py
@@ -34,10 +34,7 @@ def test_non_logged_in_user_can_see_homepage(
 
     assert normalize_spaces(page.select_one('#whos-using-notify').text) == (
         'Who’s using GOV.UK Notify '
-        'Organisations '
-        '111 organisations '
-        'Services '
-        '9,999 services '
+        'There are 111 organisations and 9,999 services using Notify. '
         'See the list of services and organisations.'
     )
     assert page.select_one('#whos-using-notify a')['href'] == url_for(
