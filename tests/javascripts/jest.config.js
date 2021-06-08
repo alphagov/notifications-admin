3a4601bbe4692c540ad8513428085763944abae4
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2021-06-08 16:02:55 +0100

    Set window location for individual test
    This is less surprising and means we don't end up in a situation
    where a future test needs to change the global config to something
    else.

--- tests/javascripts/jest.config.js
+++ tests/javascripts/jest.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   setupFiles: ['./support/setup.js'],
-  testURL: 'https://www.notifications.service.gov.uk/?next=%2Ffoo%3Fbar%3Dbaz'
+  testURL: 'https://www.notifications.service.gov.uk',
 }
