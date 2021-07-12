5dccde60ed9b7bfbe4bcbbdcb2009d0cdcf8017a
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2021-07-12 15:37:22 +0100

    DRY-up arg assertions in event handler tests
    This will make it easier to add another handler in the next commit.

--- tests/app/test_event_handlers.py
+++ tests/app/test_event_handlers.py
@@ -32,140 +32,89 @@ def test_on_user_logged_in_calls_events_api(client, api_user_active, mock_events
 
 
 def test_create_email_change_event_calls_events_api(client, mock_events):
-    user_id = str(uuid.uuid4())
-    updated_by_id = str(uuid.uuid4())
+    kwargs = {
+        "user_id": str(uuid.uuid4()),
+        "updated_by_id": str(uuid.uuid4()),
+        "original_email_address": 'original@example.com',
+        "new_email_address": 'new@example.com'
+    }
 
-    create_email_change_event(
-        user_id=user_id,
-        updated_by_id=updated_by_id,
-        original_email_address='original@example.com',
-        new_email_address='new@example.com'
-    )
-
-    mock_events.assert_called_with('update_user_email', event_dict(
-        user_id=user_id,
-        updated_by_id=updated_by_id,
-        original_email_address='original@example.com',
-        new_email_address='new@example.com'
-    ))
+    create_email_change_event(**kwargs)
+    mock_events.assert_called_with('update_user_email', event_dict(**kwargs))
 
 
 def test_create_add_user_to_service_event_calls_events_api(client, mock_events):
-    user_id = str(uuid.uuid4())
-    invited_by_id = str(uuid.uuid4())
-    service_id = str(uuid.uuid4())
+    kwargs = {
+        "user_id": str(uuid.uuid4()),
+        "invited_by_id": str(uuid.uuid4()),
+        "service_id": str(uuid.uuid4())
+    }
 
-    create_add_user_to_service_event(
-        user_id=user_id,
-        invited_by_id=invited_by_id,
-        service_id=service_id
-    )
-
-    mock_events.assert_called_with('add_user_to_service', event_dict(
-        user_id=user_id,
-        invited_by_id=invited_by_id,
-        service_id=service_id,
-    ))
+    create_add_user_to_service_event(**kwargs)
+    mock_events.assert_called_with('add_user_to_service', event_dict(**kwargs))
 
 
 def test_create_remove_user_from_service_event_calls_events_api(client, mock_events):
-    user_id = str(uuid.uuid4())
-    removed_by_id = str(uuid.uuid4())
-    service_id = str(uuid.uuid4())
+    kwargs = {
+        "user_id": str(uuid.uuid4()),
+        "removed_by_id": str(uuid.uuid4()),
+        "service_id": str(uuid.uuid4())
+    }
 
-    create_remove_user_from_service_event(
-        user_id=user_id,
-        removed_by_id=removed_by_id,
-        service_id=service_id
-    )
-
-    mock_events.assert_called_with('remove_user_from_service', event_dict(
-        user_id=user_id,
-        removed_by_id=removed_by_id,
-        service_id=service_id,
-    ))
+    create_remove_user_from_service_event(**kwargs)
+    mock_events.assert_called_with('remove_user_from_service', event_dict(**kwargs))
 
 
 def test_create_mobile_number_change_event_calls_events_api(client, mock_events):
-    user_id = str(uuid.uuid4())
-    updated_by_id = str(uuid.uuid4())
+    kwargs = {
+        "user_id": str(uuid.uuid4()),
+        "updated_by_id": str(uuid.uuid4()),
+        "original_mobile_number": '07700900000',
+        "new_mobile_number": '07700900999'
+    }
 
-    create_mobile_number_change_event(
-        user_id=user_id,
-        updated_by_id=updated_by_id,
-        original_mobile_number='07700900000',
-        new_mobile_number='07700900999'
-    )
-
-    mock_events.assert_called_with('update_user_mobile_number', event_dict(
-        user_id=user_id,
-        updated_by_id=updated_by_id,
-        original_mobile_number='07700900000',
-        new_mobile_number='07700900999'
-    ))
+    create_mobile_number_change_event(**kwargs)
+    mock_events.assert_called_with('update_user_mobile_number', event_dict(**kwargs))
 
 
 def test_create_archive_user_event_calls_events_api(client, mock_events):
-    user_id = str(uuid.uuid4())
-    archived_by_id = str(uuid.uuid4())
+    kwargs = {
+        "user_id": str(uuid.uuid4()),
+        "archived_by_id": str(uuid.uuid4())
+    }
 
-    create_archive_user_event(
-        user_id=user_id,
-        archived_by_id=archived_by_id
-    )
-
-    mock_events.assert_called_with('archive_user', event_dict(
-        user_id=user_id,
-        archived_by_id=archived_by_id
-    ))
+    create_archive_user_event(**kwargs)
+    mock_events.assert_called_with('archive_user', event_dict(**kwargs))
 
 
 def test_create_broadcast_account_type_change_event(client, mock_events):
-    service_id = str(uuid.uuid4())
-    changed_by_id = str(uuid.uuid4())
+    kwargs = {
+        "service_id": str(uuid.uuid4()),
+        "changed_by_id": str(uuid.uuid4()),
+        "service_mode": 'training',
+        "broadcast_channel": 'severe',
+        "provider_restriction": None
+    }
 
-    create_broadcast_account_type_change_event(
-        service_id=service_id,
-        changed_by_id=changed_by_id,
-        service_mode='training',
-        broadcast_channel='severe',
-        provider_restriction=None
-    )
-
-    mock_events.assert_called_with('change_broadcast_account_type', event_dict(
-        service_id=service_id,
-        changed_by_id=changed_by_id,
-        service_mode='training',
-        broadcast_channel='severe',
-        provider_restriction=None
-    ))
+    create_broadcast_account_type_change_event(**kwargs)
+    mock_events.assert_called_with('change_broadcast_account_type', event_dict(**kwargs))
 
 
 def test_suspend_service(client, mock_events):
-    service_id = str(uuid.uuid4())
-    suspended_by_id = str(uuid.uuid4())
+    kwargs = {
+        "service_id": str(uuid.uuid4()),
+        "suspended_by_id": str(uuid.uuid4())
+    }
 
-    create_suspend_service_event(
-        service_id=service_id,
-        suspended_by_id=suspended_by_id,
-    )
-
-    mock_events.assert_called_with('suspend_service', event_dict(
-        service_id=service_id,
-        suspended_by_id=suspended_by_id,
-    ))
+    create_suspend_service_event(**kwargs)
+    mock_events.assert_called_with('suspend_service', event_dict(**kwargs))
 
 
 def test_archive_service(client, mock_events):
-    service_id = str(uuid.uuid4())
-    archived_by_id = str(uuid.uuid4())
+    kwargs = {
+        "service_id": str(uuid.uuid4()),
+        "archived_by_id": str(uuid.uuid4())
+    }
 
-    create_archive_service_event(
-        service_id=service_id,
-        archived_by_id=archived_by_id,
-    )
-
-    mock_events.assert_called_with('archive_service', event_dict(
-        service_id=service_id,
-        archived_by_id=archived_by_id,
-    ))
+    create_archive_service_event(**kwargs)
+    mock_events.assert_called_with('archive_service', event_dict(**kwargs))
