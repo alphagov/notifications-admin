cca94e956b18cce9e70029502a5ab863456b9603
Rebecca Law <rebecca.law@digital.cabinet-office.gov.uk>
2021-07-14 09:54:28 +0100

    Update content

--- tests/app/main/views/test_jobs.py
+++ tests/app/main/views/test_jobs.py
@@ -236,8 +236,12 @@ def test_should_show_job_with_sending_limit_exceeded_status(
         service_id=service_one['id'],
         job_id=fake_uuid,
     )
+
     assert normalize_spaces(page.select('main p')[1].text) == (
-       "You have exceeded your daily sending limits. You can upload your CSV tomorrow or contact the GOV.UK Notify team"
+        "Notify cannot send these messages because you have reached your daily limit. You can only send 1,000 messages per day." # noqa
+    )
+    assert normalize_spaces(page.select('main p')[2].text) == (
+        "Upload this spreadsheet again tomorrow or contact the GOV.UK Notify team to raise the limit."
     )
 
 
