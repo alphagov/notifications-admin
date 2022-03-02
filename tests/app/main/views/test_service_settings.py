fb3f79d83c4b07fded487d63930df92ae4343f6c
Chris Hill-Scott <me@quis.cc>
2022-03-02 10:28:49 +0000

    Update pricing of letters on settings page
    Follows https://github.com/alphagov/notifications-admin/pull/4174/files

--- tests/app/main/views/test_service_settings.py
+++ tests/app/main/views/test_service_settings.py
@@ -3938,7 +3938,7 @@ def test_unknown_channel_404s(
 ), [
     (
         'letter',
-        'It costs between 39 pence and £1.21 to send a letter using Notify.',
+        'It costs between 41 pence and £1.28 to send a letter using Notify.',
         'Send letters',
         ['email', 'sms'],
         'False',
@@ -3947,7 +3947,7 @@ def test_unknown_channel_404s(
     ),
     (
         'letter',
-        'It costs between 39 pence and £1.21 to send a letter using Notify.',
+        'It costs between 41 pence and £1.28 to send a letter using Notify.',
         'Send letters',
         ['email', 'sms', 'letter'],
         'True',
