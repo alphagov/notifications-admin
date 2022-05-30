487dc1b488e37bbd1af041b59470acb1471a1f43
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2022-05-30 15:29:03 +0100

    Test _template_folders functions via TemplateList
    This is part of the overall migration of the "_template_folders"
    methods to the TemplateList class. Moving the existing tests now
    will make the actual migration easier to follow.
    
    To emulate the second and third tests, we need to grab a specific
    folder from the TemplateList and then look at its folders - these
    are set based on "get_template_folders" as before.

--- app/models/template_list.py
+++ app/models/template_list.py
@@ -170,6 +170,7 @@ class TemplateListFolder(TemplateListItem):
         super().__init__(folder, ancestors)
         self.folder = folder
         self.service_id = service_id
+        self.folders = folders
         self.number_of_templates = len(templates)
         self.number_of_folders = len(folders)
 
