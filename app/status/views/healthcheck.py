2a322d26d4ed299d21a1b931e03311ff02a23e0f
Richard Baker <richard.baker@digital.cabinet-office.gov.uk>
2022-08-09 16:08:22 +0100

    Remove git commit/version from status endpoint
    This is temporary for the purpose of getting running in
    Docker with minimal build steps.

--- app/status/views/healthcheck.py
+++ app/status/views/healthcheck.py
@@ -1,7 +1,7 @@
 from flask import current_app, jsonify, request
 from notifications_python_client.errors import HTTPError
 
-from app import status_api_client, version
+from app import status_api_client
 from app.status import status
 
 
@@ -15,4 +15,6 @@ def show_status():
         except HTTPError as e:
             current_app.logger.exception("API failed to respond")
             return jsonify(status="error", message=str(e.message)), 500
-        return jsonify(status="ok", api=api_status, git_commit=version.__git_commit__, build_time=version.__time__), 200
+        return jsonify(
+            status="ok",
+            api=api_status), 200
