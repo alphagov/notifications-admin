387456c49ee00fe51f3722fbfc916673248df44f
Chris Hill-Scott <me@quis.cc>
2022-10-19 11:20:38 +0100

    Use `None` default letter branding for empty org
    This is the same thing we need do for email branding already.
    
    Storing the `None` default in the model means that we don’t need to
    declare a temporary variable in the view.

--- app/utils/branding.py
+++ app/utils/branding.py
@@ -4,12 +4,10 @@ NHS_EMAIL_BRANDING_ID = "a7dc4e56-660b-4db7-8cff-12c37b12b5ea"
 
 
 def get_email_choices(service):
-    organisation_branding_id = service.organisation.email_branding_id if service.organisation else None
-
     if (
         service.organisation_type == Organisation.TYPE_CENTRAL
         and service.email_branding_id is not None  # GOV.UK is not current branding
-        and organisation_branding_id is None  # no default to supersede it (GOV.UK)
+        and service.organisation.email_branding_id is None  # no default to supersede it (GOV.UK)
     ):
         yield ("govuk", "GOV.UK")
 
@@ -25,7 +23,7 @@ def get_email_choices(service):
         if (
             service.organisation_type == Organisation.TYPE_CENTRAL
             and service.organisation
-            and organisation_branding_id is None  # don't offer both if org has default
+            and service.organisation.email_branding_id is None  # don't offer both if org has default
             and service.email_branding_name.lower() != f"GOV.UK and {service.organisation.name}".lower()
         ):
             yield ("govuk_and_org", f"GOV.UK and {service.organisation.name}")
@@ -35,14 +33,13 @@ def get_email_choices(service):
             and not service.is_nhs
             and (
                 service.email_branding_id is None  # GOV.UK is current branding
-                or service.email_branding_id != organisation_branding_id
+                or service.email_branding_id != service.organisation.email_branding_id
             )
         ):
             yield ("organisation", service.organisation.name)
 
 
 def get_letter_choices(service):
-    organisation_branding_id = service.organisation.letter_branding_id if service.organisation else None
 
     if service.is_nhs and service.letter_branding_name != "NHS":
         yield ("nhs", "NHS")
@@ -52,7 +49,7 @@ def get_letter_choices(service):
         and not service.is_nhs
         and (
             service.letter_branding_id is None  # GOV.UK is current branding
-            or service.letter_branding_id != organisation_branding_id
+            or service.letter_branding_id != service.organisation.letter_branding_id
         )
     ):
         yield ("organisation", service.organisation.name)
