13073d20755177a076804d422aff0bc90220f6cc
Leo Hemsted <leo.hemsted@digital.cabinet-office.gov.uk>
2022-11-01 12:56:57 +0000

    vendor govuk frontend jinja v1.5.1 checkboxes
    in https://github.com/alphagov/notifications-admin/pull/3535 we copied
    across the source for the govuk frontend njk checkboxes so we could make
    some required changes that weren't exposed using the normal macro
    parameters.
    
    we'll need to do the same with the new jinja library's version - pull in
    the source code, then go through and see which changes we need to make.
    
    This file is pulled from here:
    
    https://github.com/LandRegistry/govuk-frontend-jinja/blob/1.5.1/govuk_frontend_jinja/templates/components/checkboxes/macro.html
    
    Copy across the comment describing changes from the old njk template so
    we can start to migrate those changes

--- app/main/forms.py
+++ app/main/forms.py
@@ -106,6 +106,10 @@ def render_govuk_frontend_macro(component, params):
     govuk_frontend_components = {
         "radios": {"path": "govuk_frontend_jinja/components/radios/macro.html", "macro": "govukRadios"},
         "text-input": {"path": "govuk_frontend_jinja/components/input/macro.html", "macro": "govukInput"},
+        "checkbox": {
+            "path": "govuk_frontend_jinja_overrides/templates/components/checkboxes/macro.html",
+            "macro": "govukCheckboxes",
+        },
     }
 
     # we need to duplicate all curly braces to escape them from the f string so jinja still sees them
@@ -671,7 +675,7 @@ def govuk_checkbox_field_widget(self, field, param_extensions=None, **kwargs):
     if param_extensions:
         merge_jsonlike(params, param_extensions)
 
-    return Markup(render_template("forms/fields/checkboxes/macro.njk", params=params))
+    return render_govuk_frontend_macro("checkbox", params=params)
 
 
 def govuk_checkboxes_field_widget(self, field, wrap_in_collapsible=False, param_extensions=None, **kwargs):
