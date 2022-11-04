alphagov/notifications-admin

commit 245f1b9524aba8aeff540f056d0cbd44aaf43dfe

Make models for email and letter branding
This is the same pattern we have for services, templates, jobs, events,
etc, etc.

Moving code into the model layer means:
- less direct interaction with raw JSON
- a slimmer view layer, with better separation of concern

It also means we get nice features for free, like detecting typos in
property names, and making object sortable.

app/templates/views/service-settings/branding/letter-branding-options.html
@@ -22,7 +22,7 @@
   {{ page_header('Change letter branding') }}
 
   <p class="govuk-body">
-    Your letters currently have {{ branding_name }} branding.
+    Your letters currently have {{ current_service.letter_branding.name }} branding.
   </p>
 
   {% call form_wrapper() %}

app/main/views/email_branding.py
@@ -6,6 +6,7 @@ from app import email_branding_client
 from app.event_handlers import create_update_email_branding_event
 from app.main import main
 from app.main.forms import AdminEditEmailBrandingForm, SearchByNameForm
+from app.models.branding import AllEmailBranding, EmailBranding
 from app.s3_client.s3_logo_client import (
     TEMP_TAG,
     delete_email_temp_file,
@@ -20,10 +21,8 @@ from app.utils.user import user_is_platform_admin
 @main.route("/email-branding", methods=["GET", "POST"])
 @user_is_platform_admin
 def email_branding():
-    brandings = email_branding_client.get_all_email_branding(sort_key="name")
-
     return render_template(
-        "views/email-branding/select-branding.html", email_brandings=brandings, search_form=SearchByNameForm()
+        "views/email-branding/select-branding.html", email_brandings=AllEmailBranding(), search_form=SearchByNameForm()
     )
 
 
@@ -31,16 +30,16 @@ def email_branding():
 @main.route("/email-branding/<uuid:branding_id>/edit/<logo>", methods=["GET", "POST"])
 @user_is_platform_admin
 def update_email_branding(branding_id, logo=None):
-    email_branding = email_branding_client.get_email_branding(branding_id)["email_branding"]
+    email_branding = EmailBranding.from_id(branding_id)
 
     form = AdminEditEmailBrandingForm(
-        name=email_branding["name"],
-        text=email_branding["text"],
-        colour=email_branding["colour"],
-        brand_type=email_branding["brand_type"],
+        name=email_branding.name,
+        text=email_branding.text,
+        colour=email_branding.colour,
+        brand_type=email_branding.brand_type,
     )
 
-    logo = logo if logo else email_branding.get("logo") if email_branding else None
+    logo = logo or email_branding.logo
 
     if form.validate_on_submit():
         if form.file.data:

app/main/views/letter_branding.py
@@ -5,6 +5,7 @@ from notifications_python_client.errors import HTTPError
 from app import letter_branding_client
 from app.main import main
 from app.main.forms import AdminEditLetterBrandingForm, SearchByNameForm, SVGFileUpload
+from app.models.branding import AllLetterBranding, LetterBranding
 from app.s3_client.s3_logo_client import (
     LETTER_TEMP_TAG,
     delete_letter_temp_file,
@@ -20,11 +21,10 @@ from app.utils.user import user_is_platform_admin
 @main.route("/letter-branding", methods=["GET"])
 @user_is_platform_admin
 def letter_branding():
-
-    brandings = letter_branding_client.get_all_letter_branding()
-
     return render_template(
-        "views/letter-branding/select-letter-branding.html", letter_brandings=brandings, search_form=SearchByNameForm()
+        "views/letter-branding/select-letter-branding.html",
+        letter_brandings=AllLetterBranding(),
+        search_form=SearchByNameForm(),
     )
 
 
@@ -32,17 +32,17 @@ def letter_branding():
 @main.route("/letter-branding/<uuid:branding_id>/edit/<path:logo>", methods=["GET", "POST"])
 @user_is_platform_admin
 def update_letter_branding(branding_id, logo=None):
-    letter_branding = letter_branding_client.get_letter_branding(branding_id)
+    letter_branding = LetterBranding.from_id(branding_id)
 
     file_upload_form = SVGFileUpload()
     letter_branding_details_form = AdminEditLetterBrandingForm(
-        name=letter_branding["name"],
+        name=letter_branding.name,
     )
 
     file_upload_form_submitted = file_upload_form.file.data
     details_form_submitted = request.form.get("operation") == "branding-details"
 
-    logo = logo if logo else permanent_letter_logo_name(letter_branding["filename"], "svg")
+    logo = logo or permanent_letter_logo_name(letter_branding.filename, "svg")
 
     if file_upload_form_submitted and file_upload_form.validate_on_submit():
         upload_filename = upload_letter_temp_logo(
@@ -61,7 +61,7 @@ def update_letter_branding(branding_id, logo=None):
         db_filename = letter_filename_for_db(logo, session["user_id"])
 
         try:
-            if db_filename == letter_branding["filename"]:
+            if db_filename == letter_branding.filename:
 
                 letter_branding_client.update_letter_branding(
                     branding_id=branding_id,
@@ -90,8 +90,8 @@ def update_letter_branding(branding_id, logo=None):
             # we had a problem saving the file - rollback the db changes
             letter_branding_client.update_letter_branding(
                 branding_id=branding_id,
-                filename=letter_branding["filename"],
-                name=letter_branding["name"],
+                filename=letter_branding.filename,
+                name=letter_branding.name,
             )
             file_upload_form.file.errors = ["Error saving uploaded file - try uploading again"]
 

app/main/views/organisations.py
@@ -21,8 +21,6 @@ from werkzeug.exceptions import abort
 from app import (
     current_organisation,
     current_service,
-    email_branding_client,
-    letter_branding_client,
     org_invite_api_client,
     organisations_client,
 )
@@ -51,7 +49,7 @@ from app.main.views.dashboard import (
     get_tuples_of_financial_years,
     requested_and_current_financial_year,
 )
-from app.main.views.service_settings import get_branding_as_value_and_label
+from app.models.branding import AllEmailBranding, AllLetterBranding
 from app.models.organisation import AllOrganisations, Organisation
 from app.models.user import InvitedOrgUser, User
 from app.s3_client.s3_mou_client import get_mou
@@ -285,16 +283,14 @@ def cancel_invited_org_user(org_id, invited_user_id):
 @main.route("/organisations/<uuid:org_id>/settings/", methods=["GET"])
 @user_is_platform_admin
 def organisation_settings(org_id):
-    default_email_branding_name = current_organisation.email_branding_name
     other_email_branding_options = [
-        email_branding_name
-        for email_branding_name in current_organisation.email_branding_pool_names
-        if email_branding_name != default_email_branding_name
+        email_branding.name
+        for email_branding in current_organisation.email_branding_pool
+        if email_branding != current_organisation.email_branding
     ]
 
     return render_template(
         "views/organisations/organisation/settings/index.html",
-        default_email_branding_name=default_email_branding_name,
         other_email_branding_options=other_email_branding_options,
     )
 
@@ -406,15 +402,13 @@ def _handle_remove_branding(remove_branding_id) -> Optional[Response]:
     or has clicked to confirm that flash message.
     """
     try:
-        remove_branding = next(
-            filter(lambda x: x["id"] == remove_branding_id, current_organisation.email_branding_pool)
-        )
+        remove_branding = current_organisation.email_branding_pool.get_item_by_id(remove_branding_id)
     except StopIteration:
         abort(400, f"Invalid email branding ID {remove_branding_id} for {current_organisation}")
 
     if request.method == "POST":
         organisations_client.remove_email_branding_from_pool(current_organisation.id, remove_branding_id)
-        confirmation_message = f'Email branding ‘{remove_branding["name"]}’ removed.'
+        confirmation_message = f"Email branding ‘{remove_branding.name}’ removed."
         flash(confirmation_message, "default_with_tick")
         return redirect(url_for("main.organisation_email_branding", org_id=current_organisation.id))
 
@@ -422,7 +416,7 @@ def _handle_remove_branding(remove_branding_id) -> Optional[Response]:
         confirmation_question = Markup(
             render_template(
                 "partials/flash_messages/email_branding_confirm_remove_brand.html",
-                branding_name=remove_branding["name"],
+                branding_name=remove_branding.name,
             )
         )
 
@@ -452,7 +446,7 @@ def _handle_change_default_branding_to_govuk(is_central_government) -> Optional[
         return redirect(url_for("main.organisation_email_branding", org_id=current_organisation.id))
 
     else:
-        current_brand = current_organisation.email_branding_name
+        current_brand = current_organisation.email_branding.name
         confirmation_question = Markup(
             render_template(
                 "partials/flash_messages/email_branding_confirm_change_default_to_govuk.html",
@@ -475,9 +469,7 @@ def _handle_change_default_branding(form, new_default_branding_id) -> Optional[R
 
     def __get_email_branding_name(branding_id):
         try:
-            return next(brand for brand in current_organisation.email_branding_pool if brand["id"] == branding_id)[
-                "name"
-            ]
+            return current_organisation.email_branding_pool.get_item_by_id(branding_id).name
         except StopIteration:
             current_app.logger.info(
                 f"Email branding ID {branding_id} is not present in organisation {current_organisation.name}'s "
@@ -540,9 +532,7 @@ def _handle_change_default_branding(form, new_default_branding_id) -> Optional[R
 def organisation_email_branding(org_id):
     # We want to display email branding options apart from an option that has already been set as the default
     email_branding_pool_options = [
-        {"name": option["name"], "id": option["id"]}
-        for option in current_organisation.email_branding_pool
-        if option["name"] != current_organisation.email_branding_name
+        option for option in current_organisation.email_branding_pool if option != current_organisation.email_branding
     ]
 
     is_central_government = current_organisation.organisation_type == Organisation.TYPE_CENTRAL
@@ -579,8 +569,8 @@ def add_organisation_email_branding_options(org_id):
     form = AddEmailBrandingOptionsForm()
 
     form.branding_field.choices = [
-        (branding["id"], branding["name"])
-        for branding in email_branding_client.get_all_email_branding(sort_key="name")
+        (branding.id, branding.name)
+        for branding in AllEmailBranding()
         if branding not in current_organisation.email_branding_pool
     ]
 
@@ -607,10 +597,8 @@ def add_organisation_email_branding_options(org_id):
 @main.route("/organisations/<uuid:org_id>/settings/set-letter-branding", methods=["GET", "POST"])
 @user_is_platform_admin
 def edit_organisation_letter_branding(org_id):
-    letter_branding = letter_branding_client.get_all_letter_branding()
-
     form = AdminSetLetterBrandingForm(
-        all_branding_options=get_branding_as_value_and_label(letter_branding),
+        all_branding_options=AllLetterBranding().as_id_and_name,
         current_branding=current_organisation.letter_branding_id,
     )
 

app/main/views/service_settings.py
@@ -21,7 +21,6 @@ from app import (
     current_service,
     email_branding_client,
     inbound_number_client,
-    letter_branding_client,
     notification_api_client,
     organisations_client,
     service_api_client,
@@ -66,8 +65,8 @@ from app.main.forms import (
     SomethingElseBrandingForm,
 )
 from app.main.views.pricing import CURRENT_SMS_RATE
+from app.models.branding import AllEmailBranding, AllLetterBranding, EmailBranding
 from app.utils import DELIVERED_STATUSES, FAILURE_STATUSES, SENDING_STATUSES
-from app.utils.branding import NHS_EMAIL_BRANDING_ID
 from app.utils.branding import get_email_choices as get_email_branding_choices
 from app.utils.user import (
     user_has_permissions,
@@ -958,10 +957,8 @@ def set_rate_limit(service_id):
 @main.route("/services/<uuid:service_id>/service-settings/set-email-branding", methods=["GET", "POST"])
 @user_is_platform_admin
 def service_set_email_branding(service_id):
-    email_branding = email_branding_client.get_all_email_branding()
-
     form = AdminSetEmailBrandingForm(
-        all_branding_options=get_branding_as_value_and_label(email_branding),
+        all_branding_options=AllEmailBranding().as_id_and_name,
         current_branding=current_service.email_branding_id,
     )
 
@@ -1033,7 +1030,7 @@ def service_preview_email_branding(service_id):
         if (
             current_service.organisation
             and email_branding_id
-            and email_branding_id not in current_service.organisation.email_branding_pool_ids
+            and email_branding_id not in current_service.organisation.email_branding_pool.ids
         ):
             return redirect(
                 url_for(
@@ -1057,10 +1054,8 @@ def service_preview_email_branding(service_id):
 @main.route("/services/<uuid:service_id>/service-settings/set-letter-branding", methods=["GET", "POST"])
 @user_is_platform_admin
 def service_set_letter_branding(service_id):
-    letter_branding = letter_branding_client.get_all_letter_branding()
-
     form = AdminSetLetterBrandingForm(
-        all_branding_options=get_branding_as_value_and_label(letter_branding),
+        all_branding_options=AllLetterBranding().as_id_and_name,
         current_branding=current_service.letter_branding_id,
     )
 
@@ -1124,7 +1119,7 @@ def create_email_branding_zendesk_ticket(form_option_selected, detail=None):
 
     ticket_message = render_template(
         "support-tickets/branding-request.txt",
-        current_branding=current_service.email_branding_name,
+        current_branding=current_service.email_branding.name,
         branding_requested=dict(form.options.choices)[form_option_selected],
         detail=detail,
     )
@@ -1147,19 +1142,18 @@ def email_branding_request(service_id):
     form = ChooseEmailBrandingForm(current_service)
     if form.something_else_is_only_option:
         return redirect(url_for(".email_branding_something_else", service_id=current_service.id))
-    branding_name = current_service.email_branding_name
     if form.validate_on_submit():
 
         branding_choice = form.options.data
 
-        if branding_choice == NHS_EMAIL_BRANDING_ID:
+        if branding_choice == EmailBranding.NHS_ID:
             return redirect(
                 url_for(
                     ".email_branding_nhs",
                     service_id=current_service.id,
                 )
             )
-        if branding_choice in current_service.email_branding_pool_ids:
+        if branding_choice in current_service.organisation.email_branding_pool.ids:
             return redirect(
                 url_for(".email_branding_pool_option", service_id=current_service.id, branding_option=branding_choice)
             )
@@ -1174,32 +1168,29 @@ def email_branding_request(service_id):
     return render_template(
         "views/service-settings/branding/email-branding-options.html",
         form=form,
-        branding_name=branding_name,
     )
 
 
 @main.route("/services/<uuid:service_id>/service-settings/email-branding/pool", methods=["GET", "POST"])
 @user_has_permissions("manage_service")
 def email_branding_pool_option(service_id):
-    chosen_branding_id = request.args.get("branding_option")
-    chosen_brandings = [
-        branding["name"] for branding in current_service.email_branding_pool if branding["id"] == chosen_branding_id
-    ]
-    if not chosen_brandings:
+    try:
+        chosen_branding = current_service.organisation.email_branding_pool.get_item_by_id(
+            request.args.get("branding_option")
+        )
+    except StopIteration:
         flash("No branding found for this id.")
         return redirect(url_for(".email_branding_request", service_id=current_service.id))
 
-    chosen_branding_name = chosen_brandings[0]
-
     if request.method == "POST":
-        current_service.update(email_branding=chosen_branding_id)
+        current_service.update(email_branding=chosen_branding.id)
 
         flash("You’ve updated your email branding", "default")
         return redirect(url_for(".service_settings", service_id=current_service.id))
+
     return render_template(
         "views/service-settings/branding/email-branding-pool-option.html",
-        chosen_branding_id=chosen_branding_id,
-        chosen_branding_name=chosen_branding_name,
+        chosen_branding=chosen_branding,
     )
 
 
@@ -1241,16 +1232,16 @@ def email_branding_govuk_and_org(service_id):
 @main.route("/services/<uuid:service_id>/service-settings/email-branding/nhs", methods=["GET", "POST"])
 @user_has_permissions("manage_service")
 def email_branding_nhs(service_id):
-    check_email_branding_allowed_for_service(NHS_EMAIL_BRANDING_ID)
+    check_email_branding_allowed_for_service(EmailBranding.NHS_ID)
 
     if request.method == "POST":
-        current_service.update(email_branding=NHS_EMAIL_BRANDING_ID)
+        current_service.update(email_branding=EmailBranding.NHS_ID)
 
         flash("You’ve updated your email branding", "default")
         return redirect(url_for(".service_settings", service_id=current_service.id))
 
     return render_template(
-        "views/service-settings/branding/email-branding-nhs.html", nhs_branding_id=NHS_EMAIL_BRANDING_ID
+        "views/service-settings/branding/email-branding-nhs.html", nhs_branding_id=EmailBranding.NHS_ID
     )
 
 
@@ -1291,11 +1282,10 @@ def email_branding_something_else(service_id):
 def letter_branding_request(service_id):
     form = ChooseLetterBrandingForm(current_service)
     from_template = request.args.get("from_template")
-    branding_name = current_service.letter_branding_name
     if form.validate_on_submit():
         ticket_message = render_template(
             "support-tickets/branding-request.txt",
-            current_branding=branding_name,
+            current_branding=current_service.letter_branding.name or "no",
             branding_requested=dict(form.options.choices)[form.options.data],
             detail=form.something_else.data,
         )
@@ -1320,7 +1310,6 @@ def letter_branding_request(service_id):
     return render_template(
         "views/service-settings/branding/letter-branding-options.html",
         form=form,
-        branding_name=branding_name,
         from_template=from_template,
     )
 
@@ -1405,10 +1394,6 @@ def edit_service_billing_details(service_id):
         "views/service-settings/edit-service-billing-details.html",
         form=form,
     )
-
-
-def get_branding_as_value_and_label(email_branding):
-    return [(branding["id"], branding["name"]) for branding in email_branding]
 
 
 def convert_dictionary_to_wtforms_choices_format(dictionary, value, label):

app/models/branding.py
@@ -1,0 +1,86 @@
+from app.models import JSONModel, ModelList
+from app.notify_client.email_branding_client import email_branding_client
+from app.notify_client.letter_branding_client import letter_branding_client
+from app.notify_client.organisations_api_client import organisations_client
+
+
+class Branding(JSONModel):
+    ALLOWED_PROPERTIES = {"id", "name"}
+    __sort_attribute__ = "name"
+
+    def __bool__(self):
+        return bool(self.id)
+
+
+class EmailBranding(Branding):
+    ALLOWED_PROPERTIES = Branding.ALLOWED_PROPERTIES | {
+        "colour",
+        "logo",
+        "text",
+        "brand_type",
+    }
+
+    NHS_ID = "a7dc4e56-660b-4db7-8cff-12c37b12b5ea"
+
+    @classmethod
+    def from_id(cls, id):
+        if id is None:
+            return cls({key: None for key in cls.ALLOWED_PROPERTIES} | {"name": "GOV.UK"})
+        return cls(email_branding_client.get_email_branding(id)["email_branding"])
+
+    @property
+    def is_nhs(self):
+        return self.id == self.NHS_ID
+
+    @property
+    def is_govuk(self):
+        return self.id is None
+
+
+class LetterBranding(Branding):
+    ALLOWED_PROPERTIES = Branding.ALLOWED_PROPERTIES | {"filename"}
+
+    @classmethod
+    def from_id(cls, id):
+        if id is None:
+            return cls({key: None for key in cls.ALLOWED_PROPERTIES})
+        return cls(letter_branding_client.get_letter_branding(id))
+
+    @property
+    def is_nhs(self):
+        return self.name == "NHS"
+
+
+class AllBranding(ModelList):
+    @property
+    def ids(self):
+        return tuple(branding.id for branding in self)
+
+    @property
+    def as_id_and_name(self):
+        return [(branding.id, branding.name) for branding in self]
+
+    def get_item_by_id(self, id):
+        for branding in self:
+            if branding.id == id:
+                return branding
+        raise StopIteration
+
+
+class AllEmailBranding(AllBranding):
+    client_method = email_branding_client.get_all_email_branding
+    model = EmailBranding
+
+
+class EmailBrandingPool(AllEmailBranding):
+    client_method = organisations_client.get_email_branding_pool
+
+    def __init__(self, id):
+        self.items = tuple()
+        if id:
+            self.items = self.client_method(id)
+
+
+class AllLetterBranding(AllBranding):
+    client_method = letter_branding_client.get_all_letter_branding
+    model = LetterBranding

app/models/organisation.py
@@ -4,8 +4,7 @@ from flask import abort
 from werkzeug.utils import cached_property
 
 from app.models import JSONModel, ModelList, SerialisedModelCollection
-from app.notify_client.email_branding_client import email_branding_client
-from app.notify_client.letter_branding_client import letter_branding_client
+from app.models.branding import EmailBranding, EmailBrandingPool, LetterBranding
 from app.notify_client.organisations_api_client import organisations_client
 
 
@@ -107,6 +106,7 @@ class Organisation(JSONModel):
         super().__init__(_dict)
 
         if self._dict == {}:
+            self.id = None
             self.name = None
             self.crown = None
             self.agreement_signed = None
@@ -173,31 +173,15 @@ class Organisation(JSONModel):
 
     @cached_property
     def email_branding(self):
-        if self.email_branding_id:
-            return email_branding_client.get_email_branding(self.email_branding_id)["email_branding"]
+        return EmailBranding.from_id(self.email_branding_id)
 
-    @property
-    def email_branding_name(self):
-        if self.email_branding_id:
-            return self.email_branding["name"]
-        return "GOV.UK"
-
-    @property
+    @cached_property
     def email_branding_pool(self):
-        return organisations_client.get_email_branding_pool(self.id)
-
-    @property
-    def email_branding_pool_names(self):
-        return [branding["name"] for branding in self.email_branding_pool]
-
-    @property
-    def email_branding_pool_ids(self):
-        return [branding["id"] for branding in self.email_branding_pool]
+        return EmailBrandingPool(self.id)
 
     @cached_property
     def letter_branding(self):
-        if self.letter_branding_id:
-            return letter_branding_client.get_letter_branding(self.letter_branding_id)
+        return LetterBranding.from_id(self.letter_branding_id)
 
     @cached_property
     def agreement_signed_by(self):

app/models/service.py
@@ -3,17 +3,16 @@ from notifications_utils.serialised_model import SerialisedModelCollection
 from werkzeug.utils import cached_property
 
 from app.models import JSONModel
+from app.models.branding import EmailBranding, LetterBranding
 from app.models.contact_list import ContactLists
 from app.models.job import ImmediateJobs, PaginatedJobs, PaginatedUploads, ScheduledJobs
 from app.models.organisation import Organisation
 from app.models.user import InvitedUsers, User, Users
 from app.notify_client.api_key_api_client import api_key_api_client
 from app.notify_client.billing_api_client import billing_api_client
-from app.notify_client.email_branding_client import email_branding_client
 from app.notify_client.inbound_number_client import inbound_number_client
 from app.notify_client.invite_api_client import invite_api_client
 from app.notify_client.job_api_client import job_api_client
-from app.notify_client.letter_branding_client import letter_branding_client
 from app.notify_client.organisations_api_client import organisations_client
 from app.notify_client.service_api_client import service_api_client
 from app.notify_client.template_folder_api_client import template_folder_api_client
@@ -442,25 +441,11 @@ class Service(JSONModel):
 
     @cached_property
     def email_branding(self):
-        if self.email_branding_id:
-            return email_branding_client.get_email_branding(self.email_branding_id)["email_branding"]
-        return None
-
-    @cached_property
-    def email_branding_name(self):
-        if self.email_branding is None:
-            return "GOV.UK"
-        return self.email_branding["name"]
-
-    @cached_property
-    def letter_branding_name(self):
-        if self.letter_branding is None:
-            return "no"
-        return self.letter_branding["name"]
+        return EmailBranding.from_id(self.email_branding_id)
 
     @property
     def needs_to_change_email_branding(self):
-        return self.email_branding_id is None and self.organisation_type != Organisation.TYPE_CENTRAL
+        return self.email_branding.is_govuk and self.organisation_type != Organisation.TYPE_CENTRAL
 
     @property
     def letter_branding_id(self):
@@ -468,9 +453,7 @@ class Service(JSONModel):
 
     @cached_property
     def letter_branding(self):
-        if self.letter_branding_id:
-            return letter_branding_client.get_letter_branding(self.letter_branding_id)
-        return None
+        return LetterBranding.from_id(self.letter_branding_id)
 
     @cached_property
     def organisation(self):
@@ -603,15 +586,8 @@ class Service(JSONModel):
         return ContactLists(self.id)
 
     @property
-    def email_branding_pool(self):
-        if self.organisation_id:
-            return self.organisation.email_branding_pool
-        else:
-            return []
-
-    @property
-    def email_branding_pool_ids(self):
-        return [branding["id"] for branding in self.email_branding_pool]
+    def can_use_govuk_branding(self):
+        return self.organisation_type == Organisation.TYPE_CENTRAL and not self.organisation.email_branding
 
 
 class Services(SerialisedModelCollection):

commit 747247cbf2cafd665e9324aec658a572fef41b8e

Fix message when service has no letter branding
This was broken by 245f1b9524aba8aeff540f056d0cbd44aaf43dfe
because the default value for letter branding name was changed from
`'no branding'` to `None`.

This commit fixes the bug by checking for a `None` value in the template
and makes sure the message has test coverage.

app/templates/views/service-settings/branding/letter-branding-options.html
@@ -22,7 +22,7 @@
   {{ page_header('Change letter branding') }}
 
   <p class="govuk-body">
-    Your letters currently have {{ current_service.letter_branding.name }} branding.
+    Your letters currently have {{ current_service.letter_branding.name or 'no' }} branding.
   </p>
 
   {% call form_wrapper() %}

tests/app/main/views/service_settings/test_letter_branding_requests.py
@@ -42,6 +42,8 @@ def test_letter_branding_request_page_when_no_branding_is_set(
     assert mock_get_email_branding.called is False
     assert mock_get_letter_branding_by_id.called is False
 
+    assert normalize_spaces(page.select_one("main p").text) == "Your letters currently have no branding."
+
     button_text = normalize_spaces(page.select_one(".page-footer button").text)
     assert button_text == "Request new branding"
 
@@ -54,6 +56,17 @@ def test_letter_branding_request_page_when_no_branding_is_set(
     else:
         assert page.select_one("textarea")["name"] == ("something_else")
         assert not page.select(".conditional-radios-panel")
+
+
+def test_letter_branding_request_page_when_branding_is_set_already(
+    client_request,
+    service_one,
+    fake_uuid,
+    mock_get_letter_branding_by_id,
+):
+    service_one["letter_branding"] = fake_uuid
+    page = client_request.get(".letter_branding_request", service_id=SERVICE_ONE_ID)
+    assert normalize_spaces(page.select_one("main p").text) == "Your letters currently have HM Government branding."
 
 
 @pytest.mark.parametrize(