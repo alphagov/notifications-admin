b03f32dc74785154ccb01f354a30457c53b1526f
Samuel Williams <samuelhwilliams@gmail.com>
2022-11-10 08:27:04 +0000

    Fix test description: njk -> html

--- tests/templates/components/test_radios_with_images.py
+++ tests/templates/components/test_radios_with_images.py
@@ -7,7 +7,7 @@ def test_govuk_frontend_jinja_overrides_on_design_system_v3():
 
     assert package_json["dependencies"]["govuk-frontend"].startswith("3."), (
         "After upgrading the Design System, manually validate that "
-        "`app/templates/govuk_frontend_jinja_overrides/templates/components/*/template.njk`"
+        "`app/templates/govuk_frontend_jinja_overrides/templates/components/*/template.html`"
         "are all structurally-correct and up-to-date macros. If not, update the macros or retire them and update the "
         "rendering process."
     )
