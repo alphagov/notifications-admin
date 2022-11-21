cd332b2192162dcf290580269c746f418019b579
Samuel Williams <samuelhwilliams@gmail.com>
2022-11-21 08:52:01 +0000

    Generate email brand previews without DB entry

--- tests/app/main/views/test_email_preview.py
+++ tests/app/main/views/test_email_preview.py
@@ -18,10 +18,18 @@ def test_displays_govuk_branding_by_default(client_request):
     assert page.select_one("a")["href"] == "https://www.gov.uk"
 
 
-def test_displays_govuk_branding(client_request, mock_get_email_branding_with_govuk_brand_type):
-
-    page = client_request.get("main.email_template", branding_style="1", _test_page_title=False)
-
+@pytest.mark.parametrize(
+    "params",
+    [
+        {},
+        {"branding_style": None},
+        {"branding_style": "govuk"},
+        {"branding_style": "__NONE__"},
+        {"branding_style": ""},
+    ],
+)
+def test_displays_govuk_branding(client_request, params):
+    page = client_request.get("main.email_template", **params, _test_page_title=False)
     assert page.select_one("a")["href"] == "https://www.gov.uk"
 
 
@@ -83,3 +91,26 @@ def test_displays_org_branding_with_banner_without_brand_text(
     assert page.select_one("img")["src"] == "https://static-logos.test.com/example.png"
     assert page.select("body > table > tr > td[bgcolor='#f00']")  # banner colour is set
     assert not page.select("body > table table > tr > td > span") == 0  # brand text is not set
+
+
+def test_displays_custom_brand_through_query_params(client_request, mocker):
+    mock_get_email_branding = mocker.patch("app.email_branding_client.get_email_branding")
+
+    page = client_request.get(
+        "main.email_template",
+        _test_page_title=False,
+        branding_style="custom",
+        text="Some text",
+        colour="#abcdef",
+        logo="example.png",
+        type="org",
+    )
+
+    assert mock_get_email_branding.called is False
+    assert not any(a["href"] == "https://www.gov.uk" for a in page.select("a"))
+    assert page.select_one("img")["src"] == "https://static-logos.test.com/example.png"
+    assert not page.select("body > table > tr > td[bgcolor='#abcdef']")
+    assert (
+        page.select("body > table:nth-of-type(1) > tr:nth-of-type(1) > td:nth-of-type(2)")[0].get_text().strip()
+        == "Some text"
+    )
