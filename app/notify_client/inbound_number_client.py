4987638a506c44b1bf02f854b2cac04fc383ac21
Katie Smith <katie.smith@digital.cabinet-office.gov.uk>
2022-12-02 12:02:44 +0000

    Add `add_inbound_number_to_service` method to `InboundNumberClient`
    We've added a new endpoint in notifications-api for assigning an inbound
    number to a service. This adds a method to the `InboundNumberClient`
    call the new endpoint.

--- app/notify_client/inbound_number_client.py
+++ app/notify_client/inbound_number_client.py
@@ -1,4 +1,4 @@
-from app.notify_client import NotifyAdminAPIClient
+from app.notify_client import NotifyAdminAPIClient, cache
 
 
 class InboundNumberClient(NotifyAdminAPIClient):
@@ -10,6 +10,16 @@ class InboundNumberClient(NotifyAdminAPIClient):
 
     def get_inbound_sms_number_for_service(self, service_id):
         return self.get("/inbound-number/service/{}".format(service_id))
+
+    @cache.delete("service-{service_id}")
+    @cache.delete_by_pattern("service-{service_id}-template-*")
+    def add_inbound_number_to_service(self, service_id, inbound_number_id=None):
+        data = {}
+
+        if inbound_number_id:
+            data["inbound_number_id"] = inbound_number_id
+
+        return self.post(f"inbound-number/service/{service_id}", data=data)
 
 
 inbound_number_client = InboundNumberClient()
