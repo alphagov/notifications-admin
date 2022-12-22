ca9a9aba075d3b46d8d74b50e462042fdd695a20
Chris Hill-Scott <me@quis.cc>
2022-12-22 12:13:49 +0000

    Add our own page template
    Rather than reusing the normal service settings template, lets make our
    own where we can customise the content.

--- app/main/views/make_service_live.py
+++ app/main/views/make_service_live.py
@@ -14,14 +14,14 @@ def make_service_live(service_id):
     if not current_user.can_make_service_live(current_service):
         abort(403)
 
-    form = OnOffSettingForm(name="Make service live", enabled=not current_service.trial_mode)
+    form = OnOffSettingForm(name="Make service live")
 
     if form.validate_on_submit():
         current_service.update_status(live=form.enabled.data)
         return redirect(url_for(".organisation_dashboard", org_id=current_service.organisation_id))
 
     return render_template(
-        "views/service-settings/set-service-setting.html",
+        "views/make-service-live.html",
         form=form,
         title="Make service live",
     )
