63f740b3c028a4f7627546456ea09c6e0d9e1a3f
Chris Hill-Scott <me@quis.cc>
2023-01-04 16:48:15 +0000

    Fix next review date of accessibility statement
    This makes sure it’s in the future

--- tests/app/test_accessibility_statement.py
+++ tests/app/test_accessibility_statement.py
@@ -11,15 +11,19 @@ def test_last_review_date():
         [f"git diff --exit-code origin/main -- {statement_file_path}"], stdout=subprocess.PIPE, shell=True
     )
 
+    date_format = "%d %B %Y"
+
     # if statement has changed, test the review date was part of those changes
     if statement_diff.returncode == 1:
         raw_diff = statement_diff.stdout.decode("utf-8")
-        today = datetime.now().strftime("%d %B %Y")
+        today = datetime.now().strftime(date_format)
         with open(statement_file_path, "r") as statement_file:
-            current_review_date = re.search(
-                (r'"Last updated": "(\d{1,2} [A-Z]{1}[a-z]+ \d{4})"'), statement_file.read()
-            ).group(1)
+            contents = statement_file.read()
+            current_review_date = re.search((r'"Last updated": "(\d{1,2} [A-Z]{1}[a-z]+ \d{4})"'), contents).group(1)
+            next_review_due = re.search((r'"Next review due": "(\d{1,2} [A-Z]{1}[a-z]+ \d{4})"'), contents).group(1)
 
         # guard against changes that don't need to update the review date
         if current_review_date != today:
             assert '"Last updated": "' in raw_diff
+
+        assert datetime.strptime(next_review_due, date_format) > datetime.strptime(current_review_date, date_format)
