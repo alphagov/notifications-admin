da3b0ef58e72aca8e409a22348868d02b664dba2
karlchillmaid <karl.chillmaid@digital.cabinet-office.gov.uk>
2023-01-25 12:46:26 +0000

    Add delivery times

--- tests/app/test_navigation.py
+++ tests/app/test_navigation.py
@@ -146,6 +146,7 @@ EXCLUDED_ENDPOINTS = tuple(
             "go_to_dashboard_after_tour",
             "guest_list",
             "guidance_bulk_sending",
+            "guidance_delivery_times",
             "guidance_email_branding",
             "guidance_edit_and_format_messages",
             "guidance_index",
