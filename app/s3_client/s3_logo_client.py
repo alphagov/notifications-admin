037b374c28522ab82a2647ffe8b5a91614759c48
Samuel Williams <samuelhwilliams@gmail.com>
2023-02-03 12:13:57 +0000

    Remove old s3_logoo_client file

--- app/s3_client/s3_logo_client.py
+++
@@ -1,6 +0,0 @@
-EMAIL_LOGO_LOCATION_STRUCTURE = "{temp}{unique_id}-{filename}"
-LETTER_PREFIX = "letters/static/images/letter-template/"
-
-
-def permanent_letter_logo_name(filename, extension):
-    return LETTER_PREFIX + filename + "." + extension
