d02522efd4678fdfc70f962efc50cb9f3ba3c183
Leo Hemsted <leo.hemsted@digital.cabinet-office.gov.uk>
2023-02-17 14:35:01 +0000

    add left hand nav to performance page

--- app/main/views/performance.py
+++ app/main/views/performance.py
@@ -7,6 +7,7 @@ from flask import render_template
 
 from app import performance_dashboard_api_client, status_api_client
 from app.main import main
+from app.main.views.sub_navigation_dictionaries import features_nav
 
 
 @main.route("/features/performance")
@@ -32,4 +33,8 @@ def performance():
         [row["percentage_under_10_seconds"] for row in stats["processing_time"]] or [0]
     )
     stats["count_of_live_services_and_organisations"] = status_api_client.get_count_of_live_services_and_organisations()
-    return render_template("views/guidance/features/performance.html", **stats)
+    return render_template(
+        "views/guidance/features/performance.html",
+        **stats,
+        navigation_links=features_nav(),
+    )
