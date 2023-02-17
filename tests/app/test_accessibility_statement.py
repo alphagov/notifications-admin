e78360ddf12acfbb43e6c7e23bf587c920cb0d7e
Leo Hemsted <leo.hemsted@digital.cabinet-office.gov.uk>
2023-02-17 16:35:07 +0000

    let accessibility_statement test handle non-functional changes to the file
    previously it woudl error if you didn't update the last updated at
    timestamp, even if you hadn't changed the doc itself and hadn't actually
    assessed the statement.
    
    Now there's a timestamp in a jinja comment for when the last
    non-functional change was. You must update either this or the last
    updated at timestamp. Still not perfect but a bit less brittle.
    
    Also call git diff with `--unified=0` to not return any context, so we
    aren't getting false positives on context lines of the git diff

--- tests/app/test_accessibility_statement.py
+++ tests/app/test_accessibility_statement.py
@@ -8,7 +8,7 @@ def test_last_review_date():
 
     # test local changes against main for a full diff of what will be merged
     statement_diff = subprocess.run(
-        [f"git diff --exit-code origin/main -- {statement_file_path}"], stdout=subprocess.PIPE, shell=True
+        [f"git diff --unified=0 --exit-code origin/main -- {statement_file_path}"], stdout=subprocess.PIPE, shell=True
     )
 
     date_format = "%d %B %Y"
@@ -16,14 +16,19 @@ def test_last_review_date():
     # if statement has changed, test the review date was part of those changes
     if statement_diff.returncode == 1:
         raw_diff = statement_diff.stdout.decode("utf-8")
-        today = datetime.now().strftime(date_format)
         with open(statement_file_path, "r") as statement_file:
             contents = statement_file.read()
-            current_review_date = re.search((r'"Last updated": "(\d{1,2} [A-Z]{1}[a-z]+ \d{4})"'), contents).group(1)
-            next_review_due = re.search((r'"Next review due": "(\d{1,2} [A-Z]{1}[a-z]+ \d{4})"'), contents).group(1)
+            last_content_change_at = datetime.strptime(
+                re.search((r'"Last updated": "(\d{1,2} [A-Z]{1}[a-z]+ \d{4})"'), contents).group(1), date_format
+            )
+            next_review_due = datetime.strptime(
+                re.search((r'"Next review due": "(\d{1,2} [A-Z]{1}[a-z]+ \d{4})"'), contents).group(1), date_format
+            )
+        assert '"Last updated": "' in raw_diff or "Last non-functional changes" in raw_diff, (
+            "Ensure the last updated timestamp or the Last non-functional changes timestmap has been modified:"
+            + raw_diff
+        )
 
-        # guard against changes that don't need to update the review date
-        if current_review_date != today:
-            assert '"Last updated": "' in raw_diff
-
-        assert datetime.strptime(next_review_due, date_format) > datetime.strptime(current_review_date, date_format)
+        assert last_content_change_at < datetime.today()
+        assert next_review_due > datetime.today()
+        assert next_review_due > last_content_change_at
