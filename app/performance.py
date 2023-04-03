1d81242b62ee9f611eb6dbcb876df8a1befa9d72
Samuel Williams <samuelhwilliams@gmail.com>
2023-04-03 12:42:10 +0100

    Remove NewRelic

--- app/performance.py
+++ app/performance.py
@@ -16,21 +16,12 @@ def sentry_sampler(sampling_context):
 
 
 def init_performance_monitoring():
-    environment = os.getenv("NOTIFY_ENVIRONMENT").lower()
-    enable_apm = environment in {"development", "preview", "staging"}
-
-    if enable_apm and os.getenv("NEW_RELIC_ENABLED") == "1":
-        import newrelic.agent
-
-        # Expects NEW_RELIC_LICENSE_KEY set in environment as well.
-        newrelic.agent.initialize("newrelic.ini", environment=environment, ignore_errors=False)
-
     if sentry_dsn := os.getenv("SENTRY_DSN"):
         import sentry_sdk
 
         sentry_sdk.init(
             dsn=sentry_dsn,
-            environment=environment,
+            environment=os.getenv("NOTIFY_ENVIRONMENT").lower(),
             debug=False,
             # Disable options while we're only testing the performance monitoring
             # traces_sample_rate=float(os.getenv("SENTRY_SAMPLE_RATE", 0.01)),
