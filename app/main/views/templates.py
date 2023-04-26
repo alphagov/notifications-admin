1272ce7c7a2d2acba28a1c6289c3e40ab35f5828
Leo Hemsted <leo.hemsted@digital.cabinet-office.gov.uk>
2023-04-26 11:00:11 +0100

    move form handling to a separate function
    this commit won't actually work properly, since sometimes the function
    doesn't return a redirect or renderable page, but instead just adds
    error messages to the form. this just gets all the large whitespace diff
    out of the way to make future diffs easier to look at

--- app/main/views/templates.py
+++ app/main/views/templates.py
@@ -926,96 +926,7 @@ def letter_template_attach_pages(service_id, template_id):
     error = {}
 
     if form.validate_on_submit():
-        pdf_file_bytes = form.file.data.read()
-        original_filename = form.file.data.filename
-
-        if len(pdf_file_bytes) > MAX_FILE_UPLOAD_SIZE:
-            return _invalid_upload_error(
-                template_id=template_id,
-                error={
-                    "title": "Your file is too big",
-                    "detail": "Files must be smaller than 2MB.",
-                },
-            )
-
-        try:
-            # TODO: get page count from the sanitise response once template preview
-            # handles malformed files nicely - is this done yet?
-            attachment_page_count = pdf_page_count(BytesIO(pdf_file_bytes))
-        except PdfReadError:
-            current_app.logger.info(f"Invalid PDF uploaded for service_id: {service_id}")
-            return _invalid_upload_error(
-                template_id=template_id,
-                error={
-                    "title": "There’s a problem with your file",
-                    "detail": "Notify cannot read this PDF.<br>Save a new copy of your file and try again.",
-                },
-            )
-
-        upload_id = uuid.uuid4()
-        file_location = get_transient_letter_file_location(service_id, upload_id)
-
-        try:
-            response = sanitise_letter(
-                BytesIO(pdf_file_bytes),
-                upload_id=upload_id,
-                allow_international_letters=current_service.has_permission("international_letters"),
-                is_an_attachment=True,
-            )
-            response.raise_for_status()
-        except RequestException as ex:
-            if ex.response is not None and ex.response.status_code == 400:
-                validation_failed_message = response.json().get("message")
-                invalid_pages = response.json().get("invalid_pages")
-
-                status = "invalid"
-                upload_letter_to_s3(
-                    pdf_file_bytes,
-                    file_location=file_location,
-                    status=status,
-                    page_count=attachment_page_count,
-                    filename=original_filename,
-                    message=validation_failed_message,
-                    invalid_pages=invalid_pages,
-                )
-                return _invalid_upload_error(
-                    template_id=template_id,
-                    error=get_letter_validation_error(validation_failed_message, invalid_pages, attachment_page_count),
-                )
-
-            raise
-
-        template_page_count = get_page_count_for_letter(template)
-        if attachment_page_count + template_page_count <= 10:
-            try:
-                _save_letter_attachment(
-                    service_id=service_id,
-                    template_id=template_id,
-                    upload_id=upload_id,
-                    original_filename=original_filename,
-                    sanitise_response=response,
-                )
-                return redirect(
-                    url_for(
-                        "main.view_template",
-                        service_id=current_service.id,
-                        template_id=template_id,
-                    )
-                )
-            except HTTPError as e:
-                if e.status_code == 400 and e.message == "template-already-has-attachment":
-                    # TODO: decide on content. this might depend on what the management page looks like (as we might
-                    # want to redirect to the existing  to show the user the attachment that has already been added)
-                    form.file.errors.append("This template already has an attachment.")
-                else:
-                    raise
-
-        else:
-            form.file.errors.append(
-                "Letters must be 10 pages or less (5 double-sided sheets of paper). "
-                "In total, your letter template and the file you attached are "
-                f"{template_page_count + attachment_page_count} pages long."
-            )
+        return _process_letter_attachment_form(service_id, template, form)
 
     if form.file.errors:
         error = get_error_from_upload_form(form.file.errors[0])
@@ -1024,6 +935,100 @@ def letter_template_attach_pages(service_id, template_id):
         render_template("views/templates/attach-pages.html", form=form, template_id=template_id, error=error),
         400 if error else 200,
     )
+
+
+def _process_letter_attachment_form(service_id, template, form):
+    template_id = template["id"]
+    pdf_file_bytes = form.file.data.read()
+    original_filename = form.file.data.filename
+
+    if len(pdf_file_bytes) > MAX_FILE_UPLOAD_SIZE:
+        return _invalid_upload_error(
+            template_id=template_id,
+            error={
+                "title": "Your file is too big",
+                "detail": "Files must be smaller than 2MB.",
+            },
+        )
+
+    try:
+        # TODO: get page count from the sanitise response once template preview
+        # handles malformed files nicely - is this done yet?
+        attachment_page_count = pdf_page_count(BytesIO(pdf_file_bytes))
+    except PdfReadError:
+        current_app.logger.info(f"Invalid PDF uploaded for service_id: {service_id}")
+        return _invalid_upload_error(
+            template_id=template_id,
+            error={
+                "title": "There’s a problem with your file",
+                "detail": "Notify cannot read this PDF.<br>Save a new copy of your file and try again.",
+            },
+        )
+
+    upload_id = uuid.uuid4()
+    file_location = get_transient_letter_file_location(service_id, upload_id)
+
+    try:
+        response = sanitise_letter(
+            BytesIO(pdf_file_bytes),
+            upload_id=upload_id,
+            allow_international_letters=current_service.has_permission("international_letters"),
+            is_an_attachment=True,
+        )
+        response.raise_for_status()
+    except RequestException as ex:
+        if ex.response is not None and ex.response.status_code == 400:
+            validation_failed_message = response.json().get("message")
+            invalid_pages = response.json().get("invalid_pages")
+
+            status = "invalid"
+            upload_letter_to_s3(
+                pdf_file_bytes,
+                file_location=file_location,
+                status=status,
+                page_count=attachment_page_count,
+                filename=original_filename,
+                message=validation_failed_message,
+                invalid_pages=invalid_pages,
+            )
+            return _invalid_upload_error(
+                template_id=template_id,
+                error=get_letter_validation_error(validation_failed_message, invalid_pages, attachment_page_count),
+            )
+
+        raise
+
+    template_page_count = get_page_count_for_letter(template)
+    if attachment_page_count + template_page_count <= 10:
+        try:
+            _save_letter_attachment(
+                service_id=service_id,
+                template_id=template_id,
+                upload_id=upload_id,
+                original_filename=original_filename,
+                sanitise_response=response,
+            )
+            return redirect(
+                url_for(
+                    "main.view_template",
+                    service_id=current_service.id,
+                    template_id=template_id,
+                )
+            )
+        except HTTPError as e:
+            if e.status_code == 400 and e.message == "template-already-has-attachment":
+                # TODO: decide on content. this might depend on what the management page looks like (as we might
+                # want to redirect to the existing  to show the user the attachment that has already been added)
+                form.file.errors.append("This template already has an attachment.")
+            else:
+                raise
+
+    else:
+        form.file.errors.append(
+            "Letters must be 10 pages or less (5 double-sided sheets of paper). "
+            "In total, your letter template and the file you attached are "
+            f"{template_page_count + attachment_page_count} pages long."
+        )
 
 
 def _save_letter_attachment(*, service_id, template_id, upload_id, original_filename, sanitise_response):
