ed3d69c5b47a0a561571f4a4eb397a4d595cc7a2
Samuel Williams <samuelhwilliams@gmail.com>
2023-08-01 20:09:36 +0100

    Nvm build

--- start-codespace.sh
+++ start-codespace.sh
@@ -5,11 +5,14 @@ if [[ "${CODESPACES}" != "true" ]]; then
   exit 1
 fi
 
+source ${NVM_DIR}/nvm.sh
+
 make generate-version-file
+nvm install; nvm use; npm install; npm run build
 (cd ../notifications-api; make generate-version-file)
 (cd ../notifications-template-preview; make generate-version-file)
 (cd ../notifications-antivirus; make generate-version-file)
 (cd ../document-download-api; make generate-version-file)
-(cd ../document-download-frontend; make generate-version-file)
+(cd ../document-download-frontend; make generate-version-file; nvm install; nvm use; npm install; npm run build)
 
 (cd ../notifications-local; ./generate-env-files.sh; docker-compose build; make up)
