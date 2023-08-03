06c61de57a84b04d2f43861be066332e9bff02f9
Roberto Gaidarji <roberto.gaidarji@digital.cabinet-office.gov.uk>
2023-08-03 16:29:59 +0100

    Added an IF statement to manifest.yml.j2 to check if the REDIS_URL is defined Added and IF statement to cloudfoundry_config.py to check if the REDIS_URL variable is not in the env. In this case the app will use the VCAP_SERVICES value

--- app/cloudfoundry_config.py
+++ app/cloudfoundry_config.py
@@ -4,4 +4,6 @@ import os
 
 def extract_cloudfoundry_config():
     vcap_services = json.loads(os.environ["VCAP_SERVICES"])
-    os.environ["REDIS_URL"] = vcap_services["redis"][0]["credentials"]["uri"]
+    # redis config
+    if "REDIS_URL" not in os.environ:
+        os.environ["REDIS_URL"] = vcap_services["redis"][0]["credentials"]["uri"]
