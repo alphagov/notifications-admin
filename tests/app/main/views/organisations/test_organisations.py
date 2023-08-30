266be1697095810cbe976512c9abaa3aa5e55cdd
Chris Hill-Scott <me@quis.cc>
2023-08-30 11:48:33 +0100

    Make font size smaller when numbers are very big
    We have a problem where big numbers overflow the width of their columns.
    This happens when the numbers are bigger than we anticipated (for
    example when we designed the page we never imagined anyone would spend
    tens of millions of £ on text messages in a single year).
    
    This happens in 3 places:
    - at the bottom of the service dashboard, which links to…
    - the usage page for a service
    - the usage page for an organisation
    
    This commit adds some logic to reduce the font size for all the totals
    if any of them get so big that they overflow the width of the column.
    This gives us enouigh space to display the biggest numbers we currently
    store in the platform.
    
    Because we use tabular numbers for these elements we don’t need to worry
    about the width of 1,111,111 versus 9,999,999.
    
    A function which takes 2 arguments, `smaller` and `smallest` isn’t a
    great piece of API design, but if we want to refactor that it should be
    as a separate PR. This commit just works within the confines of the
    existing argument names.
    
    Similarly we can’t wrap this logic up in the `big_number` component
    because it doesn’t know about:
    - the width of the column in which it’s shown
    - whether other numbers on the page are too big

--- tests/app/main/views/organisations/test_organisations.py
+++ tests/app/main/views/organisations/test_organisations.py
@@ -526,6 +526,71 @@ def test_organisation_services_shows_live_services_and_usage_with_count_of_1(
     assert normalize_spaces(usage_rows[5].text) == "£0.00 spent on letters"
 
 
+@pytest.mark.parametrize(
+    "service_usage, expected_css_class",
+    (
+        (
+            {"emails_sent": 999_999_999, "sms_cost": 0, "letter_cost": 0},
+            ".big-number-smaller",
+        ),
+        (
+            {"emails_sent": 1_000_000_000, "sms_cost": 0, "letter_cost": 0},
+            ".big-number-smallest",
+        ),
+        (
+            {"emails_sent": 0, "sms_cost": 999_999, "letter_cost": 0},
+            ".big-number-smaller",
+        ),
+        (
+            {"emails_sent": 0, "sms_cost": 1_000_000, "letter_cost": 0},
+            ".big-number-smallest",
+        ),
+        (
+            {"emails_sent": 0, "sms_cost": 0, "letter_cost": 999_999},
+            ".big-number-smaller",
+        ),
+        (
+            {"emails_sent": 0, "sms_cost": 0, "letter_cost": 1_000_000},
+            ".big-number-smallest",
+        ),
+    ),
+)
+@freeze_time("2020-02-20 20:20")
+def test_organisation_services_shows_usage_in_correct_font_size(
+    client_request,
+    mock_get_organisation,
+    mocker,
+    active_user_with_permissions,
+    fake_uuid,
+    service_usage,
+    expected_css_class,
+):
+    mocker.patch(
+        "app.organisations_client.get_services_and_usage",
+        return_value={
+            "services": [
+                service_usage
+                | {
+                    "service_id": SERVICE_ONE_ID,
+                    "service_name": "1",
+                    "chargeable_billable_sms": 1,
+                    "free_sms_limit": 250000,
+                    "sms_billable_units": 1,
+                    "sms_remainder": None,
+                },
+            ],
+            "updated_at": None,
+        },
+    )
+
+    client_request.login(active_user_with_permissions)
+    page = client_request.get(".organisation_dashboard", org_id=ORGANISATION_ID)
+
+    usage_totals = page.select_one("main .govuk-grid-row").select(expected_css_class)
+
+    assert len(usage_totals) == 3
+
+
 @freeze_time("2020-02-20 20:20")
 @pytest.mark.parametrize(
     "financial_year, expected_selected",
