afdf04148ba30726cdf4db61b882d628032d0747
Tom Byers <tombaromba@gmail.com>
2023-08-30 10:12:58 +0100

    Update radio-select to new version
    Tries to fix these issues raised against the
    current version:
    - single radio at start of interactions is
      confusing
    - focus jumping around is disorienting for screen
      reader users
    - selection of a radio also causing a change of
      content is disorienting
    
    We did a lot of prototyping of different potential
    versions: https://time-and-day-pickers.glitch.me/
    
    From this, we chose idea 9 as something to build
    and try in place of the existing version because
    it:
    - keeps all radios in one place
    - has a confirmation step for a radio selection
    - shows and hides the radios without moving focus,
      except where this is clearly signposted
    - is a bit like the mini-form/dialog we have for
      selecting the type of template when starting a
      new one so might build on learnt behaviour

--- app/assets/javascripts/radioSelect.js
+++ app/assets/javascripts/radioSelect.js
@@ -3,212 +3,238 @@
   "use strict";
 
   var Modules = global.GOVUK.NotifyModules;
-  var Hogan = global.Hogan;
 
   // Object holding all the states for the component's HTML
-  let states = {
-    'initial': Hogan.compile(`
-      {{#showNowAsDefault}}
-        <div class="radio-select__column">
-          <div class="govuk-radios__item">
-            <input class="govuk-radios__input" checked="checked" id="{{name}}-0" name="{{name}}" type="radio" value="">
-            <label class="govuk-label govuk-radios__label" for="{{name}}-0">Now</label>
-          </div>
-        </div>
-      {{/showNowAsDefault}}
-      <div class="radio-select__column">
-        {{#categories}}
-          <input type='button' class='govuk-button govuk-button--secondary radio-select__button--category' aria-expanded="false" value='{{.}}' />
-        {{/categories}}
-      </div>
-    `),
-    'choose': Hogan.compile(`
-      {{#showNowAsDefault}}
-        <div class="radio-select__column">
-          <div class="govuk-radios__item">
-            <input class="govuk-radios__input" checked="checked" id="{{name}}-0" name="{{name}}" type="radio" value="">
-            <label class="govuk-label govuk-radios__label" for="{{name}}-0">Now</label>
-          </div>
-        </div>
-      {{/showNowAsDefault}}
-      <div class="radio-select__column">
-        {{#choices}}
-          <div class="govuk-radios__item js-option">
-            <input class="govuk-radios__input" type="radio" value="{{value}}" id="{{id}}" name="{{name}}" />
-            <label class="govuk-label govuk-radios__label" for="{{id}}">{{label}}</label>
-          </div>
-        {{/choices}}
-        <input type='button' class='govuk-button govuk-button--secondary radio-select__button--done' aria-expanded='true' value='Done' />
-      </div>
-    `),
-    'chosen': Hogan.compile(`
-      {{#showNowAsDefault}}
-        <div class="radio-select__column">
-          <div class="govuk-radios__item">
-            <input class="govuk-radios__input" id="{{name}}-0" name="{{name}}" type="radio" value="">
-            <label class="govuk-label govuk-radios__label" for="{{name}}-0">Now</label>
-          </div>
-        </div>
-      {{/showNowAsDefault}}
-      <div class="radio-select__column">
-        {{#choices}}
-          <div class="govuk-radios__item">
-            <input class="govuk-radios__input" checked="checked" type="radio" value="{{value}}" id="{{id}}" name="{{name}}" />
-            <label class="govuk-label govuk-radios__label" for="{{id}}">{{label}}</label>
-          </div>
-        {{/choices}}
-      </div>
-      <div class="radio-select__column">
-        <input type='button' class='govuk-button govuk-button--secondary radio-select__button--reset' aria-expanded='false' value='Choose a different time' />
-      </div>
-    `)
-  };
+  function getInitialHTML (params) {
 
-  let shiftFocus = function(elementToFocus, component) {
-    // The first option is always the default
-    if (elementToFocus === 'default') {
-      $('[type=radio]', component).eq(0).focus();
-    }
-    if (elementToFocus === 'option') {
-      $('[type=radio]', component).eq(1).focus();
-    }
-  };
+    return `
+      <label for="radio-select__selected-day-and-time" class="govuk-label radio-select__label">${params.componentLabel}</label>
+      <div class="radio-select__selection-and-button">
+        <input type="text" class="radio-select__selected-day-and-time" id="radio-select__selected-day-and-time" readonly value="${params.selectedTime.label}">
+        <input type="hidden" class="radio-select__selected-value" value="${params.selectedTime.value}" name="${params.componentName}">
+        <div class="radio-select__expander-and-expandee">
+          <button type='button' class='govuk-button govuk-button--secondary radio-select__expander' aria-expanded="false" >Choose another time</button>
+          <div class="radio-select__expandee" hidden>
+            <div class="radio-select__column">
+              <fieldset class="govuk-fieldset radio-select__days">
+                <legend class="govuk-visually-hidden">Day to send these messages</legend>
+                ${params.days.map((day, idx) => `
+                  <button class="govuk-button govuk-button--secondary radio-select__day" type="button" name="${day.value}" value="${day.value}">
+                    ${day.label}
+                  </button>`
+                ).join('')}
+              </fieldset>
+            </div>
+            <div class="radio-select__column" hidden>
+              <a href="" class="govuk-link govuk-back-link radio-select__return-to-days js-header">Back to days</a>
+              ${params.days.map((day, idx) => `
+                <fieldset class="govuk-fieldset radio-select__times" aria-describedby="radio-select__times-description" id="radio-select__times-for-${day.value}" hidden>
+                  <legend class="govuk-visually-hidden">Time to send these messages</legend>
+                  <p class="govuk-visually-hidden" id="radio-select__times-description">Choose a time and press confirm or cancel</p>
+                  ${params.times[day.value].map((time, idx) => `
+                    <div class="govuk-radios__item">
+                      <input class="govuk-radios__input radio-select__time" type="radio" value="${time.value}" id="${time.id}" name="${time.name}"${time.checked ? ' checked' : ''} />
+                      <label class="govuk-label govuk-radios__label" for="${time.id}">${time.label}</label>
+                    </div>`
+                  ).join('')}
+              </fieldset>`
+              ).join('')}
+              <div class="radio-select__confirm js-stick-at-bottom-when-scrolling">
+                <button type="button" class="govuk-button radio-select__confirm__button">Confirm time</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    `;
+  }
 
   Modules.RadioSelect = function() {
 
     this.start = function(component) {
 
-      let $component = $(component);
-      let render = (state, data) => {
-        $component.html(states[state].render(data));
-      };
-      // store array of all options in component
-      let choices = $('label', $component).toArray().map(function(element) {
-        let $element = $(element);
+      this.$component = $(component);
+      this.confirmMadeSticky = false;
+
+      function _getTimeFromRadio (radio) {
         return {
-          'id': $element.attr('for'),
-          'label': $.trim($element.text()),
-          'value': $element.prev('input').attr('value')
-        };
-      });
-      let categories = $component.data('categories').split(',');
-      let name = $component.find('input').eq(0).attr('name');
-      let mousedownOption = null;
-      let showNowAsDefault = (
-        $component.data('show-now-as-default').toString() === 'true' ?
-        {'name': name} : false
-      );
-
-      // functions for changing the state of the component's HTML
-      const reset = () => {
-        render('initial', {
-          'categories': categories,
-          'name': name,
-          'showNowAsDefault': showNowAsDefault
-        });
-        shiftFocus('default', component);
-      };
-      const selectOption = (value) => {
-        render('chosen', {
-          'choices': choices.filter(
-            element => element.value == value
-          ),
-          'name': name,
-          'showNowAsDefault': showNowAsDefault
-        });
-        shiftFocus('option', component);
-      };
-
-      // use mousedown + mouseup event sequence to confirm option selection
-      const trackMouseup = (event) => {
-        const parentNode = event.target.parentNode;
-
-        if (parentNode === mousedownOption) {
-          const value = $('input', parentNode).attr('value');
-
-          selectOption(value);
-
-          // clear tracking
-          mousedownOption = null;
-          $(document).off('mouseup', trackMouseup);
+          'value': radio.value,
+          'label': radio.nextElementSibling.textContent.trim()
         }
       };
 
-      // set events
-      $component
-        .on('click', '.radio-select__button--category', function(event) {
+      const timesByDay = {};
 
-          event.preventDefault();
-          let wordsInDay = $(this).attr('value').split(' ');
-          let day = wordsInDay[wordsInDay.length - 1].toLowerCase();
-          render('choose', {
-            'choices': choices.filter(
-              element => element.label.toLowerCase().indexOf(day) > -1
-            ),
-            'name': name,
-            'showNowAsDefault': showNowAsDefault
-          });
-          shiftFocus('option', component);
+      const days = this.$component.data('days').split(',').map(day => {
+        const dayValue = day.toLowerCase();
 
-        })
-        .on('mousedown', '.js-option', function(event) {
-          mousedownOption = this;
+        timesByDay[dayValue] = [];
 
-          // mouseup on the same option completes the click action
-          $(document).on('mouseup', trackMouseup);
-        })
-        // space and enter, clicked on a radio confirm that option was selected
-        .on('keydown', 'input[type=radio]', function(event) {
-
-          // allow keypresses which aren’t enter or space through
-          if (event.which !== 13 && event.which !== 32) {
-            return true;
-          }
-
-          event.preventDefault();
-          let value = $(this).attr('value');
-          selectOption(value);
-
-        })
-        .on('click', '.radio-select__button--done', function(event) {
-
-          event.preventDefault();
-          let $selection = $('input[type=radio]:checked', this.parentNode);
-          if ($selection.length) {
-
-            render('chosen', {
-              'choices': choices.filter(
-                element => element.value == $selection.eq(0).attr('value')
-              ),
-              'name': name,
-              'showNowAsDefault': showNowAsDefault
-            });
-            shiftFocus('option', component);
-
-          } else {
-
-            reset();
-            shiftFocus('default', component);
-
-          }
-
-        })
-        .on('click', '.radio-select__button--reset', function(event) {
-
-          event.preventDefault();
-          reset();
-          shiftFocus('default', component);
-
-        });
-
-      // set HTML to initial state
-      render('initial', {
-        'categories': categories,
-        'name': name,
-        'showNowAsDefault': showNowAsDefault
+        return {
+          'value': dayValue,
+          'label': day
+        }
       });
 
-      $component.css({'height': 'auto'});
+      const componentName = this.$component.find('input[type=radio]').attr('name');
+
+      this.$component.find('label').each(function (idx, label) {
+        const labelText = label.textContent.trim();
+        const relatedRadio = label.previousElementSibling;
+        const timeData = {
+          'id': label.getAttribute('for'),
+          'label': labelText,
+          'value': relatedRadio.value
+        };
+        let day = labelText.split(' ')[0].toLowerCase();
+
+        if (idx === 0) { // Store the first time
+          this.selectedTime = _getTimeFromRadio(relatedRadio);
+
+          // The first time's label doesn't contain its day so use the first day
+          day = days[0].value;
+        }
+
+        timeData.name = `times-for-${day}`;
+        timesByDay[day].push(timeData);
+
+      }.bind(this));
+
+      this.focusContainer = function ($container) {
+        const onBlurContainer = evt => {
+          $container
+            .removeAttr('tabindex')
+            .off('blur', onBlurContainer);
+        };
+
+        $container
+          .attr('tabindex', 0)
+          .focus()
+          .on('blur', onBlurContainer);
+      };
+
+      this.showDaysView = function () {
+        const viewPanes = this.$component.find('.radio-select__expandee .radio-select__column');
+
+        viewPanes.get(0).removeAttribute('hidden');
+        viewPanes.get(1).setAttribute('hidden', '');
+      };
+
+      this.showTimesView = function () {
+        const viewPanes = this.$component.find('.radio-select__expandee .radio-select__column');
+
+        viewPanes.get(0).setAttribute('hidden', '');
+        viewPanes.get(1).removeAttribute('hidden');
+
+        GOVUK.stickAtBottomWhenScrolling.recalculate();
+      };
+
+      this.showTimesForDay = function (day) {
+        this.$component.find('.radio-select__expandee .radio-select__times').each((idx, timesGroup) => {
+          if (timesGroup.id === `radio-select__times-for-${day}`) {
+            timesGroup.removeAttribute('hidden');
+          } else {
+            timesGroup.setAttribute('hidden', '');
+          }
+        });
+      };
+
+      this.expandOrCollapseSection = function (state) {
+        const expander = this.$component.find('.radio-select__expander').get(0);
+        const expandee = this.$component.find('.radio-select__expandee').get(0);
+
+        if (state === 'expand') {
+          expander.setAttribute('aria-expanded', 'true');
+          expandee.removeAttribute('hidden');
+        } else {
+          expander.setAttribute('aria-expanded', 'false');
+          expandee.setAttribute('hidden', '');
+        }
+      };
+
+      this.updateSelection = function () {
+        this.$component.find('.radio-select__selected-day-and-time').val(this.selectedTime.label);
+        this.$component.find('.radio-select__selected-value').val(this.selectedTime.value);
+      };
+
+      this.onConfirmClick = function (event) {
+        this.selectedTime = _getTimeFromRadio(
+          this.$component.find('.radio-select__times:not([hidden]) .radio-select__time:checked').get(0)
+        );
+
+        if (this.selectedTime.length === 0) { return; }
+
+        // reset state of expanding section for selecting a day + time
+        this.showDaysView();
+        this.expandOrCollapseSection('collapse');
+        this.updateSelection();
+        this.$component.find('.radio-select__selected-day-and-time').focus();
+      };
+
+      this.onReturnToDaysClick = function (event) {
+        event.preventDefault();
+
+        this.showDaysView();
+        this.focusContainer(
+          this.$component.find('.radio-select__days').eq(0)
+        );
+      };
+
+      this.onDayClick = function (event) {
+        let scrollTop;
+
+        this.selectedDay = event.target.value.trim();
+
+        this.showTimesForDay(this.selectedDay);
+        this.showTimesView();
+
+        scrollTop = $(window).scrollTop(); // store current scroll position
+
+        this.focusContainer(
+          this.$component.find(`#radio-select__times-for-${this.selectedDay}`).eq(0)
+        );
+
+        $(window).scrollTop(scrollTop); // reset window to previous scroll position
+      };
+
+      this.onExpanderClick = function (event) {
+        const isExpanded = event.target.getAttribute('aria-expanded') === 'true';
+        event.preventDefault();
+
+        this.expandOrCollapseSection(
+          isExpanded ? 'collapse' : 'expand'
+        );
+      };
+
+      // uncheck any radios for other days already checked
+      // radios for different days don't share a name so selecting one doesn't deselect those
+      // selected for other days
+      this.onTimeSelection = function (event) {
+        this.$component.find('.radio-select__time:checked').each((idx, radio) => {
+          if (radio !== event.target) { radio.checked = false}
+        });
+      };
+
+      this.selectedDay = days[0];
+      this.selectedTime = timesByDay[this.selectedDay.value][0];
+
+      this.$component.html(getInitialHTML({
+        'componentLabel': this.$component.prev('legend').text().trim(),
+        'componentName': componentName,
+        'selectedTime': this.selectedTime,
+        'days': days,
+        'times': timesByDay
+      }));
+
+      this.$component.parent('fieldset').replaceWith(this.$component);
+
+      // set events
+      this.$component
+        .on('click', '.radio-select__expander', this.onExpanderClick.bind(this))
+        .on('click', '.radio-select__day', this.onDayClick.bind(this))
+        .on('click', '.radio-select__return-to-days', this.onReturnToDaysClick.bind(this))
+        .on('click', '.radio-select__confirm__button', this.onConfirmClick.bind(this))
+        .on('change', '.radio-select__time', this.onTimeSelection.bind(this));
 
     };
 
