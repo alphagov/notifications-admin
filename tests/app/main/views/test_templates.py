59ae24a78d3baf165ce368b0190bcc725dfb5c99
Pea Tyczynska <CrystalPea@users.noreply.github.com>
2023-09-11 16:15:29 +0100

    Rearrange and rename test for ease of reading

--- tests/app/main/views/test_templates.py
+++ tests/app/main/views/test_templates.py
@@ -842,7 +842,24 @@ def test_GET_letter_template_change_language(
     mock_get_service_letter_template.assert_called_once_with(SERVICE_ONE_ID, fake_uuid, None)
 
 
-def test_letter_template_change_language_to_welsh_then_english_populates_default_welsh_content_and_subject(
+def test_GET_letter_template_change_language_404s_if_template_is_not_a_letter(
+    client_request,
+    service_one,
+    mock_get_service_template,
+    active_user_with_permissions,
+    mocker,
+    fake_uuid,
+):
+    service_one["permissions"].append("extra_letter_formatting")
+    client_request.login(active_user_with_permissions)
+    page = client_request.get(
+        "main.letter_template_change_language", service_id=SERVICE_ONE_ID, template_id=fake_uuid, _expected_status=404
+    )
+
+    assert page.select_one("h1").text.strip() != "Change language"
+
+
+def test_POST_letter_template_change_language_to_welsh_then_english_populates_default_welsh_content_and_subject(
     client_request,
     service_one,
     mocker,
@@ -873,23 +890,6 @@ def test_letter_template_change_language_to_welsh_then_english_populates_default
         letter_welsh_subject="Welsh subject line goes here",
         letter_welsh_content="Welsh content goes here",
     )
-
-
-def test_letter_template_change_language_404s_if_template_is_not_a_letter(
-    client_request,
-    service_one,
-    mock_get_service_template,
-    active_user_with_permissions,
-    mocker,
-    fake_uuid,
-):
-    service_one["permissions"].append("extra_letter_formatting")
-    client_request.login(active_user_with_permissions)
-    page = client_request.get(
-        "main.letter_template_change_language", service_id=SERVICE_ONE_ID, template_id=fake_uuid, _expected_status=404
-    )
-
-    assert page.select_one("h1").text.strip() != "Change language"
 
 
 def test_GET_letter_template_attach_pages(
