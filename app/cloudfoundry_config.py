e0e94df0ef16c1300fcad5e993abfaf05b9faffe
David McDonald <david.mcdonald@digital.cabinet-office.gov.uk>
2023-11-06 18:33:05 +0000

    Remove `extract_cloudfoundry_config`
    We no longer need to provide the REDIS_URL from VCAP_SERVICES as
    it will always come directly from the `REDIS_URL` environment variable.
    Because this was the only use for the `extract_cloudfoundry_config`
    function, we can delete the entire function!
    
    I then couldn't figure out any value in having the test_config.py
    tests. They appear to only test a particular  one of our config values can
    be set based on the value of an environment variable. I wasn't sure
    this was worth keeping or not.

--- app/cloudfoundry_config.py
+++
@@ -1,9 +0,0 @@
-import json
-import os
-
-
-def extract_cloudfoundry_config():
-    vcap_services = json.loads(os.environ["VCAP_SERVICES"])
-    # redis config
-    if "REDIS_URL" not in os.environ:
-        os.environ["REDIS_URL"] = vcap_services["redis"][0]["credentials"]["uri"]
