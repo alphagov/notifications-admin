c8bdc900563cc764f93aff2e61a3854a9ce16132
Chris Hill-Scott <me@quis.cc>
2023-12-08 16:18:18 +0000

    Redirect to English preview after editing English content
    If you edit some content or add an attachment to a letter it’s good if
    you can see the change you have made immediately, without having to
    scroll.
    
    We do this for attachments, this makes it work the same way for
    bilingual letters. If you edit the English content in a bilingual letter
    we will jump you down the page to the preview of the English content
    when redirecting.

--- tests/app/main/views/test_templates.py
+++ tests/app/main/views/test_templates.py
@@ -1777,6 +1777,40 @@ def test_should_be_able_to_view_a_letter_template_with_links(
     ]
 
 
+def test_should_be_able_to_view_a_letter_template_with_bilingual_content(
+    mocker,
+    client_request,
+    service_one,
+    mock_get_service_letter_template_welsh_language,
+    mock_get_template_folders,
+    active_user_with_permissions,
+    single_letter_contact_block,
+    fake_uuid,
+):
+    service_one["permissions"].append("extra_letter_formatting")
+    do_mock_get_page_counts_for_letter(mocker, count=5, welsh_page_count=3)
+    page = client_request.get(
+        "main.view_template",
+        service_id=SERVICE_ONE_ID,
+        template_id=fake_uuid,
+        _test_page_title=False,
+    )
+    assert [page.get("id") for page in page.select(".letter")] == [
+        None,
+        None,
+        None,
+        "first-page-of-english-in-bilingual-letter",
+        None,
+    ]
+    assert [img["loading"] for img in page.select(".letter img")] == [
+        "eager",
+        "lazy",
+        "lazy",
+        "eager",
+        "lazy",
+    ]
+
+
 def test_view_broadcast_template(
     client_request,
     service_one,
@@ -3148,6 +3182,7 @@ def test_should_redirect_when_saving_a_template_email(
 def test_should_redirect_when_saving_a_template_letter(
     client_request,
     mock_get_service_letter_template,
+    mock_get_page_counts_for_letter,
     mock_update_service_template,
     mock_get_user_by_email,
     fake_uuid,
@@ -3232,6 +3267,7 @@ def test_update_template_for_welsh_language_content(
     request,
     client_request,
     mock_update_service_template,
+    mock_get_page_counts_for_letter,
     mock_get_user_by_email,
     fake_uuid,
     service_one,
@@ -3271,6 +3307,47 @@ def test_update_template_for_welsh_language_content(
         name=name,
         letter_welsh_subject=subject,
         letter_welsh_content=content,
+    )
+
+
+def test_update_template_for_english_content_in_welsh_letter(
+    mocker,
+    client_request,
+    mock_update_service_template,
+    mock_get_service_letter_template_welsh_language,
+    mock_get_user_by_email,
+    fake_uuid,
+    service_one,
+):
+    do_mock_get_page_counts_for_letter(mocker, count=1, welsh_page_count=1)
+    service_one["permissions"].append("letter")
+    service_one["permissions"].append("extra_letter_formatting")
+    name = "new template name"
+    content = "English letter content"
+    subject = "English letter subject"
+    client_request.post(
+        ".edit_service_template",
+        service_id=SERVICE_ONE_ID,
+        template_id=fake_uuid,
+        _data={
+            "name": name,
+            "subject": subject,
+            "template_content": content,
+        },
+        _expected_status=302,
+        _expected_redirect=url_for(
+            "main.view_template",
+            service_id=SERVICE_ONE_ID,
+            template_id=fake_uuid,
+            _anchor="first-page-of-english-in-bilingual-letter",
+        ),
+    )
+    mock_update_service_template.assert_called_with(
+        service_id=SERVICE_ONE_ID,
+        template_id=fake_uuid,
+        name=name,
+        subject=subject,
+        content=content,
     )
 
 
