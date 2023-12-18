2c20566c07d94b8986dd6bc9ead8968419f749da
Samuel Williams <samuelhwilliams@gmail.com>
2023-12-18 13:23:29 +0000

    Add 'sheets' back to table

---
+++ tests/app/main/views/test_pricing.py
@@ -0,0 +1,10 @@
+def test_guidance_pricing_letters(client_request):
+    page = client_request.get(".guidance_pricing_letters")
+
+    first_row = page.select_one("main table tbody tr")
+    assert "1 sheet" in first_row.text
+
+    # Update these values if letter prices change
+    assert "54p + VAT" in first_row.text
+    assert "82p + VAT" in first_row.text
+    assert "£1.26 + VAT" in first_row.text
