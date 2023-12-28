c227e6b5d98012a10616eb22c44055db19fa3fe8
Leo Hemsted <leo.hemsted@digital.cabinet-office.gov.uk>
2023-12-28 17:29:52 +0000

    add error message handling for when running outside of git
    makes it a bit clearer how to resolve if the test errors

--- tests/app/test_accessibility_statement.py
+++ tests/app/test_accessibility_statement.py
@@ -8,12 +8,19 @@ def test_last_review_date():
 
     # test local changes against main for a full diff of what will be merged
     statement_diff = subprocess.run(
-        [f"git diff --unified=0 --exit-code origin/main -- {statement_file_path}"], stdout=subprocess.PIPE, shell=True
+        [f"git diff --unified=0 --exit-code origin/main -- {statement_file_path}"],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        shell=True,
     )
 
+    assert not statement_diff.stderr, (
+        "Error comparing the accessiblity_statement.html file to origin/main - "
+        "make sure this test is run from a local git checkout"
+    )
     date_format = "%d %B %Y"
 
-    # if statement has changed, test the review date was part of those changes
+    # if statement has changed, make sure the review date was part of those changes
     if statement_diff.returncode == 1:
         raw_diff = statement_diff.stdout.decode("utf-8")
         with open(statement_file_path, "r") as statement_file:
