fb02c37ee216d280a3055fb6ec2cc181a7457616
Robert Scott <robert.scott@digital.cabinet-office.gov.uk>
2024-03-26 11:20:49 +0000

    TMP doh

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -2,7 +2,7 @@ from gds_metrics.gunicorn import child_exit  # noqa
 from notifications_utils.gunicorn_defaults import set_gunicorn_defaults
 
 
-set_gunicorn_defaults()
+set_gunicorn_defaults(globals())
 
 workers = 5
 worker_class = "eventlet"
