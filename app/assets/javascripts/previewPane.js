9fb54bbb6411b9c26b5972ed606a68e49037aa2c
Jani Kraner <jani.kraner@digital.cabinet-office.gov.uk>
2024-04-26 16:18:53 +0100

    Fix set-letter-branding preview javascript
    As we've changed the way we serve letter branding
    previews from iframe to image, we need to update
    the admin brand preview on
    set-letter-branding to serve them in an image as
    well.
    
    No change to email and sms brand previews

--- app/assets/javascripts/previewPane.js
+++ app/assets/javascripts/previewPane.js
@@ -13,7 +13,13 @@
   const $paneWrapper = $('<div class="govuk-grid-column-full"></div>');
   const $form = $('form');
   const previewType = $form.data('previewType');
-  const $previewPane = $(`<iframe src="/_${previewType}?${buildQueryString(['branding_style', branding_style])}" class="branding-preview" scrolling="no"></iframe>`);
+  const letterBrandingPreviewRootPath = `templates/${previewType}-preview-image`;
+  const $iframePreviewPane = $(`<iframe src="/_${previewType}?${buildQueryString(['branding_style', branding_style])}" class="branding-preview" scrolling="no"></iframe>`);
+  const $imagePreviewPane = $(
+    `<div class="branding-preview-image">
+      <img src="/${letterBrandingPreviewRootPath}?${buildQueryString(['branding_style', branding_style])}" alt="Preview of selected letter branding">
+    </div>`
+  );
 
   function buildQueryString () {
     return $.map(arguments, (val, idx) => encodeURI(val[0]) + '=' + encodeURI(val[1])).join('&');
@@ -24,10 +30,20 @@
     if ($target.attr('name') == 'branding_style') {
       branding_style = $target.val();
     }
-    $previewPane.attr('src', `/_${previewType}?${buildQueryString(['branding_style', branding_style])}`);
+
+    if (previewType === 'letter') {
+      $imagePreviewPane.find('img').attr('src', `/${letterBrandingPreviewRootPath}?${buildQueryString(['branding_style', branding_style])}`);
+    } else {
+      $iframePreviewPane.attr('src', `/_${previewType}?${buildQueryString(['branding_style', branding_style])}`);
+    }
   }
 
-  $paneWrapper.append($previewPane);
+  if (previewType === 'letter') {
+    $paneWrapper.append($imagePreviewPane);
+  } else {
+    $paneWrapper.append($iframePreviewPane);
+  }
+
   $form.find('.govuk-grid-row').eq(0).prepend($paneWrapper);
   $form.attr('action', location.pathname.replace(new RegExp(`set-${previewType}-branding$`), `preview-${previewType}-branding`));
   $form.find('button').text('Save');
