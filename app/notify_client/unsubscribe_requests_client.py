7bb6c7598c01e44562903feb8f32b012716119f4
Richard Parke <richard.parke@digital.cabinet-office.gov.uk>
2024-06-26 17:08:41 +0100

    refactor frontend to use unsubscribe_request client stub

--- app/notify_client/unsubscribe_requests_client.py
+++ app/notify_client/unsubscribe_requests_client.py
@@ -4,3 +4,6 @@ from app.notify_client import NotifyAdminAPIClient
 class UnsubscribeRequestsApiClient(NotifyAdminAPIClient):
     def get_pending_unsubscribe_requests(self, service_id):
         return 300  # stub for now, before building the backend
+
+
+unsubscribe_requests_client = UnsubscribeRequestsApiClient()
