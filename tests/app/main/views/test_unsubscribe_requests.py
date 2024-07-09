d32201d65423829117ac09b6e485e5037a379386
Chukwugozie Mbeledogu <chukwugozie.mbeledogu@digital.cabinet-office.gov.uk>
2024-07-09 13:51:20 +0100

    Rename unsubscribe_request_reports_summary and unsubscribe_request_report endpoint urls
     Rename report_id as batch_id.This is a semantic distinction to differentiate the summaries from the actual report.

--- tests/app/main/views/test_unsubscribe_requests.py
+++ tests/app/main/views/test_unsubscribe_requests.py
@@ -9,7 +9,7 @@ def test_unsubscribe_request_reports_summary(client_request, mocker):
                 "earliest_timestamp": "2024-06-15",
                 "latest_timestamp": "2024-06-21",
                 "processed_by_service_at": None,
-                "report_id": "ab26e2bc-da34-4326-bc2e-d957c14edde6",
+                "batch_id": "ab26e2bc-da34-4326-bc2e-d957c14edde6",
                 "is_a_batched_report": True,
             },
             {
@@ -17,7 +17,7 @@ def test_unsubscribe_request_reports_summary(client_request, mocker):
                 "earliest_timestamp": "2024-06-8",
                 "latest_timestamp": "2024-06-14",
                 "processed_by_service_at": "2024-06-10",
-                "report_id": "a46004d4-a4f5-4e9a-ad66-309fe503f7e6",
+                "batch_id": "a46004d4-a4f5-4e9a-ad66-309fe503f7e6",
                 "is_a_batched_report": True,
             },
         ],
@@ -26,7 +26,7 @@ def test_unsubscribe_request_reports_summary(client_request, mocker):
             "earliest_timestamp": "2024-06-22",
             "latest_timestamp": "2024-07-01",
             "processed_by_service_at": None,
-            "report_id": "5e2b05ef-7552-49ef-a77f-96d46ab2b9bb",
+            "batch_id": "5e2b05ef-7552-49ef-a77f-96d46ab2b9bb",
             "is_a_batched_report": False,
         },
     }
@@ -35,8 +35,8 @@ def test_unsubscribe_request_reports_summary(client_request, mocker):
     page = client_request.get("main.unsubscribe_request_reports_summary", service_id=SERVICE_ONE_ID)
 
     assert [
-        "Report",
-        "22 June 2024 to 1 July 2024 34 unsubscribe requests Not Downloaded",
+        "Report Status",
+        "22 June 2024 to 1 July 2024 34 unsubscribe requests Not downloaded",
         "15 June 2024 to 21 June 2024 200 unsubscribe requests Downloaded",
         "8 June 2024 to 14 June 2024 321 unsubscribe requests Completed",
     ] == [normalize_spaces(row.text) for row in page.select("tr")]
@@ -51,12 +51,12 @@ def test_no_unsubscribe_request_reports_summary_to_display(client_request, mocke
 
     page = client_request.get("main.unsubscribe_request_reports_summary", service_id=SERVICE_ONE_ID)
 
-    assert ["Report", "If you have unsubscribed requests they will be listed here"] == [
+    assert ["Report Status", "If you have unsubscribed requests they will be listed here"] == [
         normalize_spaces(row.text) for row in page.select("tr")
     ]
 
 
-def test_unsubscribe_request_report_for_existing_reports(client_request, mocker):
+def test_unsubscribe_request_report_for_batched_reports(client_request, mocker):
     test_data = test_data = {
         "batched_reports_summaries": [
             {
@@ -64,7 +64,7 @@ def test_unsubscribe_request_report_for_existing_reports(client_request, mocker)
                 "earliest_timestamp": "2024-06-15",
                 "latest_timestamp": "2024-06-21",
                 "processed_by_service_at": None,
-                "report_id": "a8a526f9-84be-44a6-b751-62c95c4b9329",
+                "batch_id": "a8a526f9-84be-44a6-b751-62c95c4b9329",
                 "is_a_batched_report": True,
             },
             {
@@ -72,7 +72,7 @@ def test_unsubscribe_request_report_for_existing_reports(client_request, mocker)
                 "earliest_timestamp": "2024-06-8",
                 "latest_timestamp": "2024-06-14",
                 "processed_by_service_at": "2024-06-10",
-                "report_id": "b9c28b5b-e442-4e5f-a9c7-c2544502627a",
+                "batch_id": "b9c28b5b-e442-4e5f-a9c7-c2544502627a",
                 "is_a_batched_report": True,
             },
         ],
@@ -82,7 +82,7 @@ def test_unsubscribe_request_report_for_existing_reports(client_request, mocker)
     page = client_request.get(
         "main.unsubscribe_request_report",
         service_id=SERVICE_ONE_ID,
-        report_id=test_data["batched_reports_summaries"][1]["report_id"],
+        batch_id=test_data["batched_reports_summaries"][1]["batch_id"],
     )
     assert page.select("h1")[0].text == "8 June 2024 until 14 June 2024"
 
@@ -95,7 +95,7 @@ def test_unsubscribe_request_report_for_unbatched_reports(client_request, mocker
             "earliest_timestamp": "2024-06-22",
             "latest_timestamp": "2024-07-01",
             "processed_by_service_at": None,
-            "report_id": "efcab5ff-31e4-4aa0-ac23-9ecd862073be",
+            "batch_id": "efcab5ff-31e4-4aa0-ac23-9ecd862073be",
             "is_a_batched_report": False,
         },
     }
@@ -103,6 +103,28 @@ def test_unsubscribe_request_report_for_unbatched_reports(client_request, mocker
     page = client_request.get(
         "main.unsubscribe_request_report",
         service_id=SERVICE_ONE_ID,
-        report_id=test_data["unbatched_report_summary"]["report_id"],
+        batch_id=test_data["unbatched_report_summary"]["batch_id"],
     )
     assert page.select("h1")[0].text == "22 June 2024 until 1 July 2024"
+
+
+def test_non_existing_unsubscribe_request_report_batch_id_returns_404(client_request, mocker):
+    test_data = {
+        "batched_reports_summaries": [],
+        "unbatched_report_summary": {
+            "count": 34,
+            "earliest_timestamp": "2024-06-22",
+            "latest_timestamp": "2024-07-01",
+            "processed_by_service_at": None,
+            "batch_id": "efcab5ff-31e4-4aa0-ac23-9ecd862073be",
+            "is_a_batched_report": False,
+        },
+    }
+    mocker.patch("app.service_api_client.get_unsubscribe_reports_summary", return_value=test_data)
+    page = client_request.get(
+        "main.unsubscribe_request_report",
+        _expected_status=404,
+        service_id=SERVICE_ONE_ID,
+        batch_id="32b4e359-d4df-49b6-a92b-2eaa9343cfdd",
+    )
+    assert normalize_spaces(page.select("h1")[0].text) == "Page not found"
