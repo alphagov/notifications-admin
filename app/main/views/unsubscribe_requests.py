db574a6ec23099b7ec24a38d12c8e1dde98bfad8
Chris Hill-Scott <me@quis.cc>
2024-07-10 10:56:13 +0100

    Use the new models

--- app/main/views/unsubscribe_requests.py
+++ app/main/views/unsubscribe_requests.py
@@ -9,40 +9,19 @@ from app.utils.user import user_has_permissions
 @main.route("/services/<uuid:service_id>/unsubscribe-requests/summary")
 @user_has_permissions("view_activity")
 def unsubscribe_request_reports_summary(service_id):
-    reports_summary_data = _get_unsubscribe_request_reports_summary()
-    return render_template("views/unsubscribe-request-reports-summary.html", data=reports_summary_data)
-
-
-def _get_unsubscribe_request_reports_summary():
-    data = current_service.unsubscribe_request_reports_summary
-    reports_summary_data = []
-    if unbatched_report_summary := data["unbatched_report_summary"]:
-        reports_summary_data.append(unbatched_report_summary)
-    if batched_reports_summaries := data["batched_reports_summaries"]:
-        reports_summary_data += batched_reports_summaries
-    return reports_summary_data
+    return render_template("views/unsubscribe-request-reports-summary.html")
 
 
 @main.route("/services/<uuid:service_id>/unsubscribe-requests/reports/<uuid:batch_id>")
 @user_has_permissions("view_activity")
 def unsubscribe_request_report(service_id, batch_id):
-    report_data = None
-    if reports_summary_data := _get_unsubscribe_request_reports_summary():
-        for report_summary in reports_summary_data:
-            if report_summary["batch_id"] == batch_id:
-                report_data = report_summary
-        if report_data:
-            form = ProcessUnsubscribeRequestForm(is_a_batched_report=report_data["is_a_batched_report"])
-            return render_template(
-                "views/unsubscribe-request-report.html",
-                count=report_data["count"],
-                earliest_timestamp=report_data["earliest_timestamp"],
-                latest_timestamp=report_data["latest_timestamp"],
-                processed_by_service_at=report_data["processed_by_service_at"],
-                batch_id=report_data["batch_id"],
-                is_a_batched_report=report_data["is_a_batched_report"],
-                service_id=service_id,
-                form=form,
-            )
+    report = current_service.unsubscribe_request_reports_summary.get_by_batch_id(batch_id)
+    if report:
+        form = ProcessUnsubscribeRequestForm(is_a_batched_report=report.is_a_batched_report)
+        return render_template(
+            "views/unsubscribe-request-report.html",
+            report=report,
+            form=form,
+        )
 
     abort(404)
--- app/models/unsubscribe_requests_report.py
+++ app/models/unsubscribe_requests_report.py
@@ -21,3 +21,8 @@ class UnsubscribeRequestsReports(ModelList):
     def __init__(self, *args):
         super().__init__(self, *args)
         self.items = (self.items["unbatched_report_summary"] or []) + (self.items["batched_reports_summaries"] or [])
+
+    def get_by_batch_id(self, batch_id):
+        for report in self:
+            if report.batch_id == batch_id:
+                return report
