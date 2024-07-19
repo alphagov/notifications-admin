69271d4baf7553e1f15f980d5fa472c693799934
Jani Kraner <jani.kraner@digital.cabinet-office.gov.uk>
2024-07-19 12:33:49 +0100

    Only run our Javascript in supported browsers only
    As we no longer support IE 11 and below we can
    use a check to
    - check of existance of `govuk-frontend-supported`
    class on the `body` which is added via
    govuk-frontend that runs only in browsers
    that support type='module' which IE 11 does not.

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,30 +1,33 @@
-$(() => $("time.timeago").timeago());
 
-$(() => GOVUK.stickAtTopWhenScrolling.init());
-$(() => GOVUK.stickAtBottomWhenScrolling.init());
+if (document.body.classList.contains('govuk-frontend-supported')) {
+  $(() => $("time.timeago").timeago());
 
-var showHideContent = new GOVUK.ShowHideContent();
-showHideContent.init();
+  $(() => GOVUK.stickAtTopWhenScrolling.init());
+  $(() => GOVUK.stickAtBottomWhenScrolling.init());
 
-$(() => GOVUK.notifyModules.start());
+  var showHideContent = new GOVUK.ShowHideContent();
+  showHideContent.init();
 
-$(() => $('.error-message, .govuk-error-message').eq(0).parent('label').next('input').trigger('focus'));
+  $(() => GOVUK.notifyModules.start());
 
-$(() => $('.banner-dangerous').eq(0).trigger('focus'));
+  $(() => $('.error-message, .govuk-error-message').eq(0).parent('label').next('input').trigger('focus'));
 
-$(() => $('.govuk-header__container').on('click', function() {
-  $(this).css('border-color', '#1d70b8');
-}));
+  $(() => $('.banner-dangerous').eq(0).trigger('focus'));
 
-// Applies our expanded focus style to the siblings of links when that link is wrapped in a heading.
-//
-// This will be possible in CSS in the future, using the :has pseudo-class. When :has is available
-// in the browsers we support, this code can be replaced with a CSS-only solution.
-$('.js-mark-focus-on-parent').on('focus blur', '*', e => {
-  $target = $(e.target);
-  if (e.type === 'focusin') {
-    $target.parent().addClass('js-child-has-focus');
-  } else {
-    $target.parent().removeClass('js-child-has-focus');
-  }
-});
+  $(() => $('.govuk-header__container').on('click', function() {
+    $(this).css('border-color', '#1d70b8');
+  }));
+
+  // Applies our expanded focus style to the siblings of links when that link is wrapped in a heading.
+  //
+  // This will be possible in CSS in the future, using the :has pseudo-class. When :has is available
+  // in the browsers we support, this code can be replaced with a CSS-only solution.
+  $('.js-mark-focus-on-parent').on('focus blur', '*', e => {
+    $target = $(e.target);
+    if (e.type === 'focusin') {
+      $target.parent().addClass('js-child-has-focus');
+    } else {
+      $target.parent().removeClass('js-child-has-focus');
+    }
+  });
+}
