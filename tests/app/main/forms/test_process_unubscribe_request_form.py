13f3700ed8f568d9f76b30a8244eec98d3ea569a
Chukwugozie Mbeledogu <chukwugozie.mbeledogu@digital.cabinet-office.gov.uk>
2024-07-29 11:49:20 +0100

    Update ProcessUnsubscribeRequestForm validation to facilitate clearing the Mark as completed checkbox for a Completed report  Add tests for ProcessUnsubscribeRequestForm

---
+++ tests/app/main/forms/test_process_unubscribe_request_form.py
@@ -0,0 +1,26 @@
+from app.main.forms import ProcessUnsubscribeRequestForm
+
+
+def test_should_raise_validation_error_for_reports_whose_status_is_not_completed(
+    client_request,
+):
+    form = ProcessUnsubscribeRequestForm(is_a_batched_report=True, report_status="Downloaded")
+    form.data["report_has_been_processed"] = False
+    form.validate()
+    assert form.validate() is False
+    assert (
+        "There is a problem. You must confirm that you have removed the email addresses from your mailing list."
+        in form.errors["report_has_been_processed"]
+    )
+
+
+def test_should_raise_no_validation_error_for_reports_whose_status_is_being_changed_from_completed(
+    client_request,
+):
+    """
+    The test case covered, is that clearing a checkbox for a completed report, ie updating its status
+    from "Completed" to "Downloaded", raises no validation errors.
+    """
+    form = ProcessUnsubscribeRequestForm(is_a_batched_report=True, report_status="Completed")
+    form.data["report_has_been_processed"] = False
+    assert form.validate() is True
