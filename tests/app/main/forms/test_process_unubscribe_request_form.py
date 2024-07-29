c53056ed871cbcf618383c0b4b3adf583e75d5d0
Chukwugozie Mbeledogu <chukwugozie.mbeledogu@digital.cabinet-office.gov.uk>
2024-07-29 12:22:51 +0100

    Update ProcessUnsubscribeRequestForm validation  The form logic has been updated such that if the checkbox has been marked, subsequent submissions with the checkbox marked are  ignored, the same applies if the checkbox is cleared.A user can clear a checkbox and submit or mark a cleared checkbox and submit.

--- tests/app/main/forms/test_process_unubscribe_request_form.py
+++ tests/app/main/forms/test_process_unubscribe_request_form.py
@@ -14,6 +14,20 @@ def test_should_raise_validation_error_for_reports_whose_status_is_not_completed
     )
 
 
+def test_should_raise_validation_error_for_resubmitting_marked_checkbox_for_an_already_completed_report(
+    client_request,
+):
+    form = ProcessUnsubscribeRequestForm(
+        is_a_batched_report=True, report_status="Completed", report_has_been_processed=True
+    )
+    form.validate()
+    assert form.validate() is False
+    assert (
+        "There is a problem. You have already marked the report as Completed"
+        in form.errors["report_has_been_processed"]
+    )
+
+
 def test_should_raise_no_validation_error_for_reports_whose_status_is_being_changed_from_completed(
     client_request,
 ):
@@ -21,6 +35,7 @@ def test_should_raise_no_validation_error_for_reports_whose_status_is_being_chan
     The test case covered, is that clearing a checkbox for a completed report, ie updating its status
     from "Completed" to "Downloaded", raises no validation errors.
     """
-    form = ProcessUnsubscribeRequestForm(is_a_batched_report=True, report_status="Completed")
-    form.data["report_has_been_processed"] = False
+    form = ProcessUnsubscribeRequestForm(
+        is_a_batched_report=True, report_status="Completed", report_has_been_processed=False
+    )
     assert form.validate() is True
