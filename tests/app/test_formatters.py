328257d593384fbdb763d525284251853abfeae7
Chukwugozie Mbeledogu <chukwugozie.mbeledogu@digital.cabinet-office.gov.uk>
2024-08-12 16:22:47 +0100

    Update unsubscribe-request-report.html to use message_count_label

--- tests/app/test_formatters.py
+++ tests/app/test_formatters.py
@@ -1,4 +1,4 @@
-from app.formatters import sentence_case
+from app.formatters import message_count_label, sentence_case
 
 
 def test_sentence_case():
@@ -7,3 +7,10 @@ def test_sentence_case():
         sentence_case("mobile number 2 does not look like a UK mobile number")
         == "Mobile number 2 does not look like a UK mobile number"
     )
+
+
+def test_message_count_label_for_unsubscribe_requests():
+    assert message_count_label(count=1, message_type="unsubscribe request", suffix="") == "unsubscribe request"
+    assert message_count_label(count=2, message_type="unsubscribe request", suffix="") == "unsubscribe requests"
+    assert message_count_label(count=1, message_type="request", suffix="") == "request"
+    assert message_count_label(count=2, message_type="request", suffix="") == "requests"
