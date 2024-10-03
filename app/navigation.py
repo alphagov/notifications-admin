bfe101b17bacb6f0ce72c8637cceb44c0875a8e6
Pea Tyczynska <mervi.tyczynska@digital.cabinet-office.gov.uk>
2024-10-03 17:19:41 +0100

    Fix navigation-related tests

--- app/navigation.py
+++ app/navigation.py
@@ -60,6 +60,7 @@ class HeaderNavigation(Navigation):
             "guidance_using_notify",
             "guidance_api_documentation",
             "guidance_api_documentation_section",
+            "guidance_api_documentation_section_choose_docs",
             "guidance_attach_pages",
             "guidance_bulk_sending",
             "guidance_data_retention_period",
