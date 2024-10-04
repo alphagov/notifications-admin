dd572403895eea5d65589791b8ed8160a1806d89
Jani Kraner <jani.kraner@digital.cabinet-office.gov.uk>
2024-10-04 16:12:51 +0100

    Introduce ESLint to lint JS files
    This is to replace JSHint.
    
    Installs the package, jest eslint plugin,
    browser globals and eslint/js recomended rules.
    
    Create a separate "lint:js" npm scripts task

---
+++ eslint.config.mjs
@@ -0,0 +1,51 @@
+import globals from "globals";
+import jestConfig from 'eslint-plugin-jest';
+import js from "@eslint/js";
+
+export default [
+  {
+    files: [
+      "app/assets/**/*.{js,mjs}",
+      "tests/**/*.{js,mjs}"
+    ],
+    rules: {
+      ...js.configs.recommended.rules,
+      "semi": ["error", "always"],
+      "no-prototype-builtins": "warn",
+      "no-unused-vars": "warn",
+      "no-extra-boolean-cast": "warn",
+      "no-undef": "warn",
+      "no-useless-escape": "warn",
+      "no-unexpected-multiline": "warn",
+      //needs more standardjs rules
+    },
+    ignores: [
+      'tests/**/*.{js,mjs}'
+    ],
+  },
+  {
+    files: ["app/assets/**/*.js"],
+    languageOptions: {
+      sourceType: "commonjs",
+      globals: {
+        ...globals.browser,
+        '$': 'writable',
+        "GOVUK": "readonly"
+      }
+    },
+  },
+  {
+    files: ["**/*.mjs"],
+    languageOptions: {
+      sourceType: "module"
+    }
+  },
+  {
+    files: ['**/*.test.{js,mjs}'],
+    ...jestConfig.configs['flat/recommended'],
+    rules: {
+      "jest/no-disabled-tests": "error",
+      "jest/no-focused-tests": "warn",
+    }
+  },
+];
