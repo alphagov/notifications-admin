b9037ae5cda364073a636e0378a130569ab12ee4
Jani Kraner <jani.kraner@digital.cabinet-office.gov.uk>
2024-10-04 14:34:37 +0100

    Introduce a Rollup config
    This PR introduces a Rollup config to manage
    various build steps
    
    https://rollupjs.org/configuration-options/
    
    Config contains three separate sections:
    
    1. ESM JavaScript bundle compilation
    2. SCSS compilation
    3. Legacy Javascript compilation
    
    1. ESM JavaScript bundle compilation
    
    The simplest one. Single input to create
    a minified ESM bundle that contains links to
    sourcemaps.
    
    As Rollup "task" requires and input and output,
    and the copy plugin doesn't have separate ones,
    it's been added to this one.
    
    2. SCSS compilation
    
    Sadly, globbing isn't supported so we need to
    list the entry points we import.
    
    We minimise include links to sourcemaps and we
    disable the CSS URL resolver
    warnings (as referenced static files exists
    elsewhere). For production build we also rewrite
    the url to those assets as they're stored elsewhere.
    
    3. Legacy Javascript compilation
    
    We use multi entry to first import
    external dependecies, then local files and finally
    `main.js` - same as before.
    We include links to sourcemaps and we minify.
    
    To prevent `this is undefined` error we apply
    `window` context for `jquery` and `cbor` via
    [`moduleContext` Rollup option](https://rollupjs.org/troubleshooting/#error-this-is-undefined)
    
    Additionally we needed to not mangle "Hogan" in
    minification as the script sets a global variable on
    `window` that is then used in one of our scripts.

---
+++ rollup.config.mjs
@@ -0,0 +1,135 @@
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import multi from '@rollup/plugin-multi-entry';
+import terser from '@rollup/plugin-terser';
+import copy from 'rollup-plugin-copy';
+import styles from "rollup-plugin-styler";
+import postCSSReplace from 'postcss-replace';
+
+const paths = {
+  src: 'app/assets/',
+  dist: 'app/static/',
+  npm: 'node_modules/',
+  govuk_frontend: 'node_modules/govuk-frontend/dist/'
+};
+
+const isDevelopment = Boolean(process.env.NOTIFY_ENVIRONMENT === 'development')
+
+export default [
+  // ESM compilation and copy static assets
+  {
+    input: paths.src + 'javascripts/esm/all-esm.mjs',
+    output: {
+      dir: paths.dist + 'javascripts/',
+      entryFileNames: 'all-esm.mjs',
+      format: 'es',
+      sourcemap: true
+    },
+    plugins: [
+      nodeResolve(),
+      terser(),
+      // copy images, error pages and govuk-frontend static assets
+      copy({
+        targets: [
+          { src: paths.src + 'error_pages/**/*', dest: paths.dist + 'error_pages/' },
+          { src: [ paths.src + 'images/**/*', paths.govuk_frontend + 'govuk/assets/images/**/*' ], dest: paths.dist + 'images/' },
+          { src: paths.govuk_frontend + 'govuk/assets/fonts/**/*', dest: paths.dist + 'fonts/' },
+          { src: paths.govuk_frontend + 'govuk/assets/manifest.json', dest: paths.dist }
+        ]
+      }),
+    ]
+  },
+  // SCSS compilation
+  {
+    input: [
+      paths.src + 'stylesheets/main.scss',
+      paths.src + 'stylesheets/print.scss'
+    ],
+    output: {
+      dir: paths.dist + 'stylesheets/',
+      assetFileNames: "[name][extname]",
+    },
+    plugins: [
+      // https://anidetrix.github.io/rollup-plugin-styles/interfaces/types.Options.html
+      styles({
+        mode: "extract",
+        sass: {
+          includePaths: [
+            paths.govuk_frontend,
+            paths.npm
+          ],
+        },
+        minimize: true,
+        url: false,
+        sourceMap: true,
+        plugins:[
+          // Rewrite /static prefix for URLs in CSS files for production
+          !isDevelopment && postCSSReplace({
+            pattern: /\/static\//g,
+            data: {
+              replaceAll: '/'
+            }
+          }),
+        ]
+      }),
+    ]
+  },
+  // ES5 JS compilation
+  {
+    input: [
+      paths.npm + 'hogan.js/dist/hogan-3.0.2.js',
+      paths.npm + 'jquery/dist/jquery.min.js',
+      paths.npm + 'query-command-supported/dist/queryCommandSupported.min.js',
+      paths.npm + 'timeago/jquery.timeago.js',
+      paths.npm + 'textarea-caret/index.js',
+      paths.npm + 'cbor-js/cbor.js',
+      paths.src + 'javascripts/modules.js',
+      paths.src + 'javascripts/govuk-frontend-toolkit/show-hide-content.js',
+      paths.src + 'javascripts/stick-to-window-when-scrolling.js',
+      paths.src + 'javascripts/cookieCleanup.js',
+      paths.src + 'javascripts/copyToClipboard.js',
+      paths.src + 'javascripts/autofocus.js',
+      paths.src + 'javascripts/enhancedTextbox.js',
+      paths.src + 'javascripts/fileUpload.js',
+      paths.src + 'javascripts/radioSelect.js',
+      paths.src + 'javascripts/updateContent.js',
+      paths.src + 'javascripts/listEntry.js',
+      paths.src + 'javascripts/liveSearch.js',
+      paths.src + 'javascripts/preventDuplicateFormSubmissions.js',
+      paths.src + 'javascripts/fullscreenTable.js',
+      paths.src + 'javascripts/radios-with-images.js',
+      paths.src + 'javascripts/previewPane.js',
+      paths.src + 'javascripts/colourPreview.js',
+      paths.src + 'javascripts/liveCheckboxControls.js',
+      paths.src + 'javascripts/templateFolderForm.js',
+      paths.src + 'javascripts/addBrandingOptionsForm.js',
+      paths.src + 'javascripts/setAuthTypeForm.js',
+      paths.src + 'javascripts/registerSecurityKey.js',
+      paths.src + 'javascripts/authenticateSecurityKey.js',
+      paths.src + 'javascripts/updateStatus.js',
+      paths.src + 'javascripts/errorBanner.js',
+      paths.src + 'javascripts/homepage.js',
+      paths.src + 'javascripts/removeInPresenceOf.js',
+      paths.src + 'javascripts/main.js',
+    ],
+    output: {
+      dir: paths.dist + 'javascripts/',
+      sourcemap: true
+    },
+    moduleContext: {
+      './node_modules/jquery/dist/jquery.min.js': 'window',
+      './node_modules/cbor-js/cbor.js': 'window',
+    },
+    plugins: [
+      nodeResolve(),
+      multi({
+        entryFileName: 'all.js'
+      }),
+      terser({
+        ecma: '5',
+        mangle: {
+          reserved: ["Hogan"]
+        }
+      }),
+    ]
+  }
+];
