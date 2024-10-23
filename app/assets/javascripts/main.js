21810e9c5ad1a548dca108ab2c7b8e6b1bbdb0a6
Tom Byers <tombaromba@gmail.com>
2024-10-23 11:12:36 +0100

    Remove classesPersister JS and associated code
    The classesPersister JS was a slight hack we
    introduced in the following pull request:
    
    https://github.com/alphagov/notifications-admin/pull/4155
    
    It was added to make our 'block focus style' work
    with the variant of template list items emergency
    alerts introduced. In this variant, the link is
    wrapped in a <h2> tag which stopped our existing
    CSS targeting sibling elements.
    
    Emergency alerts code was removed in this pull
    request:
    
    https://github.com/alphagov/notifications-admin/pull/4922
    
    Because there is now no HTML that uses this code
    in our app, we shouldn't include it and be sending
    it to users. This removes it.

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -15,17 +15,4 @@ if (document.body.classList.contains('govuk-frontend-supported')) {
   $(() => $('.govuk-header__container').on('click', function() {
     $(this).css('border-color', '#1d70b8');
   }));
-
-  // Applies our expanded focus style to the siblings of links when that link is wrapped in a heading.
-  //
-  // This will be possible in CSS in the future, using the :has pseudo-class. When :has is available
-  // in the browsers we support, this code can be replaced with a CSS-only solution.
-  $('.js-mark-focus-on-parent').on('focus blur', '*', e => {
-    $target = $(e.target);
-    if (e.type === 'focusin') {
-      $target.parent().addClass('js-child-has-focus');
-    } else {
-      $target.parent().removeClass('js-child-has-focus');
-    }
-  });
 }
--- app/assets/javascripts/updateContent.js
+++ app/assets/javascripts/updateContent.js
@@ -11,56 +11,9 @@
       1000
   ));
 
-  // Methods to ensure the DOM fragment is clean of classes added by JS before diffing
-  // and that they are replaced afterwards.
-  //
-  // Added to allow the use of JS, in main.js, to apply styles which in future could be
-  // achieved with the :has pseudo-class. If :has is available in our supported browsers,
-  // this can be removed in favour of a CSS-only solution.
-  var ClassesPersister = function ($contents) {
-    this._$contents = $contents;
-    this._classNames = [];
-    this._classesTo$ElsMap = {};
-  };
-  ClassesPersister.prototype.addClassName = function (className) {
-    if (this._classNames.indexOf(className) === -1) {
-      this._classNames.push(className);
-    }
-  };
-  ClassesPersister.prototype.remove = function () {
-    // Store references to any elements with class names to persist
-    this._classNames.forEach(className => {
-      var $elsWithClassName = $('.' + className, this._$contents).removeClass(className);
-
-      if ($elsWithClassName.length > 0) {
-        this._classesTo$ElsMap[className] = $elsWithClassName;
-      }
-    });
-  };
-  ClassesPersister.prototype.replace = function () {
-    var replaceClasses = (idx, el) => {
-
-      // Avoid updating elements that are no longer present.
-      // elements removed will still exist in memory but won't be attached to the DOM any more
-      if (global.document.body.contains(el)) {
-        $(el).addClass(className);
-      }
-
-    };
-    var className;
-
-    for (className in this._classesTo$ElsMap) {
-      this._classesTo$ElsMap[className].each(replaceClasses);
-    }
-
-    // remove references to elements
-    this._classesTo$ElsMap = {};
-  };
-
-  var getRenderer = ($contents, key, classesPersister) => response => {
+  var getRenderer = ($contents, key) => response => {
     var contents = $contents.get(0);
     var contentHasUpdated = false;
-    classesPersister.remove();
     window.Morphdom(
       contents,
       $(response[key]).get(0),
@@ -77,7 +30,6 @@
         }
       }
     );
-    classesPersister.replace();
     if (contentHasUpdated === true) {
       $(document).trigger("updateContent.onafterupdate", [contents]);
     }
@@ -138,27 +90,15 @@
       var key = $component.data('key');
       var resource = $component.data('resource');
       var form = $component.data('form');
-      var classesPersister = new ClassesPersister($contents);
       interval = defaultInterval;
 
       // Replace component with contents.
       // The renderer does this anyway when diffing against the first response
       $component.replaceWith($contents);
 
-      // Store any classes that should persist through updates
-      //
-      // Added to allow the use of JS, in main.js, to apply styles which in future could be
-      // achieved with the :has pseudo-class. If :has is available in our supported browsers,
-      // this can be removed in favour of a CSS-only solution.
-      if ($contents.data('classesToPersist') !== undefined) {
-        $contents.data('classesToPersist')
-          .split(' ')
-          .forEach(className => classesPersister.addClassName(className));
-      }
-
       timeouts[resource] = setTimeout(
         () => poll(
-          getRenderer($contents, key, classesPersister),
+          getRenderer($contents, key),
           resource,
           getQueue(resource),
           form
