b8a4422db1d3ea6fc6d3895ccd1f1f391c3669de
Katie Smith <klssmith@users.noreply.github.com>
2024-11-27 09:37:37 +0000

    Add email sender changed check to go-live checklist
    This checks whether a user has visited the page to add an email sender
    and clicked "Save" even if they decided not to add one and to go with
    the default of the service name. The result of this is stored in Redis
    since this is a prototype.

--- app/models/service.py
+++ app/models/service.py
@@ -296,6 +296,12 @@ class Service(JSONModel):
             return self.has_sms_templates
         return self.volume_sms > 0
 
+    @property
+    def has_confirmed_email_sender(self):
+        if redis_client.get(f"{self.id}_has_confirmed_email_sender"):
+            return True
+        return False
+
     @cached_property
     def email_reply_to_addresses(self):
         return service_api_client.get_reply_to_email_addresses(self.id)
@@ -422,6 +428,7 @@ class Service(JSONModel):
                 any(self.volumes_by_channel.values()),
                 self.has_team_members_with_manage_service_permission,
                 self.has_templates,
+                self.has_confirmed_email_sender,
                 not self.needs_to_add_email_reply_to_address,
                 not self.needs_to_change_sms_sender,
             )
