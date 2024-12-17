91ed50455d64e2db8cef3d23ccfdb8b941821de8
Jani Kraner <jani.kraner@digital.cabinet-office.gov.uk>
2024-12-17 10:37:27 +0000

    Rewrite Autofocus as ES Module
    As we want to migrate all of our legacy Javascript
    files to ESM, this is a small part.
    
    Rewrite follows the same convention we previously
    used for FocusBanner, Collapsible checkboxes,
    ColourPreview and fileUpload.
    
    Majority of changes are replacing jQuery methods
    with native Javascript alternatives.

--- app/assets/javascripts/esm/autofocus.mjs
+++ app/assets/javascripts/esm/autofocus.mjs
@@ -15,29 +15,20 @@ class Autofocus {
     if (!isSupported()) {
       return this;
     }
-    this.$component = $module
-    const forceFocus = $module.dataset.forceFocus;
-    const arrayOfTargetElements = ['input', 'textarea', 'select']
-
+    this.$component = $module;
+    const forceFocus = Boolean(this.$component.dataset.forceFocus);
+    const arrayOfTargetElements = ['input', 'textarea', 'select'];
     // if the page loads with a scroll position, we can't assume the item to focus onload
     // is still where users intend to start
     if ((window.scrollY > 0) && !forceFocus) { return; }
 
-
     // See if the component itself is something we want to send focus to
-    let target = arrayOfTargetElements.includes(this.$component.tagName.toLowerCase());
-    
     // Otherwise look inside the component to see if there are any elements
     // we want to send focus to
-    if(!target) {
-      target = this.filterByElements(arrayOfTargetElements)
-      target.focus()
-    }
-  }
-  filterByElements(filter) {
-    let target = [...this.$component.querySelectorAll('*')]
-        .filter(item => filter.includes(item.tagName.toLowerCase()));
-    return target[0]
+    let target = arrayOfTargetElements.includes(this.$component.tagName.toLowerCase())
+      ? this.$component
+      : this.$component.querySelector(arrayOfTargetElements.join(','));
+    target.focus();
   }
 }
 
