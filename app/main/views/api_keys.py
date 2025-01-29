c59da12308b14a6af6f3d7fd8ea27de4d6bef875
Richard Parke <richard.parke@digital.cabinet-office.gov.uk>
2025-01-29 13:34:01 +0000

    fix redirection logic

--- app/main/views/api_keys.py
+++ app/main/views/api_keys.py
@@ -155,9 +155,8 @@ def check_token_against_dummy_bearer(token):
 @main.route("/services/<uuid:service_id>/api/callbacks", methods=["GET"])
 @user_has_permissions("manage_api_keys")
 def api_callbacks(service_id):
-    # if not current_service.has_permission("inbound_sms"):
-    # if True:
-    #     return redirect(url_for(".returned_letters_callback", service_id=service_id))
+    if not current_service.has_permission("inbound_sms") and not current_service.has_permission("letter"):
+        return redirect(url_for(".returned_letters_callback", service_id=service_id))
     delivery_status_callback, received_text_messages_callback = get_apis()
     returned_letters_callback = None
     returned_letters_callback = {"url": "https://test.test.com"}
