a4a350cb81a5f58a8e0955102383d327ec456d3e
Jani Kraner <jani.kraner@digital.cabinet-office.gov.uk>
2025-04-08 10:35:03 +0100

    csv generation tweaks

--- app/main/views/csv_report_request.py
+++ app/main/views/csv_report_request.py
@@ -5,6 +5,7 @@ from app.main import main
 from app.utils.user import user_has_permissions
 from werkzeug.utils import redirect
 from notifications_python_client.errors import HTTPError
+from app.models.report_request import ReportRequest
 
 @main.route("/services/<uuid:service_id>/download-report/<uuid:request_id>", methods=["GET"])
 @user_has_permissions()
@@ -12,7 +13,7 @@ def csv_report_request(service_id, request_id):
   # if users have bookmarked the page and they com back to it, there will likely be no report avaialble
   # get report status
   try:
-    report_request = report_request_api_client.get_report_request(service_id, request_id).get("data", {})
+    report_request = ReportRequest.from_id(service_id, request_id)
   except HTTPError as e:
       if e.status_code == 404:
         report_request = None
@@ -21,10 +22,9 @@ def csv_report_request(service_id, request_id):
         notification_status = None
         page_title= "Your report is no longer available"
   else:
-    report_request = report_request_api_client.get_report_request(service_id, request_id).get("data", {})
-    report_status = report_request['status']
-    notification_type =  report_request['parameter']['notification_type']
-    notification_status =  report_request['parameter']['notification_status']
+    report_status = report_request.status
+    notification_type =  report_request.parameter['notification_type']
+    notification_status =  report_request.parameter['notification_status']
     page_title= "Error: We could not create your report" if report_status == 'failed' else "Preparing your report"
 
 
@@ -43,7 +43,7 @@ def csv_report_request(service_id, request_id):
 @user_has_permissions()
 def csv_report_ready(service_id, request_id):
   try:
-    report_request = report_request_api_client.get_report_request(service_id, request_id)['data']
+    report_request = ReportRequest.from_id(service_id, request_id)
   except HTTPError as e:
       if e.status_code == 404:
         return redirect(
@@ -62,7 +62,7 @@ def csv_report_ready(service_id, request_id):
     return render_template(
         "views/csv-report/ready.html",
         retention_period = current_service.get_days_of_retention('email'),
-        notification_status = report_request['parameter']['notification_status'],
-        notification_type = report_request['parameter']['notification_type'],
+        notification_status = report_request.parameter['notification_status'],
+        notification_type = report_request.parameter['notification_type'],
         request_id = request_id,
     )
