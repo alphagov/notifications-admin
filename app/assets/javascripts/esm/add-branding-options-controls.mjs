186fcf59a067ea250bb4b8e5ead23163aec1ea9d
Tom Byers <tombaromba@gmail.com>
2025-07-18 10:42:47 +0100

    Rewrite LiveCheckboxControls as ESM module
    We want to migrate all of our legacy Javascript
    files to ESM, so this is the latest in the series
    of conversions.
    
    Rewrite follows the same conventions we previously
    used for:
    - FocusBanner
    - Collapsible checkboxes
    - ColourPreview
    - FileUpload
    - Autofocus
    - PreviewPane
    - CopyToClipboard
    - LiveSearch
    - ListEntry
    - RadiosWithImages
    - EnhancedTextbox
    
    LiveCheckboxControls is a base class for both
    AddBrandingOptionsControls and SetAuthTypeForm so
    I did them all together.
    
    This also includes a rewrite of their tests, not
    just to allow for them to be a module but also to
    make sure the states of the page loading with
    checkboxes selected and that of the page having
    checkboxes selected afterwards are both
    represented when needed and in the right place in
    the test.
    
    Added bonus: removes the use of jQuery as well.

---
+++ app/assets/javascripts/esm/add-branding-options-controls.mjs
@@ -0,0 +1,7 @@
+import LiveCheckboxControls from './live-checkbox-controls.mjs';
+
+class AddBrandingOptionsControls extends LiveCheckboxControls {
+  onNothingSelected (state) {}
+}
+
+export default AddBrandingOptionsControls;
