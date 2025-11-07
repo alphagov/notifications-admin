4087a77acb4df2dabea4886cb4f9849664a60577
Chris Hill-Scott <me@quis.cc>
2025-11-07 15:14:36 +0000

    Use StrictJSONModel for contact lists

--- tests/app/models/test_contact_list.py
+++ tests/app/models/test_contact_list.py
@@ -1,9 +1,10 @@
 from app.models.contact_list import ContactList
 from app.models.job import PaginatedJobs
+from tests import contact_list_json
 
 
 def test_get_jobs(mock_get_jobs):
-    contact_list = ContactList({"id": "a", "service_id": "b"})
+    contact_list = ContactList(contact_list_json(id_="a", service_id="b"))
     assert isinstance(contact_list.get_jobs(page=123), PaginatedJobs)
     # mock_get_jobs mocks the underlying API client method, not
     # contact_list.get_jobs
