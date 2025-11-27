0053b6c0a6bfa4a74243cd8993ba3545536af646
Robert Scott <robert.scott@digital.cabinet-office.gov.uk>
2025-11-27 16:05:03 +0000

    Spreadsheet tests: add tests of conversion to csv with "expected" results
    these "expected" results aren't necessarily the "desired" results
    but they document existing behaviour at least

--- tests/app/models/test_spreadsheet.py
+++ tests/app/models/test_spreadsheet.py
@@ -4,6 +4,10 @@ import pytest
 
 from app.models.spreadsheet import Spreadsheet
 
+conversion_original_files = tuple(
+    (Path.cwd() / "tests" / "spreadsheet_files" / "conversions" / "originals").glob("[!.]*.*")
+)
+
 
 def test_can_create_spreadsheet_from_large_excel_file():
     with open(str(Path.cwd() / "tests" / "spreadsheet_files" / "excel 2007.xlsx"), "rb") as xl:
@@ -41,3 +45,15 @@ def test_spreadsheet_checks_for_bad_arguments(args, kwargs):
     with pytest.raises(TypeError) as exception:
         Spreadsheet(*args, **kwargs)
     assert str(exception.value) == "Spreadsheet must be created from either rows or CSV data"
+
+
+@pytest.mark.parametrize("original_file", conversion_original_files, ids=[p.name for p in conversion_original_files])
+def test_conversion(original_file):
+    # these are just the "expected" results - we haven't necessarily declared that these are the
+    # exact "desired" results. it's more a documentation of the quirks allowing us to be aware of
+    # behaviour changes.
+    expected_file = original_file.parent / ".." / "expected_converted" / f"{original_file.stem}.csv"
+
+    with original_file.open("rb") as f_orig, expected_file.open("rb") as f_exp:
+        # comparison done in binary mode to avoid universal newlines complicating things
+        assert Spreadsheet.from_file(f_orig, filename=original_file.name).as_csv_data.encode("utf-8") == f_exp.read()
