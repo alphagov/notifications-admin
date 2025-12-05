3a5530c2f1fef09a9bc571ead58ee24331421181
Richard Parke <richard.parke@digital.cabinet-office.gov.uk>
2025-12-05 14:22:23 +0000

    update email_template_files_upload view to accept form POST request and : - upload file to s3 with the format `service-<service id>/template-<template id>/<file id> - add a new row to the template_email_files table for the file with default file settings
    we don't currently have the UI for file settings so the upload should just use some defaults for retention period, validate_users_email etc.
    
    This view as currently created will essentially create a bunch of "orphaned" long term file uploads. This shouldn't be an issue while this is behind a feature flag, but we should think about how we want to manage the clean up of any orphaned files in general.

--- app/main/views/template_email_files.py
+++ app/main/views/template_email_files.py
@@ -1,10 +1,17 @@
-from flask import render_template
+import uuid
 
-from app import current_service, current_user
+from flask import redirect, render_template, url_for
+
+from app import current_service, current_user, template_email_file_client
 from app.main import main
 from app.main.forms import TemplateEmailFilesUploadForm
+from app.s3_client.s3_template_email_file_upload_client import upload_template_email_file_to_s3
 from app.utils import service_has_permission
 from app.utils.user import user_has_permissions
+
+
+def _get_file_location_from_upload_id(file_id, service_id, template_id):
+    return f"service-{service_id}/template-{template_id}/{file_id}"
 
 
 @main.route("/services/<uuid:service_id>/templates/<uuid:template_id>/files/upload", methods=["GET", "POST"])
@@ -18,7 +25,20 @@ def email_template_files_upload(template_id, service_id):
     )
     form = TemplateEmailFilesUploadForm()
     if form.validate_on_submit():
-        pass
+        file_id = uuid.uuid4()
+        filename = form.file.data.filename
+        file_bytes = form.file.data.read()
+        file_location = _get_file_location_from_upload_id(file_id, service_id, template_id)
+        upload_template_email_file_to_s3(data=file_bytes, file_location=file_location)
+        template_email_file_client.create_file(file_id, service_id, template_id, filename, current_user.id)
+        return redirect(
+            url_for(
+                "main.view_template",
+                service_id=service_id,
+                template_id=template_id,
+            )
+        )
+
     return render_template(
         "views/templates/email-template-files/upload.html",
         template=template,
