a963e06b5e5fa71aff6de9861ffe2c09e91beb6e
Tom Byers <tombaromba@gmail.com>
2025-12-18 11:32:27 +0000

    Add logging

--- app/assets/javascripts/esm/update-content.mjs
+++ app/assets/javascripts/esm/update-content.mjs
@@ -23,11 +23,11 @@ class UpdateContent {
     // Replace component with contents.
     // The renderer does this anyway when diffing against the first response
     $module.parentNode.replaceChild(this.$contents, $module);
-    
-    const initialBoundPoll = this.poll.bind(this, 
+
+    const initialBoundPoll = this.poll.bind(this,
       this.getRenderer(this.$contents, this.key),
-      this.resource, 
-      this.getQueue(this.resource), 
+      this.resource,
+      this.getQueue(this.resource),
       this.$form
     );
 
@@ -43,7 +43,7 @@ class UpdateContent {
       1000
     ));
   }
-  
+
   getQueue(resource) {
     return (queues[resource] = queues[resource] || []);
   }
@@ -55,15 +55,15 @@ class UpdateContent {
   clearQueue(queue) {
     queue.length = 0;
   }
-  
+
   getRenderer(contents, key) {
-    return response => {
+    const result = response => {
       let contentHasUpdated = false;
 
       // HTML node from string
       const parser = new DOMParser();
       const doc = parser.parseFromString(response[key], 'text/html');
-      const newContents = doc.body.firstElementChild; 
+      const newContents = doc.body.firstElementChild;
 
       morphdom(
         contents,
@@ -81,11 +81,17 @@ class UpdateContent {
         }
       );
 
+      console.log(`[Renderer for ${key}]  contentHasUpdated: ${contentHasUpdated ? 'true': 'false'}`);
+
       if (contentHasUpdated === true) {
         const event = new CustomEvent("updateContent.onafterupdate", { detail: { el: [contents]} });
         document.dispatchEvent(event);
       }
     };
+
+    result.id = `${key}_renderer`;
+
+    return result;
   }
 
   async poll(renderer, resource, queue, form) {
@@ -93,10 +99,11 @@ class UpdateContent {
     const startTime = Date.now();
     let keepPolling = true;
 
+    console.log(`[Poll function:start]  resource is ${resource} renderer is ${renderer.id}  queue length is ${queue.length}`);
+
     try {
       // Only send requests when window/tab is in use and nothing in queue
       if (document.visibilityState !== "hidden" && queue.push(renderer) === 1) {
-
         const method = form ? 'POST' : 'GET';
         let fetchOptions = { method: method, headers: {} };
 
@@ -105,8 +112,9 @@ class UpdateContent {
           fetchOptions.headers['Content-Type'] = 'application/x-www-form-urlencoded';
         }
 
+        console.log(`[Poll function:pre-fetch]  resource is ${resource} renderer is ${renderer.id}`);
         const response = await fetch(resource, fetchOptions);
-
+        console.log(`[Poll function:post-fetch] resource is ${resource} renderer is ${renderer.id}  response.ok is ${response.ok ? 'true' : 'false'}`)
         if (!response.ok) {
           if (response.status === 401) {
             window.location.reload();
@@ -118,28 +126,32 @@ class UpdateContent {
         }
 
         const responseData = await response.json();
-        
+        console.log(`[Poll function:data-parsed]  resource is ${resource} renderer is ${renderer.id}`);
         this.flushQueue(queue, responseData);
 
         if (responseData.stop === 1) {
           clearTimeout(timeouts[resource]); // stop polling
           keepPolling = false;
+          return;
         } else {
           intervals[resource] = this.calculateBackoff(Date.now() - startTime); // keep polling but adjust for response time
         }
       }
 
     } catch (error) {
+      console.log(`[Poll function:catch]  resource is ${resource} renderer is ${renderer.id}, error.message is ${error.message}`);
       clearTimeout(timeouts[resource]); // stop polling
       this.clearQueue(queue);
       keepPolling = false;
+      return;
 
     } finally {
+      console.log(`[Poll function:finally]  resource is ${resource} renderer is ${renderer.id}, keepPolling: ${keepPolling ? 'true' : 'false'}, intervals[${resource}]: ${intervals[resource]}`);
       if (keepPolling && typeof intervals[resource] !== 'undefined') {
         clearTimeout(timeouts[resource]);
 
         const boundPoll = this.poll.bind(this, renderer, resource, queue, form);
-        
+
         timeout = setTimeout(
           boundPoll,
           intervals[resource]
