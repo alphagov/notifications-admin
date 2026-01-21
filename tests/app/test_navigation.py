9a9be0b051f1ee44b6c5b0e7ad84655a8d991a2a
Jani Kraner <jani.kraner@digital.cabinet-office.gov.uk>
2026-01-21 11:04:53 +0000

    Exclude 'enable_email_channel' endpoint from navigation test
    It's used for for set-email form when a tasklist is present. That form post to this endpoint and updates service email permissions. This i so avoid clashing with teh on/off form on the page

--- tests/app/test_navigation.py
+++ tests/app/test_navigation.py
@@ -118,6 +118,7 @@ EXCLUDED_ENDPOINTS = set(
             "email_branding",
             "email_not_received",
             "email_template",
+            "enable_email_channel",
             "upload_template_email_files",
             "template_email_files",
             "error",
