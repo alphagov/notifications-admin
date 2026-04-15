83ebc7f79f961b01e984d5e15ebf80a390f6a09c
Robert Scott <robert.scott@digital.cabinet-office.gov.uk>
2026-04-15 14:22:06 +0100

    interruptible_io: add interruptible_every decorator

--- tests/utils/test_interruptible_io.py
+++ tests/utils/test_interruptible_io.py
@@ -2,7 +2,35 @@ from itertools import count, islice
 
 import pytest
 
-from app.utils.interruptible_io import interruptible_iter
+from app.utils.interruptible_io import interruptible_every, interruptible_iter
+
+
+def test_interruptible_every(mocker):
+    mock_sleep = mocker.patch("app.utils.interruptible_io.sleep")
+
+    c = count()
+
+    @interruptible_every(3)
+    def my_func(a):
+        return a + next(c)
+
+    assert len(mock_sleep.mock_calls) == 0
+    assert my_func(5) == 5
+    assert len(mock_sleep.mock_calls) == 0
+    assert my_func(5) == 6
+    assert len(mock_sleep.mock_calls) == 0
+    assert my_func(5) == 7
+    assert len(mock_sleep.mock_calls) == 0
+    assert my_func(5) == 8
+    assert len(mock_sleep.mock_calls) == 1
+    assert my_func(5) == 9
+    assert len(mock_sleep.mock_calls) == 1
+    assert my_func(5) == 10
+    assert len(mock_sleep.mock_calls) == 1
+    assert my_func(5) == 11
+    assert len(mock_sleep.mock_calls) == 2
+    assert my_func(5) == 12
+    assert len(mock_sleep.mock_calls) == 2
 
 
 def test_interruptible_iter(mocker):
