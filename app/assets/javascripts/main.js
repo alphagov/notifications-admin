c8047fd1efcbdffcb574d9a0d3193742fb16aca6
Tom Byers <tombaromba@gmail.com>
2026-05-01 10:20:08 +0100

    Remove sticky JS from legacy build

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,8 +1,6 @@
 
 if (document.body.classList.contains('govuk-frontend-supported')) {
 
-  $(() => GOVUK.stickAtTopWhenScrolling.init());
-  $(() => GOVUK.stickAtBottomWhenScrolling.init());
   $(() => GOVUK.notifyModules.start());
 
   $(() => $('.error-message, .govuk-error-message').eq(0).parent('label').next('input').trigger('focus'));
--- rollup.config.mjs
+++ rollup.config.mjs
@@ -4,7 +4,7 @@ import copy from 'rollup-plugin-copy';
 import styles from "rollup-plugin-styler";
 import postCSSReplace from 'postcss-replace';
 import fs from 'node:fs/promises';
- 
+
 
 const LEGACY_BUNDLE_ID = 'legacy-bundle'
 // Simple file concatenation plugin
@@ -124,7 +124,6 @@ export default [
           paths.npm + 'jquery/dist/jquery.min.js',
           paths.npm + 'textarea-caret/index.js',
           paths.src + 'javascripts/modules.js',
-          paths.src + 'javascripts/stick-to-window-when-scrolling.js',
           paths.src + 'javascripts/templateFolderForm.js',
           paths.src + 'javascripts/main.js'
         ]),
