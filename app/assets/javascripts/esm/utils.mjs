a6ec9a9ad16619c02645478d57f91ffc00a98ff9
Jani Kraner <jani.kraner@digital.cabinet-office.gov.uk>
2026-06-08 15:29:35 +0100

    Update offset util
    Replace window.pageYOffset and window.pageXOffset
    with window.scrollY and window.scrollX.
    
    The Offset properties are now aliases and
    deprecated in modern browser standards.

--- app/assets/javascripts/esm/utils.mjs
+++ app/assets/javascripts/esm/utils.mjs
@@ -1,11 +1,12 @@
 // Taken from https://youmightnotneedjquery.com/#offset
-function offset(el) {
+const offset = (el) => {
   const box = el.getBoundingClientRect();
   const docElem = document.documentElement;
+  
   return {
     top: box.top + window.scrollY - docElem.clientTop,
     left: box.left + window.scrollX - docElem.clientLeft
   };
-}
+};
 
 export { offset };
