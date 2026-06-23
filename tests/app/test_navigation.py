8709dbae4908cf31f00811f30d21bf550b7a72c8
karlchillmaid <karl.chillmaid@digital.cabinet-office.gov.uk>
2026-06-23 13:18:24 +0100

    Update test_navigation.py

--- tests/app/test_navigation.py
+++ tests/app/test_navigation.py
@@ -156,6 +156,7 @@ EXCLUDED_ENDPOINTS = set(
             "guidance_delivery_times",
             "guidance_email_branding",
             "guidance_features",
+            "guidance_fonts_typefaces",
             "guidance_formatting",
             "guidance_how_to_pay",
             "guidance_letter_branding",
